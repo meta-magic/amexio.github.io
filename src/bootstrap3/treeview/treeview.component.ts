01275f66bc406ca8e70eeb9484de12383c600e82
Ketan Gote <ketan.gote@metamagic.in>
2017-06-28 21:32:45 +0530

    datareader blank handled

--- src/bootstrap3/treeview/treeview.component.ts
+++ src/bootstrap3/treeview/treeview.component.ts
@@ -11,18 +11,6 @@
  *
  */
 
-/*
- * Copyright 2016-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Author - Ketan Gote, Pratik Kelwalkar, Dattaram Gawas
- *
- */
 
 import {
     OnInit, Input, Component, EventEmitter, Output, SimpleChanges, ContentChild,
@@ -217,14 +205,17 @@ export class TreeViewComponent implements  OnInit{
         }
     }
 
-    getResponseData(httpResponse : any){
-        let responsedata = httpResponse;
-        let dr = this.dataReader.split(".");
-        for(let ir = 0 ; ir<dr.length; ir++){
-            responsedata = responsedata[dr[ir]];
-        }
-        return responsedata;
+  getResponseData(httpResponse : any){
+    let responsedata = httpResponse;
+    if((this.dataReader && this.dataReader.length>0)){
+      let dr = this.dataReader.split(".");
+      for(let ir = 0 ; ir<dr.length; ir++){
+        responsedata = responsedata[dr[ir]];
+      }
     }
+    return responsedata;
+  }
+     
 
     toggle(treeData : any){
         if(!treeData.leaf)
