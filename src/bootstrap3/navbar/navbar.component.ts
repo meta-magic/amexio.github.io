03ea1e9d0a1f0de2393b57be4818dbb82dd0c6d8
DatsGawas <dattaram.gawas@metamagic.in>
2017-07-04 16:21:57 +0530

    navbar top bottom functionality added

--- src/bootstrap3/navbar/navbar.component.ts
+++ src/bootstrap3/navbar/navbar.component.ts
@@ -21,61 +21,78 @@ import {Http, Headers, RequestOptions} from "@angular/http";
 @Component({
   selector: 'amexio-navbar',
   template : `
+      <footer>
+          <nav class="navbar navbar-default" [ngClass]="{ 'navbar-fixed-bottom':bottom}">
+              <div class="navbar-header">
+                  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+              </div>
 
-    <nav class="navbar navbar-default navbar-static">
-      <div class="navbar-header">
-        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-
-      <div class="collapse navbar-collapse js-navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="dropdown dropdown-large" id="dd-lr-{{ind}}" *ngFor="let mh of menus let ind = index" >
-            <a class="dropdown-toggle" data-toggle="dropdown"  (click)="adjustPosition($event, ind)">
-              <ng-container *ngIf="headerTemplate==null">
-                {{mh.text}}
-              </ng-container><ng-template *ngIf="headerTemplate!=null" [ngTemplateOutlet]="headerTemplate" [ngOutletContext]="{ $implicit: {}, navHeader:mh }"></ng-template>
-              <b *ngIf="mh.childrens" class="caret" ></b>
-            </a>
-            <ng-container *ngIf="mh.childrens">
-              <ul class="dropdown-menu row" id="dd-m-{{ind}}">
-                <li class="col-sm-4" style="position: static !important" *ngFor="let sm of mh.childrens let ind =index">
-                  <ul style="padding: 0px;margin: 0px;">
-                    <li style="list-style: none;color: #428bca;font-size: 18px;padding: 3px 2px;position: static !important">
-                      <a  style="text-decoration: none;color: #428bca" (click)="menuClick(sm)">
-                        <ng-container *ngIf="childTemplate==null">{{sm.text}}</ng-container>
-                        <ng-template *ngIf="childTemplate!=null" [ngTemplateOutlet]="childTemplate" [ngOutletContext]="{ $implicit: {}, menuHeader:sm }"></ng-template>
-                      </a>
-                    </li>
-                    <ng-container *ngIf="sm.childrens">
-                      <li (nodeClick)="menuClick($event)"  style="list-style: none; padding:0px;"  amexio-submenu-view [subMenuData]="sm.childrens"  [templates]="subMenuTemplate"></li>
-                    </ng-container>
+              <div class="collapse navbar-collapse js-navbar-collapse">
+                  <ul class="nav navbar-nav">
+                      <li class="dropdown dropdown-large" id="dd-lr-{{ind}}" *ngFor="let mh of menus let ind = index" >
+                          <a class="dropdown-toggle" data-toggle="dropdown"  (click)="adjustPosition($event, ind)">
+                              <ng-container *ngIf="headerTemplate==null">
+                                  {{mh.text}}
+                              </ng-container><ng-template *ngIf="headerTemplate!=null" [ngTemplateOutlet]="headerTemplate" [ngOutletContext]="{ $implicit: {}, navHeader:mh }"></ng-template>
+                              <b *ngIf="mh.childrens" class="caret" ></b>
+                          </a>
+                          <ng-container *ngIf="mh.childrens">
+                              <ul class="dropdown-menu row" id="dd-m-{{ind}}">
+                                  <li class="col-sm-4" style="position: static !important" *ngFor="let sm of mh.childrens let ind =index">
+                                      <ul style="padding: 0px;margin: 0px;">
+                                          <li style="list-style: none;color: #428bca;font-size: 18px;padding: 3px 2px;position: static !important">
+                                              <a  style="text-decoration: none;color: #428bca" (click)="menuClick(sm)">
+                                                  <ng-container *ngIf="childTemplate==null">{{sm.text}}</ng-container>
+                                                  <ng-template *ngIf="childTemplate!=null" [ngTemplateOutlet]="childTemplate" [ngOutletContext]="{ $implicit: {}, menuHeader:sm }"></ng-template>
+                                              </a>
+                                          </li>
+                                          <ng-container *ngIf="sm.childrens">
+                                              <li (nodeClick)="menuClick($event)"  style="list-style: none; padding:0px;"  amexio-submenu-view [subMenuData]="sm.childrens"  [templates]="subMenuTemplate"></li>
+                                          </ng-container>
+                                      </ul>
+                                  </li>
+                              </ul>
+                          </ng-container>
+                      </li>
                   </ul>
-                </li>
-              </ul>
-            </ng-container>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    
+              </div>
+          </nav>
+      </footer>
+
+
 
 
 
   `,
   styles:[
-    `
-      .dropdown-large {
-        position: static !important;
-      }
-      
-      .dropdown-menu-large {
-        padding: 10px 0px;
-      }
+      `
+          footer .navbar-collapse.in {
+              bottom: 70px;
+              position: absolute;
+              background-color:#333;
+              width: 100%;
+          }
+
+          @media screen and (max-width: 768px) {
+              footer .navbar-collapse {
+                  position: absolute;
+                  bottom: 70px;
+                  width: 100%;
+                  background-color: #303030;
+              }
+          }
+          .dropdown-large {
+              position: static !important;
+          }
+
+          .dropdown-menu-large {
+              padding: 10px 0px;
+          }
 
     `
   ]
@@ -97,6 +114,9 @@ export class NavbarComponent implements OnInit{
 
   @Input()
   bindData: any;
+
+  @Input()
+  bottom:boolean;
 
   @Output()
   selectedNode : any = new EventEmitter<any>();
@@ -160,25 +180,25 @@ export class NavbarComponent implements OnInit{
     let options = new RequestOptions({headers : headers,method : this.httpMethod});
     if(this.httpMethod == "post"){
       this._http.post(this.httpUrl,requestJson,options).subscribe(
-        response=>{
-          this.bindData = response.json();
-        },
-        error=>{
-        },
-        ()=>{
-          this.renderServiceData();
-        }
+          response=>{
+            this.bindData = response.json();
+          },
+          error=>{
+          },
+          ()=>{
+            this.renderServiceData();
+          }
       );
     }else if(this.httpMethod == "get"){
       this._http.get(this.httpUrl,options).subscribe(
-        response=>{
-          this.bindData = response.json();
-        },
-        error=>{
-        },
-        ()=>{
-          this.renderServiceData();
-        }
+          response=>{
+            this.bindData = response.json();
+          },
+          error=>{
+          },
+          ()=>{
+            this.renderServiceData();
+          }
       );
     }
   }
