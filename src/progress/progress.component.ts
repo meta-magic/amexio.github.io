d9e67fec33db3071d63cd7244465afe5f6ad1825
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-08-02 17:12:24 +0530

    Added Progress color variant

--- src/progress/progress.component.ts
+++ src/progress/progress.component.ts
@@ -14,46 +14,47 @@
 import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'amexio-progress-bar',
-  template : `      
-      
-    <div class="progress" *ngIf="showProgress">
-      <div class="progress-bar progress-bar-striped {{progressType !=null ? 'bg-'+progressType : ''}} active" role="progressbar" [attr.aria-valuenow]="currentValue" [attr.aria-valuemin]="minValue" [attr.aria-valuemax]="maxValue" style="width: 100%">
-        <span>{{displayText}}<span class="dotdotdot"></span></span> 
+  template : `
+
+      <div class="progress" *ngIf="showProgress">
+          <div class="progress-bar progress-bar-striped {{progressType !=null ? 'bg-'+progressType : ''}} active" role="progressbar" [attr.aria-valuenow]="currentValue" [attr.aria-valuemin]="minValue" [attr.aria-valuemax]="maxValue" [style.width.%]="infinteMode ? 100 : this.currentValue">
+              <span>{{infinteMode ? displayText : currentValue+'%'}}<span class="dotdotdot"></span></span>
+          </div>
       </div>
-    </div>
-  
+
   `,
-  styles : [`.progress {
-    margin: 15px;
-  }
+  styles : [`
+      .progress {
+          margin: 15px;
+      }
 
-  .progress .progress-bar.active {
-    font-weight: 700;
-    animation: progress-bar-stripes .5s linear infinite;
-  }
+      .progress .progress-bar.active {
+          font-weight: 700;
+          animation: progress-bar-stripes .5s linear infinite;
+      }
 
-  .dotdotdot:after {
-    font-weight: 300;
-    content: '...';
-    display: inline-block;
-    width: 20px;
-    text-align: left;
-    animation: dotdotdot 1.5s linear infinite;
-  }
+      .dotdotdot:after {
+          font-weight: 300;
+          content: '...';
+          display: inline-block;
+          width: 20px;
+          text-align: left;
+          animation: dotdotdot 1.5s linear infinite;
+      }
 
-  @keyframes dotdotdot {
-    0%   { content: '...'; }
-    25% { content: ''; }
-    50% { content: '.'; }
-    75% { content: '..'; }
-  }`]
+      @keyframes dotdotdot {
+          0%   { content: '...'; }
+          25% { content: ''; }
+          50% { content: '.'; }
+          75% { content: '..'; }
+      }`]
 })
 
 export class ProgressComponent implements OnInit{
 
   @Input()    showProgress : boolean = true;
 
-  @Input()    infinteMode : boolean = true;
+  @Input()    infinteMode : boolean;
 
   @Input()    minValue : string;
 
