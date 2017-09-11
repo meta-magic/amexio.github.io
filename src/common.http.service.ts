905058a896799698d8d5919cdfb9489ea24d7943
DatsGawas <dattaram.gawas@metamagic.in>
2017-09-11 14:19:36 +0530

    validateAll method added to validate all form component

--- src/common.http.service.ts
+++ src/common.http.service.ts
@@ -12,25 +12,26 @@
  */
 import {Injectable} from '@angular/core';
 import {Http, RequestOptions, Headers} from '@angular/http';
-import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Observable';
 declare var $;
 @Injectable()
 export class CommonHttpService {
 
   filteredObject: any = [];
+  isValid: boolean;
 
-  constructor(private http : Http) {
+  constructor(private http: Http) {
 
   }
 
-  fetchData(serviceUrl : string, methodType: string) : Observable<any>{
-    let requestJson = {};
-    let headers = new Headers({ 'Content-Type': 'application/json;charset=UTF-8'  });
-    let options = new RequestOptions({headers : headers,method : methodType});
-    if(methodType == "post"){
-      return this.http.post(serviceUrl,requestJson,options);
-    }else if(methodType == "get"){
-      return this.http.get(serviceUrl,options);
+  fetchData(serviceUrl: string, methodType: string): Observable<any> {
+    const requestJson = {};
+    const headers = new Headers({ 'Content-Type': 'application/json;charset=UTF-8'  });
+    const options = new RequestOptions({headers : headers, method : methodType});
+    if (methodType == 'post') {
+      return this.http.post(serviceUrl, requestJson, options);
+    }else if (methodType == 'get') {
+      return this.http.get(serviceUrl, options);
     }
   }
 
@@ -40,14 +41,14 @@ export class CommonHttpService {
    * @param fieldName Name of the Field
    * @param disabled  Boolean, Set true | false to disable
    */
-  setDisabled(inputTexts : any, fieldName: string, disabled: boolean){
+  setDisabled(inputTexts: any, fieldName: string, disabled: boolean){
 
-    let components = inputTexts._results;
+    const components = inputTexts._results;
 
-    for(let iComponents = 0 ; iComponents<components.length; iComponents++){
-      let inputText = components[iComponents];
+    for (let iComponents = 0 ; iComponents < components.length; iComponents++){
+      const inputText = components[iComponents];
 
-      if(inputText.fieldName == fieldName){
+      if (inputText.fieldName == fieldName) {
         inputText.disabled = disabled;
       }
 
@@ -55,51 +56,59 @@ export class CommonHttpService {
 
   }
 
-  validate(inputTexts: any){
+  /* validate(inputTexts: any){
 
-    let components = inputTexts;
+   let components = inputTexts;
 
-    let showMessage = false;
+   let showMessage = false;
 
+   let errorCounter = 1;
+
+   let title= "<strong>Please validate following fields</strong><br><hr>";
+   let validateMsg = "<br>";
+
+   for(let iComponents = 0 ; iComponents<components.length; iComponents++){
+   let inputText = components[iComponents];
+   let isValid = inputText.isValidInput();
+
+   if(isValid){
+   validateMsg = validateMsg +' '+errorCounter +') '+inputText.fieldLabel +"<br/>";
+   showMessage = true;
+   errorCounter++;
+   }
+   }
+
+   return showMessage;
+   }*/
+
+  validateAll(inputTexts: any[]) {
+    let validationData: any = {};
+    const invalidComponent: any[] = [];
+    let invalidMsg: any = {};
+    this.isValid = true;
     let errorCounter = 1;
+    for (let ic = 0; ic < inputTexts.length; ic++) {
+      const component = inputTexts[ic];
+      const isValid = component.isValidInput();
 
-    let title= "<strong>Please validate following fields</strong><br><hr>";
-    let validateMsg = "<br>";
-
-    for(let iComponents = 0 ; iComponents<components.length; iComponents++){
-      let inputText = components[iComponents];
-      let isValid = inputText.isValidInput();
-
-      if(isValid){
-        validateMsg = validateMsg +' '+errorCounter +') '+inputText.fieldLabel +"<br/>";
-        showMessage = true;
+      if (isValid) {
+        if (component.minErrorMsg) {
+          invalidMsg['minErrorMsg'] = component.minErrorMsg;
+        }
+        if (component.maxErrorMsg) {
+          invalidMsg['maxErrorMsg'] =  component.maxErrorMsg;
+        }
+        if (component.errorMsg) {
+          invalidMsg['errorMsg'] = component.errorMsg;
+        }
+        invalidComponent.push({'componentName': component.fieldLabel , 'errorMsgs': invalidMsg});
+        invalidMsg = [];
+        this.isValid = false;
         errorCounter++;
       }
     }
-
-    return showMessage;
-  }
-
-  validateAll(inputTexts: any[]){
-    let showMessage = false;
-    let errorCounter = 1;
-    let title= '<strong>Please validate following fields</strong><hr>';
-    let validateMsg = '<b>'+title+'</b>';
-    for (let ic = 0; ic < inputTexts.length; ic++){
-
-      let component = inputTexts[ic];
-      let isValid = component.isValidInput();
-
-      if(isValid){
-        validateMsg = validateMsg + ' '+errorCounter +') '+component.fieldLabel +"<br>";
-        showMessage = true;
-        errorCounter++;
-      }
-
-
-    }
-
-    return showMessage;
+    validationData = {'ValidationStatus': this.isValid , 'invalidComponent': invalidComponent};
+    return validationData;
   }
 
 
