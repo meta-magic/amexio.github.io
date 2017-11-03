74079d561ea8a2e77d8961829c08b779144d11a3
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-11-03 19:42:39 +0530

    Updated Slider CSS

--- src/slider/slider.component.ts
+++ src/slider/slider.component.ts
@@ -30,112 +30,45 @@ export const BASE_IMPL_RANGE_INPUT: any = {
 @Component({
  selector: 'amexio-slider',
  template: `
-   <div class="amexio-range">
-    <input type="range"
-           name="value"
-           #rangeHndl
-           (blur)="onBlur()"
-           [(ngModel)]="value"
-           [attr.id]="elementId"
-           [attr.min]="minValue"
-           [attr.max]="maxValue"
-           [attr.step]="stepValue"
-           />
-     <output [attr.for]="elementId">{{value}}</output>
-   </div>
+     <div class="amexio-slider">
+         <input type="range"
+                name="value"
+                #rangeHndl
+                (blur)="onBlur()"
+                [(ngModel)]="value"
+                [attr.id]="elementId"
+                [attr.min]="minValue"
+                [attr.max]="maxValue"
+                [attr.step]="stepValue"
+                [ngStyle]="{'background':'linear-gradient(to right, #2cafb3 0%, #2cafb3 '+this.value +'%, #fff ' + this.value + '%, #636363 100%)'}"
+         />
+         <!--<output [attr.for]="elementId">{{value}}</output>-->
+     </div>
  `,
-  styles : [`input[type=range] {
-      height: 25px;
-      -webkit-appearance: none;
-      margin: 10px 0;
-      width: 100%;
-  }
-  input[type=range]:focus {
-      outline: none;
-  }
-  input[type=range]::-webkit-slider-runnable-track {
-      width: 100%;
-      height: 7px;
-      cursor: pointer;
-      animate: 0.2s;
-      box-shadow: 0px 0px 0px #000000;
-      background: #838688;
-      border-radius: 1px;
-      border: 0px solid #000000;
-  }
-  input[type=range]::-webkit-slider-thumb {
-      box-shadow: 0px 0px 0px #000000;
-      border: 1px solid #2497E3;
-      height: 18px;
-      width: 18px;
-      border-radius: 25px;
-      background: #A1D0FF;
-      cursor: pointer;
-      -webkit-appearance: none;
-      margin-top: -7px;
-  }
-  input[type=range]:focus::-webkit-slider-runnable-track {
-      background: #838688;
-  }
-  input[type=range]::-moz-range-track {
-      width: 100%;
-      height: 5px;
-      cursor: pointer;
-      animate: 0.2s;
-      box-shadow: 0px 0px 0px #000000;
-      background: #838688;
-      border-radius: 1px;
-      border: 0px solid #000000;
-  }
-  input[type=range]::-moz-range-thumb {
-      box-shadow: 0px 0px 0px #000000;
-      border: 1px solid #2497E3;
-      height: 18px;
-      width: 18px;
-      border-radius: 25px;
-      background: #A1D0FF;
-      cursor: pointer;
-  }
-  input[type=range]::-ms-track {
-      width: 100%;
-      height: 5px;
-      cursor: pointer;
-      animate: 0.2s;
-      background: transparent;
-      border-color: transparent;
-      color: transparent;
-  }
-  input[type=range]::-ms-fill-lower {
-      background: #838688;
-      border: 0px solid #000000;
-      border-radius: 2px;
-      box-shadow: 0px 0px 0px #000000;
-  }
-  input[type=range]::-ms-fill-upper {
-      background: #838688;
-      border: 0px solid #000000;
-      border-radius: 2px;
-      box-shadow: 0px 0px 0px #000000;
-  }
-  input[type=range]::-ms-thumb {
-      margin-top: 1px;
-      box-shadow: 0px 0px 0px #000000;
-      border: 1px solid #2497E3;
-      height: 18px;
-      width: 18px;
-      border-radius: 25px;
-      background: #A1D0FF;
-      cursor: pointer;
-  }
-  input[type=range]:focus::-ms-fill-lower {
-      background: #838688;
-  }
-  input[type=range]:focus::-ms-fill-upper {
-      background: #838688;
-  }
-  .amexio-range{
-      display: inline !important;
-  }
+  styles : [`
+      .amexio-slider input {
+          background: -webkit-linear-gradient(left, #2cafb3 0%, #2cafb3 50%, #636363 50%, #636363 100%);
+          background: linear-gradient(to right, #2cafb3 0%, #2cafb3 50%, #636363 50%, #636363 100%);
+          border: solid 1px #2cafb3;
+          border-radius: 8px;
+          height: 7px;
+          width: 356px;
+          outline: none;
+          -webkit-transition: background 450ms ease-in;
+          transition: background 450ms ease-in;
+          -webkit-appearance: none;
+      }
+      .amexio-slider input::-webkit-slider-thumb {
+          background-color: black;
+          border: solid 12px black;
+          border-radius: 0;
+          height: 15px;
+          width: 15px;
+          border-radius: 50%;
+          -webkit-appearance: none;
+      }
+
+
   `],
   providers : [CUSTOM_RANGE_INPUT_CONTROL_VALUE_ACCESSOR, BASE_IMPL_RANGE_INPUT],
 })
