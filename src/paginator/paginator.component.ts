f51d221045f7a858b42faa7301e8080f3f17ebb2
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-11-15 19:05:06 +0530

    Updated Paginator

--- src/paginator/paginator.component.ts
+++ src/paginator/paginator.component.ts
@@ -17,14 +17,14 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
     template: `
 
         <ul class="pagination justify-content-center" [ngClass]="{'pagination-lg' : size =='large','pagination-md' : size =='medium','pagination-sm' : size =='small'}" style="background-color: f6f7f9;border: 1px solid #d9d9d9;">
-            <li class="page-item " style="cursor: pointer;" (click)="onFirstClick()">
-                <a class="page-link" aria-label="Previous">
+            <li class="page-item " (click)="onFirstClick()">
+                <a class="page-link" href="#" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                     <span class="sr-only">First</span>
                 </a>
             </li>
             <li class="page-item {{activePageIndex == 0 ? 'disabled' : ''}}" (click)="onPrevious()">
-                <a class="page-link" style="cursor: pointer;" aria-label="Previous">
+                <a class="page-link" href="#" aria-label="Previous">
                     <span aria-hidden="true">&#x3C;</span>
                     <span class="sr-only">Previous</span>
                 </a>
@@ -36,23 +36,42 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
                 </li>
             </ng-container>
 
-            <li style="cursor: pointer;" class="page-item {{activePageIndex == pagesArray.length ? 'disabled' : ''}}" (click)="onNext()">
-                <a class="page-link" aria-label="Next">
+            <li class="page-item {{activePageIndex == pagesArray.length ? 'disabled' : ''}}" (click)="onNext()">
+                <a class="page-link" href="#" aria-label="Next">
                     <span aria-hidden="true">&#x3E;</span>
                     <span class="sr-only">Next</span>
                 </a>
             </li>
-            <li style="cursor: pointer;" class="page-item" (click)="onLastClick()">
-                <a class="page-link" aria-label="Next">
+            <li class="page-item" (click)="onLastClick()">
+                <a class="page-link" href="#" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                     <span class="sr-only">Last</span>
                 </a>
             </li>
 
+            <li class="page-item">
+                <div class="btn-group" style="padding-top: 5px;padding-left: 5px;">
+                    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{currentRow}}
+                    </button>
+                    <div class="dropdown-menu amexio-scrollable">
+                        <a *ngFor="let row of pageIndex" class="dropdown-item" style="cursor : pointer;" (click)="changeRows(row)">{{row}}</a>
+                    </div>
+                </div>
+            </li>
+
         </ul>
 
 
-    `
+    `,
+    styles : [`
+        .amexio-scrollable{
+            height: auto;
+            max-height: 300px;
+            overflow-x: hidden;
+        }
+
+    `]
 })
 
 export class AmexioPaginatorComponent implements OnInit {
@@ -65,18 +84,56 @@ export class AmexioPaginatorComponent implements OnInit {
 
     pagesArray : number[] = [];
 
+    pageIndex : number[] = [];
+
+    currentRow : number ;
+
     activePageIndex : number = 0;
 
+    first : number;
+
+    last : number;
+
     @Output()    onPageChange : EventEmitter<any> = new EventEmitter<any>();
+
+    @Output()    onRowChange : EventEmitter<any> = new EventEmitter<any>();
 
     constructor() { }
 
     ngOnInit() {
-        for (let i = 0; i < this.pages;i++){
-            this.pagesArray.push(i+1);
-        }
         if(this.size == null || this.size == '')
             this.size = 'medium';
+
+        if(this.rows != null){
+            this.calculateRows();
+            this.setRows(this.rows);
+        }
+        else{
+            for (let i = 0; i < this.pages;i++){
+                this.pagesArray.push(i+1);
+            }
+        }
+    }
+
+    setRows(rowNumber : number){
+        this.currentRow = rowNumber;
+        for(let i = 0 ; i < rowNumber; i++){
+            this.pagesArray.push(i+1);
+        }
+
+    }
+
+    calculateRows(){
+        for(let i = 0; i < this.rows ; i++){
+            this.pagesArray.push(i+1);
+        }
+        this.currentRow = this.rows;
+        //calc rows
+        let loopI = Math.round(this.pages/this.rows);
+
+        for(let i = 1;i <= loopI ; i++){
+            this.pageIndex.push(this.rows * i);
+        }
     }
 
     onPageClick(pageNumber : number, index : number){
@@ -86,7 +143,6 @@ export class AmexioPaginatorComponent implements OnInit {
 
     onPrevious(){
         if(this.activePageIndex != 0){
-            debugger;
             this.activePageIndex -= 1;
             this.onPageChange.emit(this.activePageIndex+1);
         }
@@ -101,11 +157,28 @@ export class AmexioPaginatorComponent implements OnInit {
 
     onFirstClick(){
         this.activePageIndex = 0;
-        this.onPageChange.emit(1);
+        this.onPageChange.emit(this.first);
     }
 
     onLastClick(){
         this.activePageIndex = this.pagesArray.length-1;
-        this.onPageChange.emit(this.pagesArray.length);
+        this.onPageChange.emit(this.last);
+    }
+
+    changeRows(rowNumber : number){
+        this.currentRow = rowNumber;
+        this.onRowChange.emit(this.currentRow);
+
+        this.pagesArray = [];
+        for(let i = this.currentRow - this.rows;i<=this.currentRow;i++){
+            this.pagesArray.push(i);
+        }
+        this.setBoundaries();
+
+    }
+
+    setBoundaries(){
+        this.first = this.pagesArray[0];
+        this.last = this.pagesArray[this.pagesArray.length];
     }
 }
