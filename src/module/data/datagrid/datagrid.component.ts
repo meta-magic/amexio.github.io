bb58b06c87b785314bf51bd3acfbfb7c5c3f7f32
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-01-17 21:22:36 +0530

    Event Name changed

--- src/module/data/datagrid/datagrid.component.ts
+++ src/module/data/datagrid/datagrid.component.ts
@@ -731,7 +731,7 @@ export class AmexioDatagridComponent implements OnInit, AfterContentInit, DoChec
   }
 
   sortOnColHeaderClick(sortCol: any, event: any) {
-    this.onHeaderClick.emit({event: event, data: sortCol);
+    this.onHeaderClick.emit({event: event, data: sortCol});
     if (this.sortBy === -1) {
       this.sortBy = 1;
     } else if (this.sortBy === 1) {
