e5fe33f799d0e9deac1434ba07c19dc308aae85e
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-30 15:11:35 +0530

    slider : Added new slider

--- src/module/forms/slider/slider.component.ts
+++ src/module/forms/slider/slider.component.ts
@@ -1,118 +1,365 @@
-/**
- * Created by pratik on 28/12/17.
- */
-import {Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {
+  Component, ElementRef, OnDestroy, Input, Output, EventEmitter, forwardRef, Renderer2,
+  NgZone
+} from '@angular/core';
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms';
+import {DomHandler} from "./slider.handler";
 
-const noop = () => {
-};
 
-export const CUSTOM_SLIDER_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => AmexioSliderComponent), multi: true
+export const SLIDER_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => AmexioSliderComponent),
+  multi: true
 };
 
 @Component({
-  selector: 'amexio-slider', template: `
-    <input type="range"
-           [(ngModel)]="value"
-           #inp="ngModel"
-           [ngClass]="{'input-control-error' : inp.invalid && (inp.dirty || inp.touched),'input-control-success' : inp.valid && (inp.dirty || inp.touched)}"
-           (input)="onInput()"
-           (blur)="onblur()"
-           (focus)="onFocus()"
-           (change)="onChange($event)"
-           [attr.min]="minvalue"
-           [attr.max]="maxvalue"
-           [attr.step]="stepvalue"
-           orient="vertical"
-           value="15"
-           [ngStyle]="{'background-image' : '-webkit-gradient(linear, left top, right top,color-stop(' + val + ', #94A14E),color-stop(' + val + ', #C5C5C5)'}">
-
-  `, providers: [CUSTOM_SLIDER_CONTROL_VALUE_ACCESSOR]
+  selector: 'amexio-slider',
+  template: `
+    <div [ngStyle]="style" [class]="styleClass" [ngClass]="{'ui-slider ui-widget ui-widget-content ui-corner-all':true,'ui-state-disabled':disabled,
+            'ui-slider-horizontal':orientation == 'horizontal','ui-slider-vertical':orientation == 'vertical','ui-slider-animate':animate}"
+         (click)="onBarClick($event)">
+      <span *ngIf="range && orientation == 'horizontal'" class="ui-slider-range ui-widget-header ui-corner-all" [ngStyle]="{'left':handleValues[0] + '%',width: (handleValues[1] - handleValues[0] + '%')}"></span>
+      <span *ngIf="range && orientation == 'vertical'" class="ui-slider-range ui-widget-header ui-corner-all" [ngStyle]="{'bottom':handleValues[0] + '%',height: (handleValues[1] - handleValues[0] + '%')}"></span>
+      <span *ngIf="!range && orientation=='vertical'" class="ui-slider-range ui-slider-range-min ui-widget-header ui-corner-all" [ngStyle]="{'height': handleValue + '%'}"></span>
+      <span *ngIf="!range && orientation=='horizontal'" class="ui-slider-range ui-slider-range-min ui-widget-header ui-corner-all" [ngStyle]="{'width': handleValue + '%'}"></span>
+      <span *ngIf="!range" class="ui-slider-handle ui-state-default ui-corner-all ui-clickable" (mousedown)="onMouseDown($event)" (touchstart)="onTouchStart($event)" (touchmove)="onTouchMove($event)" (touchend)="dragging=false"
+            [style.transition]="dragging ? 'none': null" [ngStyle]="{'left': orientation == 'horizontal' ? handleValue + '%' : null,'bottom': orientation == 'vertical' ? handleValue + '%' : null}"></span>
+      <span *ngIf="range" (mousedown)="onMouseDown($event,0)" (touchstart)="onTouchStart($event,0)" (touchmove)="onTouchMove($event,0)" (touchend)="dragging=false" [style.transition]="dragging ? 'none': null" class="ui-slider-handle ui-state-default ui-corner-all ui-clickable"
+            [ngStyle]="{'left': rangeStartLeft, 'bottom': rangeStartBottom}" [ngClass]="{'ui-slider-handle-active':handleIndex==0}"></span>
+      <span *ngIf="range" (mousedown)="onMouseDown($event,1)" (touchstart)="onTouchStart($event,1)" (touchmove)="onTouchMove($event,1)" (touchend)="dragging=false" [style.transition]="dragging ? 'none': null" class="ui-slider-handle ui-state-default ui-corner-all ui-clickable"
+            [ngStyle]="{'left': rangeEndLeft, 'bottom': rangeEndBottom}" [ngClass]="{'ui-slider-handle-active':handleIndex==1}"></span>
+    </div>
+  `,
+  providers : [SLIDER_VALUE_ACCESSOR]
 })
+export class AmexioSliderComponent implements OnDestroy,ControlValueAccessor {
 
-export class AmexioSliderComponent implements OnInit, ControlValueAccessor {
+  @Input() animate: boolean;
 
-  @Input('min-value') minvalue: number;
+  @Input() disabled: boolean;
 
-  @Input('max-value') maxvalue: number;
+  @Input('min-value') min: number = 0;
 
-  @Input('step-value') stepvalue: number;
+  @Input('max-value') max: number = 100;
 
-  @Input('icon-feedback') iconfeedback: boolean;
+  @Input() orientation: string = 'horizontal';
 
-  @Output() onBlur: any = new EventEmitter<any>();
+  @Input('step-value') step: number;
 
-  @Output() input: any = new EventEmitter<any>();
+  @Input() range: boolean;
 
-  @Output() focus: any = new EventEmitter<any>();
+  @Input() style: any;
 
-  @Output() change: any = new EventEmitter<any>();
+  @Input('style-class') styleClass: string;
 
-  val: any;
+  @Output() onChange: EventEmitter<any> = new EventEmitter();
 
-  constructor() {
+  @Output() onSlideEnd: EventEmitter<any> = new EventEmitter();
+
+  public value: number;
+
+  public values: number;
+
+  public handleValue: number;
+
+  public handleValues: number[] = [];
+
+  public onModelChange: Function = () => {};
+
+  public onModelTouched: Function = () => {};
+
+  public dragging: boolean;
+
+  public dragListener: any;
+
+  public mouseupListener: any;
+
+  public initX: number;
+
+  public initY: number;
+
+  public barWidth: number;
+
+  public barHeight: number;
+
+  public sliderHandleClick: boolean;
+
+  public handleIndex: number = 0;
+
+  public startHandleValue: any;
+
+  public startx: number;
+
+  public starty: number;
+
+  constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2, private ngZone: NgZone) {}
+
+  onMouseDown(event:Event, index?:number) {
+    if(this.disabled) {
+      return;
+    }
+
+    this.dragging = true;
+    this.updateDomData();
+    this.sliderHandleClick = true;
+    this.handleIndex = index;
+    this.bindDragListeners();
+    event.preventDefault();
   }
 
-  ngOnInit() {
+  onTouchStart(event : any, index?:number) {
+    var touchobj = event.changedTouches[0];
+    this.startHandleValue = (this.range) ? this.handleValues[index] : this.handleValue;
+    this.dragging = true;
+    this.handleIndex = index;
+
+    if (this.orientation === 'horizontal') {
+      this.startx = parseInt(touchobj.clientX, 10);
+      this.barWidth = this.el.nativeElement.children[0].offsetWidth;
+    }
+    else {
+      this.starty = parseInt(touchobj.clientY, 10);
+      this.barHeight = this.el.nativeElement.children[0].offsetHeight;
+    }
+
+    event.preventDefault();
   }
 
-  onChange(event: any) {
-    this.val = (this.value - this.minvalue) / (this.maxvalue - this.minvalue);
+  onTouchMove(event : any, index?:number) {
+    var touchobj = event.changedTouches[0],
+      handleValue = 0;
+
+    if (this.orientation === 'horizontal') {
+      handleValue = Math.floor(((parseInt(touchobj.clientX, 10) - this.startx) * 100) / (this.barWidth)) + this.startHandleValue;
+    }
+    else {
+      handleValue = Math.floor(((this.starty - parseInt(touchobj.clientY, 10)) * 100) / (this.barHeight))  + this.startHandleValue;
+    }
+
+    this.setValueFromHandle(event, handleValue);
+
+    event.preventDefault();
   }
 
+  onBarClick(event : any) {
+    if(this.disabled) {
+      return;
+    }
 
-  // The internal dataviews model
-  private innerValue: any = '';
+    if(!this.sliderHandleClick) {
+      this.updateDomData();
+      this.handleChange(event);
+    }
 
-  //Placeholders for the callbacks which are later provided
-  //by the Control Value Accessor
-  private onTouchedCallback: () => void = noop;
-  private onChangeCallback: (_: any) => void = noop;
-
-  //get accessor
-  get value(): any {
-    return this.innerValue;
+    this.sliderHandleClick = false;
   }
 
-  //set accessor including call the onchange callback
-  set value(v: any) {
-    if (v !== this.innerValue) {
-      this.innerValue = v;
-      this.onChangeCallback(v);
+  handleChange(event: Event) {
+    let handleValue = this.calculateHandleValue(event);
+    this.setValueFromHandle(event, handleValue);
+  }
+
+  bindDragListeners() {
+    this.ngZone.runOutsideAngular(() => {
+      if (!this.dragListener) {
+        this.dragListener = this.renderer.listen('document', 'mousemove', (event : any) => {
+          if (this.dragging) {
+            this.ngZone.run(() => {
+              this.handleChange(event);
+            });
+          }
+        });
+      }
+
+      if (!this.mouseupListener) {
+        this.mouseupListener = this.renderer.listen('document', 'mouseup', (event : any) => {
+          if (this.dragging) {
+            this.dragging = false;
+            this.ngZone.run(() => {
+              if (this.range) {
+                this.onSlideEnd.emit({originalEvent: event, values: this.values});
+              } else {
+                this.onSlideEnd.emit({originalEvent: event, value: this.value});
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+
+  unbindDragListeners() {
+    if(this.dragListener) {
+      this.dragListener();
+    }
+
+    if(this.mouseupListener) {
+      this.mouseupListener();
     }
   }
 
-  //Set touched on blur
-  onblur() {
-    this.onTouchedCallback();
-    // this.showToolTip = false;
-    this.onBlur.emit();
-  }
+  setValueFromHandle(event: Event, handleValue: any) {
+    let newValue = this.getValueFromHandle(handleValue);
 
-  onFocus() {
-    // this.showToolTip = true;
-    this.change.emit();
-  }
-
-  //From ControlValueAccessor interface
-  writeValue(value: any) {
-    if (value !== this.innerValue) {
-      this.innerValue = value;
+    if(this.range) {
+      if(this.step) {
+        this.handleStepChange(newValue, this.values[this.handleIndex]);
+      }
+      else {
+        this.handleValues[this.handleIndex] = handleValue;
+        this.updateValue(newValue, event);
+      }
+    }
+    else {
+      if(this.step) {
+        this.handleStepChange(newValue, this.value);
+      }
+      else {
+        this.handleValue = handleValue;
+        this.updateValue(newValue, event);
+      }
     }
   }
 
-  //From ControlValueAccessor interface
-  registerOnChange(fn: any) {
-    this.onChangeCallback = fn;
+  handleStepChange(newValue: number, oldValue: number) {
+    let diff = (newValue - oldValue);
+    let val = oldValue;
+
+    if(diff < 0) {
+      val = oldValue + Math.ceil((newValue - oldValue) / this.step) * this.step;
+    }
+    else if(diff > 0) {
+      val = oldValue + Math.floor((newValue - oldValue) / this.step) * this.step;
+    }
+
+    this.updateValue(val);
+    this.updateHandleValue();
   }
 
-  //From ControlValueAccessor interface
-  registerOnTouched(fn: any) {
-    this.onTouchedCallback = fn;
+  writeValue(value: any) : void {
+    if(this.range)
+      this.values = value||[0,0];
+    else
+      this.value = value||0;
+
+    this.updateHandleValue();
   }
 
-  onInput() {
-    this.input.emit();
+  registerOnChange(fn: Function): void {
+    this.onModelChange = fn;
+  }
+
+  registerOnTouched(fn: Function): void {
+    this.onModelTouched = fn;
+  }
+
+  setDisabledState(val: boolean): void {
+    this.disabled = val;
+  }
+
+  get rangeStartLeft() {
+    return this.isVertical() ? 'auto' : this.handleValues[0] + '%';
+  }
+
+  get rangeStartBottom() {
+    return this.isVertical() ? this.handleValues[0] + '%' : 'auto';
+  }
+
+  get rangeEndLeft() {
+    return this.isVertical() ? 'auto' : this.handleValues[1] + '%';
+  }
+
+  get rangeEndBottom() {
+    return this.isVertical() ? this.handleValues[1] + '%' : 'auto';
+  }
+
+  isVertical(): boolean {
+    return this.orientation === 'vertical';
+  }
+
+  updateDomData(): void {
+    let rect = this.el.nativeElement.children[0].getBoundingClientRect();
+    this.initX = rect.left + this.domHandler.getWindowScrollLeft();
+    this.initY = rect.top + this.domHandler.getWindowScrollTop();
+    this.barWidth = this.el.nativeElement.children[0].offsetWidth;
+    this.barHeight = this.el.nativeElement.children[0].offsetHeight;
+  }
+
+  calculateHandleValue(event : any): number {
+    if(this.orientation === 'horizontal')
+      return ((event.pageX - this.initX) * 100) / (this.barWidth);
+    else
+      return(((this.initY + this.barHeight) - event.pageY) * 100) / (this.barHeight);
+  }
+
+  updateHandleValue(): void {
+    if(this.range) {
+      this.handleValues[0] = (this.values[0] < this.min ? 0 : this.values[0] - this.min) * 100 / (this.max - this.min);
+      this.handleValues[1] = (this.values[1] > this.max ? 100 : this.values[1] - this.min) * 100 / (this.max - this.min);
+    }
+    else {
+      if(this.value < this.min)
+        this.handleValue = 0;
+      else if(this.value > this.max)
+        this.handleValue = 100;
+      else
+        this.handleValue = (this.value - this.min) * 100 / (this.max - this.min);
+    }
+  }
+
+  updateValue(val: number, event?: Event): void {
+    if(this.range) {
+      let value = val;
+
+      if(this.handleIndex == 0) {
+        if(value < this.min) {
+          value = this.min;
+          this.handleValues[0] = 0;
+        }
+        else if (value > this.values[1]) {
+          value = this.values[1];
+          this.handleValues[0] = this.handleValues[1];
+        }
+      }
+      else {
+        if(value > this.max) {
+          value = this.max;
+          this.handleValues[1] = 100;
+        }
+        else if (value < this.values[0]) {
+          value = this.values[0];
+          this.handleValues[1] = this.handleValues[0];
+        }
+      }
+
+      this.values[this.handleIndex] = Math.floor(value);
+      this.onModelChange(this.values);
+      this.onChange.emit({event: event, values: this.values});
+    }
+    else {
+      if(val < this.min) {
+        val = this.min;
+        this.handleValue = 0;
+      }
+      else if (val > this.max) {
+        val = this.max;
+        this.handleValue = 100;
+      }
+
+      this.value = Math.floor(val);
+      this.onModelChange(this.value);
+      this.onChange.emit({event: event, value: this.value});
+    }
+  }
+
+  getValueFromHandle(handleValue: number): number {
+    return (this.max - this.min) * (handleValue / 100) + this.min;
+  }
+
+  ngOnDestroy() {
+    this.unbindDragListeners();
   }
 }
+
+
+
