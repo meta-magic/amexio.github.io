621cd630928c5f15f691e9739f940bac18f12498
DatsGawas <dattaram.gawas@metamagic.in>
2018-02-01 19:30:12 +0530

    video player : width added.

--- src/module/enterprise/youtube-player/youtube.player.component.ts
+++ src/module/enterprise/youtube-player/youtube.player.component.ts
@@ -7,11 +7,11 @@ import {DomSanitizer} from "@angular/platform-browser";
 @Component({
   selector: 'amexio-ee-youtube-player', template: `
     <div>
-      <span class="close-button">
+      <span class="close-button" [ngStyle]="{'padding-left':closePadding+'%'}">
         <i class="fa fa-times fa-lg" (click)="routeBackToApp()" aria-hidden="true"></i>
       </span>
       <div class="text-center">
-        <iframe [height]="height" width="98%"
+        <iframe [height]="height" [width]="width+'%'"
                 [src]="sanitizedUrl" frameborder="0"
                 allowfullscreen>
         </iframe>
@@ -20,7 +20,6 @@ import {DomSanitizer} from "@angular/platform-browser";
   `, styles: [`
     .close-button {
       cursor: pointer;
-      padding-left: 98%;
       color: gray;
       background: radial-gradient(ellipse at top right, rgba(0, 0, 0, .4) 0, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0) 100%);
     }
@@ -39,9 +38,13 @@ export class AmexioYoutubePlayerComponent implements OnInit {
 
   @Input() height: any;
 
+  @Input() width : number;
+
   @Output() onCloseVideoPlayer: EventEmitter<any> = new EventEmitter<any>();
 
   sanitizedUrl: any;
+
+  closePadding: any;
 
   constructor(private sanitizer: DomSanitizer) {
 
@@ -50,6 +53,12 @@ export class AmexioYoutubePlayerComponent implements OnInit {
   ngOnInit() {
     if (this.height == null || this.height === 'undefined') {
       this.height = '98%';
+    }
+    if (this.width == null) {
+      this.width = 98;
+      this.closePadding = 97;
+    } else {
+      this.closePadding = this.width - 1;
     }
     if (this.url != null) this.sanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
