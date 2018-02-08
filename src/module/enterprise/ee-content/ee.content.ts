f19e177680131516adb6f9e2015c61162a036701
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-02-08 12:08:36 +0530

    enterprise-content  : removed all inline styling

--- src/module/enterprise/ee-content/ee.content.ts
+++ src/module/enterprise/ee-content/ee.content.ts
@@ -8,7 +8,7 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
        <span *ngIf="closeEnable" class="close-button">
         <i class="fa fa-times fa-lg" (click)="closeDetailPage()" aria-hidden="true"></i>
       </span>
-      <div class="col-lg-6 col-sm-12 content-area" style="color: white;padding-left: 30px;">
+      <div class="content-area" >
         <span class="title">{{title}}</span><br>
         <span class="subtitle"><span class="match" *ngIf="matchPercentage">{{matchPercentage}} Match</span> {{releaseYear}} <span
           class="age" *ngIf="ageLimit">{{ageLimit}}</span> {{seasonNo}}</span><br>
@@ -20,13 +20,13 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
                   style="color: #999;">{{data.key}}: </strong> <strong>  {{data.value}}</strong></li><br>
           </span>
         </ng-container>
-        <ul style="padding-left: 0">
+        <ul class="inline-list">
           <li *ngIf="enableWatch">
-            <button (click)="playVideo()"><i class="fa fa-play"></i> &nbsp;&nbsp; Play</button>
+            <button class="content-button" (click)="playVideo()"><i class="fa fa-play"></i> &nbsp;&nbsp; Play</button>
             <!--<amexio-ee-btn [cClass]="'buttoncustom'" [label]="'Play'" [type]="'danger'" [icon]="'fa fa-play'" [tooltipMessage]="'play'" [size]="size" (onClick)="playVideo()"></amexio-ee-btn>-->
           </li>
           <li *ngIf="enableMyList">
-            <button (click)="addToList()"><i class="fa fa-plus"></i>&nbsp;&nbsp; MY LIST</button>
+            <button class="content-button" (click)="addToList()"><i class="fa fa-plus"></i>&nbsp;&nbsp; MY LIST</button>
             <!--<amexio-ee-btn [cClass]="'secondarybutton'" [label]="'MY LIST'" [icon]="'fa fa-plus'" [type]="'secondary'" [tooltipMessage]="'My List'" [size]="size" (onClick)="addToList()"></amexio-ee-btn>-->
           </li>
           <li>
@@ -45,224 +45,7 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
           </amexio-rating-input>
         </ng-container>
       </div>
-    </div>
-  `, styles: [`
-    ul li {
-      display: inline;
-    }
-
-    .maincontent {
-      position: relative;
-      width: 100%; /* for IE 6 */
-      height: 65vh;
-      width: 100%;
-      background-size: 100% 100%;
-      background-repeat: no-repeat;
-    }
-
-    .maincontent div {
-      position: absolute;
-      top: 10px;
-      left: 0;
-      width: 100%;
-    }
-
-    .title {
-      width: 100%;
-      font-size: 180%;
-      font-weight: 700;
-      line-height: 130%;
-      color: #fff;
-      text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
-    }
-
-    @media screen and (min-width: 1400px) {
-      .maincontent div {
-        height: 32vw;
-        padding-bottom: 20px;
-      }
-    }
-
-    @media (max-width: 799px) {
-      .maincontent div {
-        font-size: 11px;
-      }
-
-      .maincontent div {
-        top: 10px;
-        width: 33vw;
-        min-width: 330px;
-        color: #999;
-        line-height: 1.3;
-      }
-
-      .maincontent div table {
-        height: 50px;
-      }
-    }
-
-    @media (max-width: 480px) {
-
-      img {
-        width: 170px;
-        height: 35px;
-      }
-
-      .title {
-        width: 100%;
-        font-size: 180%;
-        font-weight: 700;
-        line-height: 130%;
-        color: #fff;
-        text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
-      }
-
-      h3 {
-        font-size: 9pt;
-      }
-
-      span {
-        font-size: 6pt;
-      }
-
-      li b {
-        font-size: 6pt;
-      }
-
-      li {
-        font-size: 6pt;
-      }
-
-      .maincontent {
-        position: relative;
-        width: 100%; /* for IE 6 */
-        height: 35vh;
-        width: 100%;
-        background-size: 100% 100%;
-        background-repeat: no-repeat;
-      }
-    }
-
-    header {
-      width: 100%;
-      padding: 0px 48px;
-      background-image: linear-gradient(rgba(0, 0, 0, 0.0), #141414);
-      background-size: cover;
-      background-position: center;
-      height: 100vh;
-    }
-
-    header .billboard {
-      margin-top: 40px;
-      width: 40%;
-      padding: 2px;
-      font-size: 110%;
-      line-height: 160%;
-    }
-
-    header .billboard .tab-area a {
-      width: 120px;
-      float: left;
-      font-size: 90%;
-      color: #ffffff;
-      text-align: center;
-      text-transform: uppercase;
-      text-decoration: none;
-      padding: 8px;
-      margin-right: .75em;
-      margin-bottom: 30px;
-      transition: border 0.5s;
-    }
-
-    header .billboard .tab-area a:hover {
-      border-bottom: solid 2px #eeeeee;
-    }
-
-    header .billboard .tab-area .active {
-      border-bottom: solid 2px #e50914;
-    }
-
-    /* Text Related CSS */
-    header .billboard .content-area {
-      color: #999;
-      font-size: 100%;
-    }
-
-    header .billboard .content-area .subtitle {
-      font-size: 120%;
-      line-height: 200%;
-    }
-
-    header .billboard .content-area .credits {
-      font-size: 80%;
-    }
-
-    header .billboard .content-area .credits strong {
-      color: #fff;
-    }
-
-    /* Button Related CSS */
-    header .billboard .button {
-      display: inline-block;
-      margin-right: .75em;
-      margin-top: 20px;
-      padding: 5px 20px;
-      font-size: 70%;
-      font-weight: 500;
-      text-decoration: none;
-      text-transform: uppercase;
-      color: #fff;
-    }
-
-    .button i {
-      display: inline-block;
-      margin-top: 20px;
-      width: 40px;
-      height: 40px;
-      padding: 10px 0 0 11px;
-      border: solid 2px rgba(255, 255, 255, .4);
-      border-radius: 100px;
-      font-size: 18px;
-      font-weight: 500;
-      text-decoration: none;
-      text-transform: uppercase;
-      color: #fff;
-      transition: transform 0.5s;
-    }
-
-    .button i:hover {
-      border: solid 2px #fff;
-      transform: scale(1.1);
-    }
-
-    .buttonSmall i {
-      display: inline-block;
-      width: 25px;
-      height: 25px;
-      padding: 7px 0 0 5px;
-      border: solid 2px rgba(255, 255, 255, .4);
-      border-radius: 100px;
-      font-size: 12px;
-      font-weight: 500;
-      text-decoration: none;
-      text-transform: uppercase;
-      color: #fff;
-      transition: transform 0.5s;
-    }
-
-    .buttonSmall i:hover {
-      border: solid 2px #fff;
-      transform: scale(1.1);
-    }
-
-    .close-button {
-      cursor: pointer;
-      padding-left: 98%;
-      color: gray;
-      background: radial-gradient(ellipse at top right, rgba(0, 0, 0, .4) 0, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0) 100%);
-    }
-
-  `]
+    </div>`
 })
 
 export class ContentComponent implements OnInit, OnChanges {
