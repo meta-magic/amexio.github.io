97b4ffa590b6fd39eb29effda5e170945cc24761
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-02-12 13:42:55 +0530

    File Upload :field-label  + service call issue resolved

--- src/module/forms/fileupload/fileupload.component.ts
+++ src/module/forms/fileupload/fileupload.component.ts
@@ -2,23 +2,33 @@
  * Created by pratik on 15/1/18.
  */
 import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-
+import {CommonDataService} from "../../services/data/common.data.service";
 @Component({
   selector: 'amexio-fileupload', template: `
     <div class="input-group">
-      <label>Choose File</label>
+      <ng-container *ngIf="fieldlabel">
+       <label>{{fieldlabel}}</label>
+      </ng-container>
+        <ng-container *ngIf="!fieldlabel">
+       <label >Choose File</label>
+      </ng-container>
       <input type="file" class="input-control"
-             [attr.accept]="filetype" (change)="uploadFile($event)"
+             [attr.accept]="filetype" (change)="uploadFile($event,false)"
              [attr.multiple]="multiplefile" #inp>
     </div>
     <ng-container *ngIf="droppable">
-      <h5>Drag and DropFiles below</h5>
+      <ng-container *ngIf="fieldlabel">
+      <label>{{fieldlabel}}</label>
+     </ng-container>
+       <ng-container *ngIf="!fieldlabel">
+       <label>Drag and Drop Files below</label>
+       </ng-container>
       <div class="upload-drop-zone {{dropClass}}" (drop)="onFileDrop($event)" (dragover)="onDragOver($event)"
            (dragleave)="dropClass = '';" #drpZone>
         Just drag and drop files here
       </div>
       <span>File Name : {{uploadedFileName}}</span>
-    </ng-container>
+
 
   `
 })
@@ -43,6 +53,8 @@ export class AmexioFileUploadComponent implements OnInit {
 
   @ViewChild('inp') inpHandle: any;
 
+  responseData:any;
+
   @Output() blur: EventEmitter<any> = new EventEmitter<any>();
   @Output() change: EventEmitter<any> = new EventEmitter<any>();
   @Output() input: EventEmitter<any> = new EventEmitter<any>();
@@ -52,7 +64,7 @@ export class AmexioFileUploadComponent implements OnInit {
 
   dropClass: string;
 
-  constructor() {
+  constructor(public dataService: CommonDataService,) {
 
   }
 
@@ -73,7 +85,7 @@ export class AmexioFileUploadComponent implements OnInit {
         if (dt.items[i].kind == "file") {
           let f = dt.items[i].getAsFile();
           this.uploadedFileName = f.name;
-          this.uploadFile(event);
+          this.uploadFile(f,true);
         }
       }
     } else {
@@ -90,24 +102,53 @@ export class AmexioFileUploadComponent implements OnInit {
   }
 
   //  For Uploading files
-  uploadFile(event: any) {
-    let fileList: FileList = event.target.files;
-    let formData = new FormData();
-    if (fileList) {
-      for (let i = 0; i < fileList.length; i++) {
-        if (!this.paramname) {
-          this.paramname = "file";
+  uploadFile(event: any,singleFile:boolean) {
+    if(singleFile){
+      let formData = new FormData();
+      formData.append(this.paramname, event);
+      this.dataService.uploadFile(this.httpurl, this.httpmethod, formData).subscribe(
+        response=>{
+        this.responseData = response;
+        },
+        error=>{
+
+        },
+        ()=>{
+
         }
-        formData.append(this.paramname, fileList[i]);
+        );
+        this.uploadedFileName = event.name;
+    }else{
+      let fileList: FileList = event.target.files!=null?event.target.files:event;
+      let formData = new FormData();
+      if (fileList) {
+        for (let i = 0; i < fileList.length; i++) {
+          if (!this.paramname) {
+            this.paramname = "file";
+          }
+          formData.append(this.paramname, fileList[i]);
+        }
+        this.dataService.uploadFile(this.httpurl, this.httpmethod, formData).subscribe(
+          response=>{
+          this.responseData = response;
+          },
+          error=>{
+
+          },
+          ()=>{
+
+          }
+          );
+        if (fileList.length == 1) {
+          this.uploadedFileName = fileList[0].name;
+        } else if (fileList.length > 1) {
+          this.uploadedFileName = fileList.length + ' files';
+          // this.inpHandle.nativeElement.value = this.uploadedFileName;
+        }
       }
-      // this.commonHttpService.uploadFile(this, this.httpurl, this.httpmethod, formData);
-      if (fileList.length == 1) {
-        this.uploadedFileName = fileList[0].name;
-      } else if (fileList.length > 1) {
-        this.uploadedFileName = fileList.length + ' files';
-        // this.inpHandle.nativeElement.value = this.uploadedFileName;
-      }
+
     }
 
   }
 }
+
