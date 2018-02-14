105b846741cab12042a9209db5a63033a2a031f6
anaghak07 <anagha.kulkarni@metamagic.in>
2018-02-14 17:15:11 +0530

    styles : updating themes post file restructuring

---
+++ src/styles/sasstocss-temp.sh
@@ -0,0 +1,61 @@
+
+cp mda/search-icon.png css/;
+cp mda/slider.png css/;
+
+#sass mda/at-md-gold-amber.scss:css/at-md-gold-amber.css;
+
+sass mda/at-md-army-olive.scss:css/at-md-army-olive.css;
+sass mda/at-md-ash-stone-black.scss:css/at-md-ash-stone-black.css;
+sass mda/at-md-black.scss:css/at-md-black.css;
+sass mda/at-md-blue.scss:css/at-md-blue.css;
+
+sass mda/at-md-blue-grey.scss:css/at-md-blue-grey.css;
+#sass mda/at-md-gingerbread.scss:css/at-md-gingerbread.css;
+
+sass mda/at-md-cerise-magenta.scss:css/at-md-cerise-magenta.css;
+sass mda/at-md-charcoal-iron.scss:css/at-md-charcoal-iron.css;
+sass mda/at-md-chocolate-russet.scss:css/at-md-chocolate-russet.css;
+sass mda/at-md-fern-lime.scss:css/at-md-fern-lime.css;
+# sass mda/at-md-frenchrose-mulberry.scss:css/at-md-frenchrose-mulberry.css;
+# sass mda/at-md-grape-violet.scss:css/at-md-grape-violet.css;
+# sass mda/at-md-hot-pink-fuchsia.scss:css/at-md-hot-pink-fuchsia.css;
+
+# sass mda/at-md-indigo.scss:css/at-md-indigo.css;
+
+# sass mda/at-md-jungle-mint.scss:css/at-md-jungle-mint.css;
+
+# sass mda/at-md-maya.scss:css/at-md-maya.css;
+
+# sass mda/at-md-mahogany-barny-red.scss:css/at-md-mahogany-barny-red.css;
+# sass mda/at-md-navy-egyptian.scss:css/at-md-navy-egyptian.css;
+# sass mda/at-md-rasberry-sangria.scss:css/at-md-rasberry-sangria.css;
+# sass mda/at-md-red-maroon.scss:css/at-md-red-maroon.css;
+# sass mda/at-md-sacramento-jade.scss:css/at-md-sacramento-jade.css;
+
+# sass amexio/at-am-ocean-blue.scss:css/at-am-ocean-blue.css;
+# sass amexio/at-am-tiger.scss:css/at-am-tiger.css;
+# sass amexio/at-am-limegreen.scss:css/at-am-limegreen.css;
+# sass amexio/at-am-matrix.scss:css/at-am-matrix.css;
+# sass amexio/at-am-spicy-apricot.scss:css/at-am-spicy-apricot.css;
+# sass amexio/at-am-byzantine-crystal.scss:css/at-am-byzantine-crystal.css;
+# sass amexio/at-am-honey-prussian.scss:css/at-am-honey-prussian.css;
+# sass amexio/at-am-persian-laguna.scss:css/at-am-persian-laguna.css;
+
+# sass amexio/at-am-burgandy-magenta.scss:css/at-am-burgandy-magenta.css;
+# sass amexio/at-am-navy-grape.scss:css/at-am-navy-grape.css;
+# sass amexio/at-am-matrix-amber.scss:css/at-am-matrix-amber.css;
+# sass amexio/at-am-matrix-cream.scss:css/at-am-matrix-cream.css;
+# sass amexio/at-am-matrix-maya.scss:css/at-am-matrix-maya.css;
+# sass amexio/at-am-matrix-ultra.scss:css/at-am-matrix-ultra.css;
+# sass amexio/at-am-royal-kelly.scss:css/at-am-royal-kelly.css;
+# sass amexio/at-am-royal-pineapple.scss:css/at-am-royal-pineapple.css;
+
+# sass mda/at-md-tronbone-royal.scss:css/at-md-tronbone-royal.css;
+# sass mda/at-md-pineapple-flaxen.scss:css/at-md-pineapple-flaxen.css;
+# sass mda/at-md-pumpkin-fire.scss:css/at-md-pumpkin-fire.css;
+# sass mda/at-md-mauve-orchid.scss:css/at-md-mauve-orchid.css;
+# sass mda/at-md-honey-mustard.scss:css/at-md-honey-mustard.css;
+sass mda/at-md-bruntor-tangerine.scss:css/at-md-bruntor-tangerine.css;
+
+
+
