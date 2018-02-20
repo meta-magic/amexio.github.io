e23a15be2c006aab1b91ad2bdd62f01da2aa9816
datsgawas <dattaram.gawas@metamagic.in>
2018-02-20 16:34:42 +0530

    dialogue  : show dialogue.

--- src/module/panes/dialogue/dialogue.pane.component.ts
+++ src/module/panes/dialogue/dialogue.pane.component.ts
@@ -81,6 +81,8 @@ export class AmexiodialoguePaneComponent implements OnInit {
 
   @Output() actionStatus : EventEmitter<any> = new EventEmitter<any>();
 
+  @Output() close : EventEmitter<any> = new EventEmitter<any>();
+
   constructor() {
     this.closable = true;
     this.secondaryactionlabel  = 'Cancel';
@@ -96,6 +98,7 @@ export class AmexiodialoguePaneComponent implements OnInit {
   onCloseClick() {
     if (this.closable) {
       this.showdialogue  = !this.showdialogue ;
+      this.close.emit(this.showdialogue);
     }
   }
 
