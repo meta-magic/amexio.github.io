f35bd04d426d5175e2d41b24b6060db8f2e658ac
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-03-02 14:52:49 +0530

    script: copy to server

--- copytoserver.sh
+++ copytoserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Copy dist/ to Apache Server"
-cd dist && npm pack && mv amexio-ng-extensions-4.0.10.tgz amexio-ng-extensions.tgz
+cd dist && npm pack && mv amexio-ng-extensions-4.1.0.tgz amexio-ng-extensions.tgz
 cp -a amexio-ng-extensions.tgz /var/www/html/desireplatform/amexio-builds/
 
