cfbe701d1c7748398e0e3e63f6f1369a6d3a403d
anaghak07 <anagha.kulkarni@metamagic.in>
2018-03-15 17:19:11 +0530

    updated and added new mda themes

--- src/styles/sasstocss.sh
+++ src/styles/sasstocss.sh
@@ -1,88 +1,94 @@
+#!/bin/bash
 
-cp mda/search-icon.png css/;
-cp mda/slider.png css/;
+#  Copyright (c) 2018 MetaMagic Global Inc, NJ, USA 
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+#
+#  This program and the accompanying materials are licensed under
+#  the terms of the Apache 2 License version 2.0 as published by the Apache
+#  Software Foundation.
 
-sass mda/at-md-gold-amber.scss:css/at-md-gold-amber.css;
+
 
 sass mda/at-md-army-olive.scss:css/at-md-army-olive.css;
 sass mda/at-md-ash-stone-black.scss:css/at-md-ash-stone-black.css;
+
 sass mda/at-md-black.scss:css/at-md-black.css;
 sass mda/at-md-blue.scss:css/at-md-blue.css;
-
 sass mda/at-md-blue-grey.scss:css/at-md-blue-grey.css;
-sass mda/at-md-gingerbread.scss:css/at-md-gingerbread.css;
-
+sass mda/at-md-bruntor-tangerine.scss:css/at-md-bruntor-tangerine.css;
 sass mda/at-md-cerise-magenta.scss:css/at-md-cerise-magenta.css;
+
 sass mda/at-md-charcoal-iron.scss:css/at-md-charcoal-iron.css;
 sass mda/at-md-chocolate-russet.scss:css/at-md-chocolate-russet.css;
-sass mda/at-md-fern-lime.scss:css/at-md-fern-lime.css;
-sass mda/at-md-frenchrose-mulberry.scss:css/at-md-frenchrose-mulberry.css;
-sass mda/at-md-grape-violet.scss:css/at-md-grape-violet.css;
-sass mda/at-md-hot-pink-fuchsia.scss:css/at-md-hot-pink-fuchsia.css;
-
-sass mda/at-md-indigo.scss:css/at-md-indigo.css;
-
-sass mda/at-md-jungle-mint.scss:css/at-md-jungle-mint.css;
-
-sass mda/at-md-maya.scss:css/at-md-maya.css;
-
-sass mda/at-md-mahogany-barny-red.scss:css/at-md-mahogany-barny-red.css;
-sass mda/at-md-navy-egyptian.scss:css/at-md-navy-egyptian.css;
-sass mda/at-md-rasberry-sangria.scss:css/at-md-rasberry-sangria.css;
-sass mda/at-md-red-maroon.scss:css/at-md-red-maroon.css;
-sass mda/at-md-sacramento-jade.scss:css/at-md-sacramento-jade.css;
-
-sass amexio/at-am-ocean-blue.scss:css/at-am-ocean-blue.css;
-sass amexio/at-am-tiger.scss:css/at-am-tiger.css;
-sass amexio/at-am-limegreen.scss:css/at-am-limegreen.css;
-sass amexio/at-am-matrix.scss:css/at-am-matrix.css;
-sass amexio/at-am-spicy-apricot.scss:css/at-am-spicy-apricot.css;
-sass amexio/at-am-byzantine-crystal.scss:css/at-am-byzantine-crystal.css;
-sass amexio/at-am-honey-prussian.scss:css/at-am-honey-prussian.css;
-sass amexio/at-am-persian-laguna.scss:css/at-am-persian-laguna.css;
-
-sass amexio/at-am-burgandy-magenta.scss:css/at-am-burgandy-magenta.css;
-sass amexio/at-am-navy-grape.scss:css/at-am-navy-grape.css;
-sass amexio/at-am-matrix-amber.scss:css/at-am-matrix-amber.css;
-sass amexio/at-am-matrix-cream.scss:css/at-am-matrix-cream.css;
-sass amexio/at-am-matrix-maya.scss:css/at-am-matrix-maya.css;
-sass amexio/at-am-matrix-ultra.scss:css/at-am-matrix-ultra.css;
-sass amexio/at-am-royal-kelly.scss:css/at-am-royal-kelly.css;
-sass amexio/at-am-royal-pineapple.scss:css/at-am-royal-pineapple.css;
-
-sass mda/at-md-tronbone-royal.scss:css/at-md-tronbone-royal.css;
-sass mda/at-md-pineapple-flaxen.scss:css/at-md-pineapple-flaxen.css;
-sass mda/at-md-pumpkin-fire.scss:css/at-md-pumpkin-fire.css;
-sass mda/at-md-mauve-orchid.scss:css/at-md-mauve-orchid.css;
-sass mda/at-md-honey-mustard.scss:css/at-md-honey-mustard.css;
-sass mda/at-md-bruntor-tangerine.scss:css/at-md-bruntor-tangerine.css;
-
-
-sass mda/at-md-hot-pink-violetred.scss:css/at-md-hot-pink-violetred.css;
-sass mda/at-md-orangered-tomato.scss:css/at-md-orangered-tomato.css;
-sass mda/at-md-rebeccapurple-orchid.scss:css/at-md-rebeccapurple-orchid.css;
-sass mda/at-md-indigo-darkviolet.scss:css/at-md-indigo-darkviolet.css;
-sass mda/at-md-indigo-slateblue.scss:css/at-md-indigo-slateblue.css;
+sass mda/at-md-darkgreen-darkolivegreen.scss:css/at-md-darkgreen-darkolivegreen.css;
 sass mda/at-md-darkgreen-teal.scss:css/at-md-darkgreen-teal.css;
-
-
-sass mda/at-md-royalblue-dodgerblue.scss:css/at-md-royalblue-dodgerblue.css;
-sass mda/at-md-mediumblue-slateblue.scss:css/at-md-mediumblue-slateblue.css;
-sass mda/at-md-sienna-rosybrown.scss:css/at-md-sienna-rosybrown.css;
-sass mda/at-md-darkslategray-dimgray.scss:css/at-md-darkslategray-dimgray.css;
-sass mda/at-md-mediumblue-deepskyblue.scss:css/at-md-mediumblue-deepskyblue.css;
-sass mda/at-md-darkturquoise-aquamarine.scss:css/at-md-darkturquoise-aquamarine.css;
-
-
 sass mda/at-md-darkkhaki-moccasin.scss:css/at-md-darkkhaki-moccasin.css;
-sass mda/at-md-indianred-salmon.scss:css/at-md-indianred-salmon.css;
 
 sass mda/at-md-darkmagenta-darkorchid.scss:css/at-md-darkmagenta-darkorchid.css;
 sass mda/at-md-darkslateblue-indigo.scss:css/at-md-darkslateblue-indigo.css;
-sass mda/at-md-darkgreen-darkolivegreen.scss:css/at-md-darkgreen-darkolivegreen.css;
-sass mda/at-md-navy-royalblue.scss:css/at-md-navy-royalblue.css;
-sass mda/at-md-saddlebrown-peru.scss:css/at-md-saddlebrown-peru.css;
-sass mda/at-md-mediumvioletred-hotpink.scss:css/at-md-mediumvioletred-hotpink.css;
-sass mda/at-md-firebrick-crimson.scss:css/at-md-firebrick-crimson.css;
+sass mda/at-md-darkslategray-dimgray.scss:css/at-md-darkslategray-dimgray.css;
+sass mda/at-md-darkturquoise-aquamarine.scss:css/at-md-darkturquoise-aquamarine.css;
+sass mda/at-md-fern-lime.scss:css/at-md-fern-lime.css;
 
+sass mda/at-md-firebrick-crimson.scss:css/at-md-firebrick-crimson.css;
+sass mda/at-md-frenchrose-mulberry.scss:css/at-md-frenchrose-mulberry.css;
+sass mda/at-md-gingerbread.scss:css/at-md-gingerbread.css;
+sass mda/at-md-gold-amber.scss:css/at-md-gold-amber.css;
+sass mda/at-md-grape-violet.scss:css/at-md-grape-violet.css;
+
+sass mda/at-md-honey-mustard.scss:css/at-md-honey-mustard.css;
+sass mda/at-md-hot-pink-fuchsia.scss:css/at-md-hot-pink-fuchsia.css;
+sass mda/at-md-hot-pink-violetred.scss:css/at-md-hot-pink-violetred.css;
+sass mda/at-md-indianred-salmon.scss:css/at-md-indianred-salmon.css;
+sass mda/at-md-indigo.scss:css/at-md-indigo.css;
+
+sass mda/at-md-indigo-darkviolet.scss:css/at-md-indigo-darkviolet.css;
+sass mda/at-md-indigo-slateblue.scss:css/at-md-indigo-slateblue.css;
+sass mda/at-md-jungle-mint.scss:css/at-md-jungle-mint.css;
+sass mda/at-md-mahogany-barny-red.scss:css/at-md-mahogany-barny-red.css;
+sass mda/at-md-mauve-orchid.scss:css/at-md-mauve-orchid.css;
+
+sass mda/at-md-maya.scss:css/at-md-maya.css;
+sass mda/at-md-mediumblue-deepskyblue.scss:css/at-md-mediumblue-deepskyblue.css;
+sass mda/at-md-mediumblue-slateblue.scss:css/at-md-mediumblue-slateblue.css;
+sass mda/at-md-mediumvioletred-hotpink.scss:css/at-md-mediumvioletred-hotpink.css;
+sass mda/at-md-navy-egyptian.scss:css/at-md-navy-egyptian.css;
+
+sass mda/at-md-navy-royalblue.scss:css/at-md-navy-royalblue.css;
+sass mda/at-md-orangered-tomato.scss:css/at-md-orangered-tomato.css;
+sass mda/at-md-pineapple-flaxen.scss:css/at-md-pineapple-flaxen.css;
+sass mda/at-md-pumpkin-fire.scss:css/at-md-pumpkin-fire.css;
+sass mda/at-md-rasberry-sangria.scss:css/at-md-rasberry-sangria.css;
+
+sass mda/at-md-rebeccapurple-orchid.scss:css/at-md-rebeccapurple-orchid.css;
+sass mda/at-md-red-maroon.scss:css/at-md-red-maroon.css;
+sass mda/at-md-royalblue-dodgerblue.scss:css/at-md-royalblue-dodgerblue.css;
+sass mda/at-md-sacramento-jade.scss:css/at-md-sacramento-jade.css;
+sass mda/at-md-saddlebrown-peru.scss:css/at-md-saddlebrown-peru.css;
+
+sass mda/at-md-sienna-rosybrown.scss:css/at-md-sienna-rosybrown.css;
+sass mda/at-md-tronbone-royal.scss:css/at-md-tronbone-royal.css;
 sass mda/at-md-yale-azure.scss:css/at-md-yale-azure.css;
+sass mda/at-md-z-flag-bahamas.scss:css/at-md-z-flag-bahamas.css;
+sass mda/at-md-z-flag-france.scss:css/at-md-z-flag-france.css;
+
+sass mda/at-md-z-flag-germany.scss:css/at-md-z-flag-germany.css;
+sass mda/at-md-z-flag-india.scss:css/at-md-z-flag-india.css;
+sass mda/at-md-z-flag-italy.scss:css/at-md-z-flag-italy.css;
+sass mda/at-md-z-flag-japan.scss:css/at-md-z-flag-japan.css;
+sass mda/at-md-z-flag-usa.scss:css/at-md-z-flag-usa.css;
+
