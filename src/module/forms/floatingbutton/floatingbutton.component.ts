fe84e1f21c26efea1b01e223a9b3ea3d1cab2150
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-03-21 16:52:23 +0530

    Floating :Two way binding issue resolved in floatin g button

--- src/module/forms/floatingbutton/floatingbutton.component.ts
+++ src/module/forms/floatingbutton/floatingbutton.component.ts
@@ -2,6 +2,8 @@
  * Created by ketangote on 26/2/2018.
  */
 import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'amexio-floating-button', templateUrl: './floatingbutton.component.html'
@@ -9,9 +11,9 @@ import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
 export class AmexioFloatingButtonComponent implements OnInit{
 
   @Input('vertical-position') verticalposition : string;
-  
+
   @Input('horizontal-position') horizontalposition : string;
-  
+
   @Input('position-top') top : string;
 
   @Input('position-bottom') bottom : string;
@@ -38,75 +40,78 @@ export class AmexioFloatingButtonComponent implements OnInit{
 
   absoluteposition : boolean = false;
 
+
   constructor() {
 
   }
-
   ngOnInit()
   {
-  
-      if(this.top || this.bottom || this.right || this.left){
-        this.absoluteposition = true;
-      }  
-
-      this.positionclass = ""
-
-      if(this.relative){
-        this.positionclass = " floatingbutton-relative "
-      }else{
-        this.positionclass = " floatingbutton-fixed "
-      }
-
-      if(!this.absoluteposition && !this.relative){
-        if (this.verticalposition == null) {
-            this.verticalposition = 'top';
-          } else if (this.horizontalposition == null) {
-            this.horizontalposition = 'right'
-          }
-          this.positionclass = this.positionclass+ " floatingbutton-" + this.verticalposition + " floatingbutton-" + this.horizontalposition+" floatingbutton-default";
-      }  
-      if(this.block === "circle"){
-          this.positionclass = this.positionclass +" floatingbutton-circle";
-          this.label="";
-      }else if(this.block === "square")
-      {
-        this.positionclass = this.positionclass + " floatingbutton-square";
-      }
-
-      if(this.type === "theme-color"){
-        if(this.disabled)
-            this.positionclass = this.positionclass  + " floatingbutton-theme-color-disabled"
-        else
-            this.positionclass = this.positionclass + " floatingbutton-theme-color";
-      }
-      else if( this.type === "green"){
-        if(this.disabled)
-            this.positionclass = this.positionclass  + " floatingbutton-green-disabled"
-        else
-            this.positionclass = this.positionclass + " floatingbutton-green";
-      }else if(this.type === "red"){
-        if(this.disabled)
-            this.positionclass = this.positionclass  + " floatingbutton-red-disabled"
-        else
-            this.positionclass = this.positionclass + " floatingbutton-red";
-      }else if(this.type === "yellow"){
-        if(this.disabled)
-            this.positionclass = this.positionclass  + " floatingbutton-yellow-disabled"
-        else
-            this.positionclass = this.positionclass + " floatingbutton-yellow";
-      }else{
-        if(this.disabled)
-            this.positionclass = this.positionclass  + " floatingbutton-default-disabled"
-        else
-            this.positionclass = this.positionclass + " floatingbutton-default";
-      }
-      
+  this.updateModel();
   }
+// THIS METHOD IS USED FOR SETTING CSS CLASSSES
+  updateModel():any{
 
+    if(this.top || this.bottom || this.right || this.left){
+      this.absoluteposition = true;
+    }
+
+    this.positionclass = ""
+
+    if(this.relative){
+      this.positionclass = " floatingbutton-relative "
+    }else{
+      this.positionclass = " floatingbutton-fixed "
+    }
+
+    if(!this.absoluteposition && !this.relative){
+      if (this.verticalposition == null) {
+          this.verticalposition = 'top';
+        } else if (this.horizontalposition == null) {
+          this.horizontalposition = 'right'
+        }
+        this.positionclass = this.positionclass+ " floatingbutton-" + this.verticalposition + " floatingbutton-" + this.horizontalposition+" floatingbutton-default";
+    }
+    if(this.block === "circle"){
+        this.positionclass = this.positionclass +" floatingbutton-circle";
+        this.label="";
+    }else if(this.block === "square")
+    {
+      this.positionclass = this.positionclass + " floatingbutton-square";
+    }
+
+    if(this.type === "theme-color"){
+      if(this.disabled)
+          this.positionclass = this.positionclass  + " floatingbutton-theme-color-disabled"
+      else
+          this.positionclass = this.positionclass + " floatingbutton-theme-color";
+    }
+    else if( this.type === "green"){
+      if(this.disabled)
+          this.positionclass = this.positionclass  + " floatingbutton-green-disabled"
+      else
+          this.positionclass = this.positionclass + " floatingbutton-green";
+    }else if(this.type === "red"){
+      if(this.disabled)
+          this.positionclass = this.positionclass  + " floatingbutton-red-disabled"
+      else
+          this.positionclass = this.positionclass + " floatingbutton-red";
+    }else if(this.type === "yellow"){
+      if(this.disabled)
+          this.positionclass = this.positionclass  + " floatingbutton-yellow-disabled"
+      else
+          this.positionclass = this.positionclass + " floatingbutton-yellow";
+    }else{
+      if(this.disabled)
+          this.positionclass = this.positionclass  + " floatingbutton-default-disabled"
+      else
+          this.positionclass = this.positionclass + " floatingbutton-default";
+    }
+    return this.positionclass;
+  }
   buttonClick(event:any){
       if(!this.disabled){
         this.onClick.emit({'this':this, 'event':event});
       }
-    
+
   }
 }
