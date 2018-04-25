b6ab7ede818aae8285322077efc1640f0472f536
Dattaram Gawas <dattaram.gawas@metamagic.in>
2018-04-25 16:16:52 +0530

    Update textinput.component.ts
    Error hightlighter issue fixed.

--- src/module/forms/textinput/textinput.component.ts
+++ src/module/forms/textinput/textinput.component.ts
@@ -334,9 +334,14 @@ description : On field value change event
     let classObj;
     if (!this.allowblank) {
       if (this.innerValue == null || this.innerValue == '') {
-        classObj = {'input-control-error': true};
-        this.isValid = false;
-        this.isComponentValid = false;
+         if(inp.touched) {
+          classObj = {'input-control-error': true};
+          this.isValid = false;
+          this.isComponentValid = false;
+        } else {
+          this.isValid = false;
+          this.isComponentValid = false;
+        }
       }else if (inp.touched && !this.allowblank && (this.value == '' || this.value == null)) {
         classObj = {'input-control-error': true};
         this.isValid = false;
