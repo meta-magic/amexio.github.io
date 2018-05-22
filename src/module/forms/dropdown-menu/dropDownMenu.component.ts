497c94897646a3b6bb06801a69d2199140644dfc
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-05-22 18:57:01 +0530

    Dropdown Menu: Updated

--- src/module/forms/dropdown-menu/dropDownMenu.component.ts
+++ src/module/forms/dropdown-menu/dropDownMenu.component.ts
@@ -3,7 +3,7 @@ import {
   AfterContentInit
 } from '@angular/core';
 
-import { AmexioDropDownOptionComponent } from '../component/dropDownMenu.component.options'
+import { AmexioDropDownOptionComponent } from './dropDownMenu.component.options'
 
 
 @Component({
@@ -80,7 +80,6 @@ import { AmexioDropDownOptionComponent } from '../component/dropDownMenu.compone
 export class AmexioDropDownMenuComponent implements AfterContentInit {
 
   dropDownPreviewData : any;
-  flag: boo;
 
   /* for internal use*/
   @Input() dropDownMenuLocalData: any;
