36242d983bc88e98b7dc50f5ad3155d6fb3af589
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-05-30 21:12:30 +0530

    DataGrid: Global Filter Added

--- src/module/data/datagrid/data.grid.column.ts
+++ src/module/data/datagrid/data.grid.column.ts
@@ -10,7 +10,7 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Author - Ketan Gote, Pratik Kelwalkar, Dattaram Gawas
+ * Author - Ketan Gote, Pratik Kelwalkar, Dattaram Gawas, Rashmi Thakkar
  *
  */
 
