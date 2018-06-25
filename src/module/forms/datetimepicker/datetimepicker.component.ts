ddab18898e3734d6be26754bc5e5e974760c6211
Manisha1205 <manisha.boddu@metamagic.in>
2018-06-25 19:31:15 +0530

    datetimepicker: inline datepicker added

--- src/module/forms/datetimepicker/datetimepicker.component.ts
+++ src/module/forms/datetimepicker/datetimepicker.component.ts
@@ -117,6 +117,16 @@ default : none
 description : sets inline calender
 */  
   @Input('inline-datepicker') inlineDatepicker: boolean = false;
+/*
+Properties
+name : dropdown-datepicker
+datatype :  boolean
+version : 4.2 onwards
+default : none
+description : sets dropdown datepicker
+*/  
+@Input('dropdown-datepicker') dropdownDatepicker: boolean = true;  
+  
   /*
   Properties
   name : required
@@ -182,6 +192,9 @@ description : On field focus event
 
 
   constructor(public element: ElementRef) {
+    if(this.inlineDatepicker){
+      this.showToolTip=true;
+    }
     this.minDate = "";
     this.maxDate = "";
     
@@ -199,7 +212,9 @@ description : On field focus event
 
 
   ngOnInit() {
-
+    if(this.inlineDatepicker){
+      this.showToolTip=true;
+    }
     this.isComponentValid = !this.required;
     if (this.dateformat != null) {
       this.dateformat = "dd/MM/yyyy";
@@ -263,7 +278,12 @@ description : On field focus event
     this.dateModel = this.selectedDate;
     this.value = this.selectedDate;
     this.isComponentValid = true;
+    if(this.inlineDatepicker){
+      this.showToolTip=true;
+    }
+    else{
     this.showToolTip = !this.showToolTip;
+    }
   }
 
   resetSelection(dateObj: any) {
@@ -317,23 +337,24 @@ description : On field focus event
     this.setDateData1("minus", 12, event);
   }
   //this function validates month
-  setDateData(state: string, mon: number, event: any) {  
+  setDateData(state: string, mon: number, event: any) { 
     let d = new Date(this.currrentDate.getFullYear(), this.currrentDate.getMonth(), this.currrentDate.getDate());
     let min = new Date(this.minDate);
     let max = new Date(this.maxDate);
     //checks if selected date is within maximum range of month
     if (state === "plus") {
-      
+     
       if (this.maxDate.length > 0) {
-         
+        
         if (d.getFullYear() == max.getFullYear()) {
-          
+         
           if (d.getMonth() == max.getMonth()) {
 
             // event.stopPropagation();
           }
           else {
-                       //*********check here******************* */
+          
+            //*********check here******************* */
             //logic to chk if year is valid
             if(d.getFullYear()<=max.getFullYear()){
               if(d.getMonth()<=max.getMonth()){
@@ -356,26 +377,27 @@ description : On field focus event
     }
     //checks if selected date is within minimum range of month
     else if (state === "minus") {
-      
-      if (this.minDate.length > 0) {
        
+      if (this.minDate.length > 0) {
+        
         if (d.getFullYear() == min.getFullYear()) {
-                  if (d.getMonth() == min.getMonth()) {
+         
+          if (d.getMonth() == min.getMonth()) {
             // event.stopPropagation();
           }
           else { //logic to chk if year is valid
-         
+          
            if(d.getFullYear()>=min.getFullYear()){
-             
+           
              if(d.getMonth()>=min.getMonth()){
-                
+            
               d.setMonth(d.getMonth() - mon);
              }
            }
            
           }
         } else {
-           
+          
           d.setMonth(d.getMonth() - mon);
         }
       }
@@ -561,7 +583,13 @@ description : On field focus event
   }
 
   openPicker(elem: any) {
-    this.showToolTip = true;
+    if(this.inlineDatepicker){
+      this.showToolTip=this.inlineDatepicker;
+    }
+    else{
+this.showToolTip=true;
+    }
+    //this.showToolTip = true;
     this.posixUp = this.getListPosition(elem);
 
   }
@@ -669,5 +697,8 @@ description : On field focus event
       });
     });
   }
+
+ 
+ 
 
 }
