7e0d0105b3734e67e7de0bedb04792a78b9994f4
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-07-11 17:05:43 +0530

    Toolbar: lint updated

--- src/module/forms/toolbar/toolbar.component.ts
+++ src/module/forms/toolbar/toolbar.component.ts
@@ -1,87 +1,73 @@
-import { Component, OnInit, Input,ContentChildren,QueryList,Output,EventEmitter,ElementRef,ViewChild,} from '@angular/core';
-import{ToolbaroneComponent } from './toolbarone.component';
-import {ToolBarActionComponent} from './toolbaraction.component';
+import { AfterContentInit, Component, ContentChildren, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChild } from '@angular/core';
+import { ToolBarActionComponent } from './toolbaraction.component';
+import { ToolbaroneComponent } from './toolbarone.component';
 
 @Component({
   selector: 'amexio-toolbar',
   templateUrl: `./toolbar.component.html`,
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements AfterContentInit, OnInit {
 
- 
-/*
+  /*
 Properties
-name :seperator
+name : seperator
 datatype :boolean
 version : 4.2onwards
-default : 
+default :
 description : This will seperate the toolbar.
 */
-@Input("seperator") seperator:boolean;
-/*
-Properties
-name :toolbarposition
-datatype :string
-version : 4.0 onwards
-default : 
-description : This will allign the toolbar.
-*/
-@Input("toolbar-position") toolbarposition: string;
-/*
-Events
-name : onClick
-datatype : none
-version : none
-default : none
-description : Callback to invoke on activated tab event.
-*/
-@Output() onClick: any = new EventEmitter<any>();
-@Input() tabLocalData: any;
-tabPreviewData: any;
+  @Input('seperator') seperator: boolean;
+  /*
+  Properties
+  name :toolbarposition
+  datatype :string
+  version : 4.0 onwards
+  default :
+  description : This will allign the toolbar.
+  */
+  @Input('toolbar-position') toolbarposition: string;
+  /*
+  Events
+  name : onClick
+  datatype : none
+  version : none
+  default : none
+  description : Callback to invoke on activated tab event.
+  */
+  @Output() onClick: any = new EventEmitter<any>();
+  @Input() tabLocalData: any;
+  tabPreviewData: any;
 
-  constructor() { 
-   // this.justifycontent= "";
-    this.toolbarposition="top";
-    this.seperator=false;
-   
-    
+  constructor() {
+    this.toolbarposition = 'top';
+    this.seperator = false;
   }
 
   @ContentChildren(ToolbaroneComponent) queryTool: QueryList<ToolbaroneComponent>;
   toolCollection: ToolbaroneComponent[];
 
   ngOnInit() {
-  
   }
-   
-  
+
   ngAfterContentInit() {
-     // this.toolCollection=this.queryTo;
-      this.toolCollection = this.queryTool.toArray();
-      for (let i = 0; i < this.toolCollection.length; i++) {
-        this.toolCollection[i]['position'] =this.getToolbaritemposition(this.toolCollection[i]['position']);
-      }
-      // this.toolCollection.forEach((object)=>{
-      //   if(object){
-      //     let aa=object.getToolbaritemposition();
-      //     console.log('COMPOENNT'+aa);
-      //   }
-      // });
-  
+    this.toolCollection = this.queryTool.toArray();
+    for (let i = 0; i < this.toolCollection.length; i++) {
+      this.toolCollection[i]['position'] = this.getToolbaritemposition(this.toolCollection[i]['position']);
+    }
   }
-  getToolbaritemposition(position:any):any{
-  
-    if(position== 'right'){
-      return 'main-right';  
+  getToolbaritemposition(position: any): any {
+
+    if (position === 'right') {
+      return 'main-right';
     }
-    if(position == 'left'){
-      return 'main-left';  
+    if (position === 'left') {
+      return 'main-left';
     }
-    if(position == 'center'){
-      return 'main-center';  
+    if (position === 'center') {
+      return 'main-center';
     }
-    if(position == ''){
-      return 'main-center';  
+    if (position === '') {
+      return 'main-center';
     }
   }
   onToolClick(tool: any) {
@@ -93,32 +79,29 @@ tabPreviewData: any;
         } else {
           this.toolCollection[i]['active'] = false;
         }
-        // this.toolCollection[i]['position'] =this.getToolbaritemposition(this.toolCollection[i]['position']);
       }
     }
   }
 
   // THIS METHOD IS  FOR APPLIED SPERATOR CLASS
-  getSeperatotClass(toolnode:any):any{
-  let cssName:string=' ';
-    if(this.seperator){
-      cssName='seperator-line ';
+  getSeperatotClass(toolnode: any): any {
+    let cssName: string = '';
+    if (this.seperator) {
+      cssName = 'seperator-line';
     }
-    cssName=cssName +toolnode.position;
+    cssName = cssName + toolnode.position;
     return cssName;
   }
 
-  getToolbarPosition(){
-    if(this.toolbarposition=='top'){
+  getToolbarPosition() {
+    if (this.toolbarposition === 'top') {
       return '';
     }
-    if(this.toolbarposition=='right'){
+    if (this.toolbarposition === 'right') {
       return '';
     }
-    if(this.toolbarposition=='bottom'){
+    if (this.toolbarposition === 'bottom') {
       return '';
     }
   }
-
-
 }
--- src/module/panes/amexio.pane.module.ts
+++ src/module/panes/amexio.pane.module.ts
@@ -1,64 +1,61 @@
 /**
  * Created by pratik on 16/1/18.
  */
-import {ModuleWithProviders, NgModule} from "@angular/core";
-import {CommonDataService} from "../services/data/common.data.service";
-import {DeviceQueryService} from "../services/device/device.query.service";
-import {CommonModule} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AmexioHeaderComponent} from "./header/pane.action.header";
-import {AmexioFooterComponent} from "./action/pane.action.footer";
-import {AmexioBodyComponent} from "./body/pane.action.body";
-import {AmexioFieldSetComponent} from "./fieldset/fieldset.component";
-import {AmexioWindowPaneComponent} from "./window/window.pane.component";
-import {AmexioStepsComponent} from "./steps/steps.component";
-import {AmexioTabComponent} from "./tab/tab.component";
-import {AmexioTabPill} from "./tab/tab.pill.component";
-import {AmexioVerticalTabComponent} from "./tab/vertical-tab/vertical.tab.component";
-import {AmexioRightVerticalTabComponent} from "./tab/right-vertical-tab/right.vertical.component";
-import {AmexioCarouselComponent} from "./carousel/carousel.component";
-import {AmexioTemplate, AmexioTemplateWrapper} from "./carousel/carousel.template";
-import {StepBlockComponent} from "./steps/step-block";
-import {IconLoaderService} from "../services/icon/icon.service";
-import {AmexioIconPane} from "./icon/icon.component";
-import {AmexiodialoguePaneComponent} from "./dialogue/dialogue.pane.component";
-import {AmexioFormsModule} from "../forms/amexio.forms.module";
-import {AmexioPanelComponent} from "./panel/panel.component";
-import {HttpClientModule} from "@angular/common/http";
-import {AmexioFormComponent} from "./form/form.component"
-import { AmexioFormActionComponent } from "./form/form.action.component";
-import { AmexioFormBodyComponent } from "./form/form.body.component";
-import { AmexioFormHeaderComponent } from "./form/form.header.component";
-import {AmexioTabActionComponent} from "./tab/tab.action";
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-export * from './header/pane.action.header';
+import {AmexioFooterComponent} from './action/pane.action.footer';
+import {AmexioBodyComponent} from './body/pane.action.body';
+import {AmexioCarouselComponent} from './carousel/carousel.component';
+import {AmexioTemplate, AmexioTemplateWrapper} from './carousel/carousel.template';
+import {AmexiodialoguePaneComponent} from './dialogue/dialogue.pane.component';
+import {AmexioFieldSetComponent} from './fieldset/fieldset.component';
+import { AmexioFormActionComponent } from './form/form.action.component';
+import { AmexioFormBodyComponent } from './form/form.body.component';
+import {AmexioFormComponent} from './form/form.component';
+import { AmexioFormHeaderComponent } from './form/form.header.component';
+import {AmexioHeaderComponent} from './header/pane.action.header';
+import {AmexioIconPane} from './icon/icon.component';
+import {AmexioPanelComponent} from './panel/panel.component';
+import {StepBlockComponent} from './steps/step-block';
+import {AmexioStepsComponent} from './steps/steps.component';
+import {AmexioRightVerticalTabComponent} from './tab/right-vertical-tab/right.vertical.component';
+import {AmexioTabActionComponent} from './tab/tab.action';
+import {AmexioTabComponent} from './tab/tab.component';
+import {AmexioTabPill} from './tab/tab.pill.component';
+import {AmexioVerticalTabComponent} from './tab/vertical-tab/vertical.tab.component';
+import {AmexioWindowPaneComponent} from './window/window.pane.component';
+
+import {CommonDataService} from '../services/data/common.data.service';
+import {DeviceQueryService} from '../services/device/device.query.service';
+import {IconLoaderService} from '../services/icon/icon.service';
+
+import {AmexioFormsModule} from '../forms/amexio.forms.module';
+
 export * from './action/pane.action.footer';
 export * from './body/pane.action.body';
-
-export * from './fieldset/fieldset.component';
-
-export * from './window/window.pane.component';
-export * from './dialogue/dialogue.pane.component';
-
-export * from './steps/steps.component';
-export * from './steps/step-block';
-
-export * from './tab/tab.pill.component';
-export * from './tab/tab.component';
-export * from './tab/right-vertical-tab/right.vertical.component';
-export * from './tab/vertical-tab/vertical.tab.component';
-export * from './tab/vertical-tab/vertical.tab.component';
-export * from './tab/tab.action';
-
 export * from './carousel/carousel.component';
 export * from './carousel/carousel.template';
-export * from '../services/icon/icon.service';
-
-export * from './form/form.component';
+export * from './dialogue/dialogue.pane.component';
+export * from './fieldset/fieldset.component';
 export * from './form/form.action.component';
-export * from './form/form.header.component';
 export * from './form/form.body.component';
+export * from './form/form.component';
+export * from './form/form.header.component';
+export * from './header/pane.action.header';
+export * from './steps/steps.component';
+export * from './steps/step-block';
+export * from './tab/right-vertical-tab/right.vertical.component';
+export * from './tab/tab.action';
+export * from './tab/tab.component';
+export * from './tab/tab.pill.component';
+export * from './tab/vertical-tab/vertical.tab.component';
+export * from './window/window.pane.component';
+
+export * from '../services/icon/icon.service';
 
 const PANE_COMPONENTS = [
   AmexioHeaderComponent,
@@ -82,7 +79,7 @@ const PANE_COMPONENTS = [
   AmexioFormActionComponent,
   AmexioFormHeaderComponent,
   AmexioFormBodyComponent,
-  AmexioTabActionComponent
+  AmexioTabActionComponent,
 ];
 
 @NgModule({
@@ -90,18 +87,18 @@ const PANE_COMPONENTS = [
     CommonModule,
     FormsModule,
     AmexioFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  entryComponents:[AmexioTabPill],
+  entryComponents: [AmexioTabPill],
   exports: PANE_COMPONENTS,
   declarations: PANE_COMPONENTS,
-  providers : [CommonDataService,DeviceQueryService,IconLoaderService]
+  providers : [CommonDataService, DeviceQueryService, IconLoaderService],
 })
 export class AmexioPaneModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: AmexioPaneModule,
-      providers: [CommonDataService,DeviceQueryService,IconLoaderService]
+      providers: [CommonDataService, DeviceQueryService, IconLoaderService],
     };
   }
 }
