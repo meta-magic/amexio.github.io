33f14b7338530f1570237e3dd9a6ec446317527d
Dattaram Gawas <dattaram.gawas@metamagic.in>
2018-07-11 13:33:23 +0530

    datareader issue fixed.

--- src/module/data/tree/filter.tree.component.ts
+++ src/module/data/tree/filter.tree.component.ts
@@ -297,9 +297,13 @@ description : it will search for text relevant to entered character
 
   getData(httpResponse: any) {
     let responsedata: any = httpResponse;
-    let dr = this.datareader.split('.');
-    for (let ir = 0; ir < dr.length; ir++) {
-      responsedata = responsedata[dr[ir]];
+    if (this.datareader != null) {
+      let dr = this.datareader.split('.');
+      for (let ir = 0; ir < dr.length; ir++) {
+        responsedata = responsedata[dr[ir]];
+      }
+    } else {
+      responsedata = httpResponse;
     }
     return responsedata;
   }
