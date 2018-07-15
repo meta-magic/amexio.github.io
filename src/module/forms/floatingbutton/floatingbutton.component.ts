88dd602e517df079e8adc081db1cbef4f4495a5a
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-07-15 13:53:30 +0530

    Floating btn: Lint chnages

--- src/module/forms/floatingbutton/floatingbutton.component.ts
+++ src/module/forms/floatingbutton/floatingbutton.component.ts
@@ -1,138 +1,138 @@
 /**
  * Created by ketangote on 26/2/2018.
  */
- /*
- Component Name : Amexio Floating-Button
- Component Selector :  <amexio-floating-button>
- Component Description : A Floating button component with various position
- options (vertical/horizontal/top/bottom/left/right)
+/*
+Component Name : Amexio Floating-Button
+Component Selector :  <amexio-floating-button>
+Component Description : A Floating button component with various position
+options (vertical/horizontal/top/bottom/left/right)
 */
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 @Component({
   selector: 'amexio-floating-button',
   templateUrl: './floatingbutton.component.html',
 })
 export class AmexioFloatingButtonComponent implements OnInit {
-/*
-Properties
-name : vertical-position
-datatype : string
-version : 4.1 onwards
-default : none
-description : Postion floating button vertically: top or bottom or center.
-This attribute is ignored if user specify position explicitly
-(using position-top/position-bottom/position-left/position-right)
-*/
+  /*
+  Properties
+  name : vertical-position
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : Postion floating button vertically: top or bottom or center.
+  This attribute is ignored if user specify position explicitly
+  (using position-top/position-bottom/position-left/position-right)
+  */
   @Input('vertical-position') verticalposition: string;
-/*
-Properties
-name : horizontal-position
-datatype : none
-version : 4.1 onwards
-default : none
-description : Postion floating button horizontally: left or right or center.
-This attribute is ignored if user specify position explicitly
-(using position-top/position-bottom/position-left/position-right)
-*/
+  /*
+  Properties
+  name : horizontal-position
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Postion floating button horizontally: left or right or center.
+  This attribute is ignored if user specify position explicitly
+  (using position-top/position-bottom/position-left/position-right)
+  */
   @Input('horizontal-position') horizontalposition: string;
-/*
-Properties
-name : position-top
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes top position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-top
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes top position in percentage or pixel
+  */
   @Input('position-top') top: string;
-/*
-Properties
-name : position-bottom
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes bottom position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-bottom
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes bottom position in percentage or pixel
+  */
   @Input('position-bottom') bottom: string;
-/*
-Properties
-name : position-left
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes left position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-left
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes left position in percentage or pixel
+  */
   @Input('position-left') left: string;
-/*
-Properties
-name : position-right
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes right position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-right
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes right position in percentage or pixel
+  */
   @Input('position-right') right: string;
-/*
-Properties
-name : label
-datatype : none
-version : 4.1 onwards
-default : none
-description : Label on floating button, this is activated in case of block=square
-*/
+  /*
+  Properties
+  name : label
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Label on floating button, this is activated in case of block=square
+  */
   @Input('label') label: string;
-/*
-Properties
-name : block
-datatype : none
-version : 4.1 onwards
-default : none
-description : Display button as round or square
-*/
+  /*
+  Properties
+  name : block
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Display button as round or square
+  */
   @Input('block') block = 'circle';
-/*
-Properties
-name : icon
-datatype : string
-version : 4.1 onwards
-default : none
-description : FaIcon class-name.
-*/
+  /*
+  Properties
+  name : icon
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : FaIcon class-name.
+  */
   @Input('icon') icon: string;
-/*
-Properties
-name : type
-datatype : string
-version : 4.1 onwards
-default : none
-description : Type of button - default / theme-color / theme-backgroundcolor / green / red / yellow
-*/
+  /*
+  Properties
+  name : type
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : Type of button - default / theme-color / theme-backgroundcolor / green / red / yellow
+  */
   @Input('type') type: string;
-/*
-Properties
-name : disabled
-datatype : string
-version : 4.1 onwards
-default : none
-description :Enable/Disables the button
-*/
+  /*
+  Properties
+  name : disabled
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description :Enable/Disables the button
+  */
   @Input('disabled') disabled: string;
-/*
-Properties
-name : relative
-datatype : boolean
-version : 4.1 onwards
-default : none
-description : Place floating button at relative position
-*/
+  /*
+  Properties
+  name : relative
+  datatype : boolean
+  version : 4.1 onwards
+  default : none
+  description : Place floating button at relative position
+  */
   @Input('relative') relative = false;
-/*
-Events
-name : onClick
-datatype : any
-version : none
-default : none
-description : Event is fired when button is click
-*/
+  /*
+  Events
+  name : onClick
+  datatype : any
+  version : none
+  default : none
+  description : Event is fired when button is click
+  */
   @Output() onClick: any = new EventEmitter<any>();
   positionclass: string;
   absoluteposition = false;
@@ -154,56 +154,57 @@ description : Event is fired when button is click
     }
     if (!this.absoluteposition && !this.relative) {
       if (this.verticalposition === null) {
-          this.verticalposition = 'top';
-        } else if (this.horizontalposition === null) {
-          this.horizontalposition = 'right';
-        }
-        this.positionclass = this.positionclass + ' floatingbutton-' +
+        this.verticalposition = 'top';
+      } else if (this.horizontalposition === null) {
+        this.horizontalposition = 'right';
+      }
+      this.positionclass = this.positionclass + ' floatingbutton-' +
         this.verticalposition + ' floatingbutton-' + this.horizontalposition +
         ' floatingbutton-default';
     }
     if (this.block === 'circle') {
-        this.positionclass = this.positionclass + ' floatingbutton-circle';
-        this.label = '';
+      this.positionclass = this.positionclass + ' floatingbutton-circle';
+      this.label = '';
     } else if (this.block === 'square') {
       this.positionclass = this.positionclass + ' floatingbutton-square';
     }
     if (this.type === 'theme-color') {
       if (this.disabled) {
-        this.positionclass = this.positionclass  + ' floatingbutton-theme-color-disabled';
+        this.positionclass = this.positionclass + ' floatingbutton-theme-color-disabled';
       } else {
         this.positionclass = this.positionclass + ' floatingbutton-theme-color';
       }
-    } else if ( this.type === 'green') {
+    } else if (this.type === 'green') {
       if (this.disabled) {
-          this.positionclass = this.positionclass  + ' floatingbutton-green-disabled';
+        this.positionclass = this.positionclass + ' floatingbutton-green-disabled';
       } else {
         this.positionclass = this.positionclass + ' floatingbutton-green';
       }
     } else if (this.type === 'red') {
       if (this.disabled) {
-        this.positionclass = this.positionclass  + ' floatingbutton-red-disabled';
+        this.positionclass = this.positionclass + ' floatingbutton-red-disabled';
       } else {
         this.positionclass = this.positionclass + ' floatingbutton-red';
       }
     } else if (this.type === 'yellow') {
       if (this.disabled) {
-        this.positionclass = this.positionclass  + ' floatingbutton-yellow-disabled';
+        this.positionclass = this.positionclass + ' floatingbutton-yellow-disabled';
       } else {
         this.positionclass = this.positionclass + ' floatingbutton-yellow';
       }
     } else {
       if (this.disabled) {
-        this.positionclass = this.positionclass  + ' floatingbutton-default-disabled';
+        this.positionclass = this.positionclass + ' floatingbutton-default-disabled';
       } else {
         this.positionclass = this.positionclass + ' floatingbutton-default';
       }
     }
     return this.positionclass;
   }
-  buttonClick(event: any) {
-      if (!this.disabled) {
-        this.onClick.emit({this: this, event: event});
-      }
+  // Method for button click
+  buttonClick(clickEvent: any) {
+    if (!this.disabled) {
+      this.onClick.emit({thisObj: this, event: clickEvent});
+    }
   }
 }
--- src/module/forms/floatinggroupbutton/floatinggroupbutton.component.ts
+++ src/module/forms/floatinggroupbutton/floatinggroupbutton.component.ts
@@ -9,130 +9,130 @@
 import { Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'amexio-floating-group-button',
-   templateUrl: './floatinggroupbutton.component.html',
+  templateUrl: './floatinggroupbutton.component.html',
 })
 export class AmexioFloatingGroupButtonComponent implements OnInit {
-/*
-Properties
-name : vertical-position
-datatype : string
-version : 4.1 onwards
-default : none
-description : Positions floating button vertically:
-top or bottom or center. This attribute is ignored if user specify position
-explicitly (using position-top/position-bottom/position-left/position-right)
-*/
+  /*
+  Properties
+  name : vertical-position
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : Positions floating button vertically:
+  top or bottom or center. This attribute is ignored if user specify position
+  explicitly (using position-top/position-bottom/position-left/position-right)
+  */
   @Input('vertical-position') verticalposition: string;
-/*
-Properties
-name : horizontal-position
-datatype : none
-version : 4.1 onwards
-default : none
-description : Positions floating button horizontally: left or right or center.
-This attribute is ignored if user specify position explicitly
-(using position-top/position-bottom/position-left/position-right)
-*/
+  /*
+  Properties
+  name : horizontal-position
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Positions floating button horizontally: left or right or center.
+  This attribute is ignored if user specify position explicitly
+  (using position-top/position-bottom/position-left/position-right)
+  */
   @Input('horizontal-position') horizontalposition: string;
-/*
-Properties
-name : position-top
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes top position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-top
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes top position in percentage or pixel
+  */
   @Input('position-top') top: string;
-/*
-Properties
-name : position-bottom
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes bottom position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-bottom
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes bottom position in percentage or pixel
+  */
   @Input('position-bottom') bottom: string;
-/*
-Properties
-name : position-left
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes left position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-left
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes left position in percentage or pixel
+  */
   @Input('position-left') left: string;
-/*
-Properties
-name : position-right
-datatype : none
-version : 4.1 onwards
-default : none
-description : Takes right position in percentage or pixel
-*/
+  /*
+  Properties
+  name : position-right
+  datatype : none
+  version : 4.1 onwards
+  default : none
+  description : Takes right position in percentage or pixel
+  */
   @Input('position-right') right: string;
-/*
-Properties
-name : icon
-datatype : string
-version : 4.1 onwards
-default : none
-description : FaIcon classname.
-*/
+  /*
+  Properties
+  name : icon
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : FaIcon classname.
+  */
   @Input('icon') icon: string;
-/*
-Properties
-name : type
-datatype : string
-version : 4.1 onwards
-default : none
-description : Type of button - default  / theme-color /
-theme-backgroundcolor / green / red / yellow
-*/
+  /*
+  Properties
+  name : type
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : Type of button - default  / theme-color /
+  theme-backgroundcolor / green / red / yellow
+  */
   @Input('type') type: string;
-/*
-Properties
-name : disabled
-datatype : string
-version : 4.1 onwards
-default : none
-description :Enable/Disables the button
-*/
+  /*
+  Properties
+  name : disabled
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description :Enable/Disables the button
+  */
   @Input('disabled') disabled: string;
-/*
-Properties
-name : relative
-datatype : boolean
-version : 4.1 onwards
-default : none
-description : Place floating buttong at relative position
-*/
+  /*
+  Properties
+  name : relative
+  datatype : boolean
+  version : 4.1 onwards
+  default : none
+  description : Place floating buttong at relative position
+  */
   @Input('relative') relative = false;
-/*
-Properties
-name : floating-group-position
-datatype : string
-version : 4.1 onwards
-default : none
-description : Positions floating button group at specified position
-*/
+  /*
+  Properties
+  name : floating-group-position
+  datatype : string
+  version : 4.1 onwards
+  default : none
+  description : Positions floating button group at specified position
+  */
   @Input('floating-group-position') floatinggroupposition: string;
-/*
-Properties
-name : data
-datatype : array
-version : 4.1 onwards
-default : none
-description : local data for buttons
-*/
+  /*
+  Properties
+  name : data
+  datatype : array
+  version : 4.1 onwards
+  default : none
+  description : local data for buttons
+  */
   @Input('data') data: any[];
-/*
-Events
-name : onClick
-datatype : any
-version : none
-default : none
-description : Event is fired when button is click
-*/
+  /*
+  Events
+  name : onClick
+  datatype : any
+  version : none
+  default : none
+  description : Event is fired when button is click
+  */
   @Output() onClick: any = new EventEmitter<any>();
   floatinggroupxposition: string;
   floatinggroupyposition: string;
@@ -141,40 +141,40 @@ description : Event is fired when button is click
   constructor(private elementref: ElementRef) {
   }
   ngOnInit() {
-      if (this.data && this.data.length > 0 ) {
-        this.datacount = this.data.length;
-        this.data.forEach((node: any) => {
-          if (!node ['type']) {
-            node['type'] = this.type;
-            node['typeclass'] = 'floatingbutton-' + this.type;
-          } else {
-            node['typeclass'] = 'floatingbutton-' + node['type'];
-          }
-        });
-      }
+    if (this.data && this.data.length > 0) {
+      this.datacount = this.data.length;
+      this.data.forEach((node: any) => {
+        if (!node['type']) {
+          node['type'] = this.type;
+          node['typeclass'] = 'floatingbutton-' + this.type;
+        } else {
+          node['typeclass'] = 'floatingbutton-' + node['type'];
+        }
+      });
+    }
   }
-  buttonClick(event: any) {
-      let x = event.currentTarget.getBoundingClientRect().left;
-      let y = event.currentTarget.getBoundingClientRect().top;
-      if (!this.disabled) {
-        if (this.floatinggroupposition  === 'bottom') {
-          this.floatinggroupxposition = (x) + 'px';
-          this.floatinggroupyposition = (y + 70) + 'px';
-        }
-        if (this.floatinggroupposition === 'top') {
-          this.floatinggroupxposition = (x) + 'px';
-          this.floatinggroupyposition = (y - (70 * this.datacount)) + 'px';
-        }
-        this.togglefloatinggroup = !this.togglefloatinggroup ;
-        this.onClick.emit({this: this, event: event});
-     }
+  buttonClick(clickEvent: any) {
+    const x = clickEvent.currentTarget.getBoundingClientRect().left;
+    const y = clickEvent.currentTarget.getBoundingClientRect().top;
+    if (!this.disabled) {
+      if (this.floatinggroupposition === 'bottom') {
+        this.floatinggroupxposition = (x) + 'px';
+        this.floatinggroupyposition = (y + 70) + 'px';
+      }
+      if (this.floatinggroupposition === 'top') {
+        this.floatinggroupxposition = (x) + 'px';
+        this.floatinggroupyposition = (y - (70 * this.datacount)) + 'px';
+      }
+      this.togglefloatinggroup = !this.togglefloatinggroup;
+      this.onClick.emit({ thisObj: this, event: clickEvent });
+    }
   }
   onFloatingButtonClick(event: any) {
     this.buttonClick(event.event);
   }
-  onButtonClick(node: any, event: any) {
-    this.togglefloatinggroup = !this.togglefloatinggroup ;
-    this.onClick.emit({this: node, parent: this, event: event});
+  onButtonClick(node: any, clickEvent: any) {
+    this.togglefloatinggroup = !this.togglefloatinggroup;
+    this.onClick.emit({ this: node, parent: this, event: clickEvent });
   }
   @HostListener('document:click', ['$event.target']) @HostListener('document: touchstart', ['$event.target'])
   public onElementOutClick(targetElement: HTMLElement) {
@@ -186,7 +186,7 @@ description : Event is fired when button is click
       targetElement = targetElement.parentElement;
     }
     if (!parentFound) {
-       this.togglefloatinggroup = false;
-     }
+      this.togglefloatinggroup = false;
+    }
   }
 }
