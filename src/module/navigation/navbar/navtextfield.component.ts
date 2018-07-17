46ca3805b84df6bb74ee73523f5c1f9bc162fcb0
ankita202 <ankita.jain@metamgic.in>
2018-07-17 16:49:45 +0530

    Navbar: navtextfeild Component lint updated.

--- src/module/navigation/navbar/navtextfield.component.ts
+++ src/module/navigation/navbar/navtextfield.component.ts
@@ -1,12 +1,8 @@
 import {
-  Component, ElementRef, EventEmitter, forwardRef, Input, Output, ViewChild, ViewEncapsulation} from '@angular/core';
+  Component, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output, ViewChild, ViewEncapsulation} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 const noop = () => {
-};
-
-export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => AmexioNavTextFieldComponent), multi: true,
 };
 
 @Component({
@@ -16,26 +12,25 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   <input type="text" class="top-nav-input-control" [(ngModel)]="value"/>
 
   `,
-  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => AmexioNavTextFieldComponent), multi: true,
+  }],
   encapsulation: ViewEncapsulation.None,
 })
 
-export class AmexioNavTextFieldComponent implements ControlValueAccessor {
+export class AmexioNavTextFieldComponent implements ControlValueAccessor, OnInit {
+  private innerValue: any = '';
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
 
   constructor() {
   }
 
   ngOnInit() {
   }
-
   // The internal dataviews model
-  private innerValue: any = '';
-
   // Placeholders for the callbacks which are later provided
   // by the Control Value Accessor
-  private onTouchedCallback: () => void = noop;
-  private onChangeCallback: (_: any) => void = noop;
-
   // get accessor
   get value(): any {
     return this.innerValue;
