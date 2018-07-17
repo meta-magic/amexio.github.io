d89bce64c3a5b528fc556c1606616a9cc84e2c6d
ankita202 <ankita.jain@metamgic.in>
2018-07-17 18:25:36 +0530

    Navigation:notification

--- src/module/navigation/notification/notification.component.ts
+++ src/module/navigation/notification/notification.component.ts
@@ -1,27 +1,29 @@
 /**
  * Created by ketangote on 12/18/17.
  */
-import {AfterContentInit, ChangeDetectorRef,ContentChild, HostListener, TemplateRef, Component, DoCheck, EventEmitter, Input, OnInit, Output,
-  QueryList} from '@angular/core';
+import {
+  AfterContentInit, ChangeDetectorRef, Component, ContentChild,  DoCheck, EventEmitter, HostListener, Input, OnInit, Output, QueryList,
+  TemplateRef,
+} from '@angular/core';
 
-  export enum KEY_CODE_notify {
-    esc = 27
-    }
+export enum KEY_CODE_notify {
+  esc = 27,
+}
 
 @Component({
-  selector: 'amexio-notification', templateUrl: './notification.component.html'
+  selector: 'amexio-notification', templateUrl: './notification.component.html',
 })
 export class AmexioNotificationComponent implements OnInit {
 
-   /*
-  Properties
-  name : data
-  datatype : any
-  version : 4.2 onwards
-  default : none
-  description :  what you want to display on notification window by using data.
-  */
- @Input('data') messageData: any[];
+  /*
+ Properties
+ name : data
+ datatype : any
+ version : 4.2 onwards
+ default : none
+ description :  what you want to display on notification window by using data.
+ */
+  @Input('data') messageData: any[];
   /*
 Properties
 name : closable
@@ -30,15 +32,17 @@ version : 4.0 onwards
 default : false
 description : User can close the window.
 */
-  @Input() closable: boolean = true;
+  @Input() closable = true;
 
-   /*
+  /*
 Properties
 name : vertical-position
 datatype : string
 version : 4.2 onwards
 default : none
-description : Position of notification window vertically: top or bottom or center. This attribute is ignored if user specify position explicitly (using position-top/position-bottom/position-center/position-right/position-left)
+description : Position of notification window vertically:
+ top or bottom or center. This attribute is ignored if user specify
+  position explicitly (using position-top/position-bottom/position-center/position-right/position-left)
 */
 
   @Input('vertical-position') verticalposition: string;
@@ -48,99 +52,100 @@ description : Position of notification window vertically: top or bottom or cente
   datatype : string
   version : 4.2 onwards
   default : none
-  description : Position of notification Window horizontally: left or right or center. This attribute is ignored if user specify position explicitly (using position-top/position-bottom/position-center/position-left/position-right)
+  description : Position of notification Window horizontally:
+  left or right or center. This attribute is ignored if user specify
+   position explicitly (using position-top/position-bottom/position-center/position-left/position-right)
   */
 
   @Input('horizontal-position') horizontalposition: string;
 
- /*
-  Properties
-  name : auto-dismiss-msg
-  datatype : boolean
-  version : 4.2 onwards
-  default : none
-  description :notification message will dismiss automatically.
-  */
+  /*
+   Properties
+   name : auto-dismiss-msg
+   datatype : boolean
+   version : 4.2 onwards
+   default : none
+   description :notification message will dismiss automatically.
+   */
   @Input('auto-dismiss-msg') autodismissmsg: boolean;
-/*
-  Properties
-  name : auto-dismiss-msg-interval
-  datatype : number
-  version : 4.2 onwards
-  default : none
-  description :notification message will dismiss after some user define interval.
-  */
+  /*
+    Properties
+    name : auto-dismiss-msg-interval
+    datatype : number
+    version : 4.2 onwards
+    default : none
+    description :notification message will dismiss after some user define interval.
+    */
   @Input('auto-dismiss-msg-interval') autodismissmsginterval: number;
-/*
-  Properties
-  name : close-on-escape
-  datatype : boolean
-  version : 4.2 onwards
-  default : none
-  description : close the notification window on click on ESC button.
-  */
+  /*
+    Properties
+    name : close-on-escape
+    datatype : boolean
+    version : 4.2 onwards
+    default : none
+    description : close the notification window on click on ESC button.
+    */
   @Input('close-on-escape') closeonescape: boolean;
-/*
-  Properties
-  name : background-color
-  datatype : string
-  version : 4.2 onwards
-  default : none
-  description : background color to the notification window .
-  */
+  /*
+    Properties
+    name : background-color
+    datatype : string
+    version : 4.2 onwards
+    default : none
+    description : background color to the notification window .
+    */
   @Input('background-color') backgroundColor: string;
-/*
-  Properties
-  name : foreground-color
-  datatype : string
-  version : 4.2 onwards
-  default : none
-  description : foreground color to the notification window text .
-  */
+  /*
+    Properties
+    name : foreground-color
+    datatype : string
+    version : 4.2 onwards
+    default : none
+    description : foreground color to the notification window text .
+    */
   @Input('foreground-color') foregroundColor: string;
 
   data: any[];
 
   positionclass: string;
 
-  customClass : boolean;
+  customClass: boolean;
 
+ @ContentChild('amexioNotificationTemp') notificationTemplate: TemplateRef<any>;
 
-  @ContentChild('amexioNotificationTemp') notificationTemplate: TemplateRef<any>;
-
-   //Escape Key Functionality
-   @HostListener('window:keyup', ['$event'])keyEvent(event: KeyboardEvent) {
-  if (this.closeonescape == true) {
-        if (event.keyCode == KEY_CODE_notify.esc) {
-          const count = this.messageData.length;
-          for (let i = 0; i < count; i++) {
-            let msg = this.messageData[i]
-              this.messageData.splice(msg, 1);
-                 }
-              }
-         }
+  // Escape Key Functionality
+  @HostListener('window:keyup', ['$event']) keyEvent(event: KeyboardEvent) {
+    if (this.closeonescape === true) {
+      if (event.keyCode === KEY_CODE_notify.esc) {
+        const count = this.messageData.length;
+        for (let i = 0; i < count; i++) {
+          const msg = this.messageData[i];
+          this.messageData.splice(msg, 1);
+        }
+      }
     }
+  }
   constructor(private ref: ChangeDetectorRef) {
   }
   ngOnInit() {
 
     if (this.autodismissmsg) {
-          if (!this.autodismissmsginterval) {
-            this.autodismissmsginterval = 1500;
-          }
-            if (this.messageData !== null) {
-              setInterval(() => {
-                this.messageData.shift();
-                this.ref.markForCheck();
-              }, this.autodismissmsginterval);
-        }
+      if (!this.autodismissmsginterval) {
+        this.autodismissmsginterval = 1500;
+      }
+      if (this.messageData !== null) {
+        setInterval(() => {
+          this.messageData.shift();
+          this.ref.markForCheck();
+        }, this.autodismissmsginterval);
+      }
     }
     if (this.verticalposition === null) {
       this.verticalposition = 'top';
     } else if (this.horizontalposition === null) {
-      this.horizontalposition = 'right'
+      this.horizontalposition = 'right';
     }
-  this.positionclass = "notification-vertical-" + this.verticalposition + " notification-horizontal-" + this.horizontalposition;
+    this.positionclass = "notification-vertical-" + this.verticalposition + " notification-horizontal-" + this.horizontalposition;
   }
   closeNotification(msg: any) {
     const count = this.messageData.length;
