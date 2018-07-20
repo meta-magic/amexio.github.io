ebcee7b75c010f4ede6b4215c3deb93f210ca3d6
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-07-20 17:52:04 +0530

    mediaModule: exported

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export * from './module/charts/amexio.charts.module';
 export * from './module/map/amexio.map.module';
 export * from './module/panes/amexio.pane.module';
 export * from './module/enterprise/amexio.enterprise.module';
-
+export * from './module/media/amexio.media.module';
 export * from './module/services/icon/icon.service';
 export * from './module/charts/chart.loader.service';
 export * from './module/dashboard/chart.loader.service';
