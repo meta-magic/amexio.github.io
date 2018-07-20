78c0ecc1bb352ceca32f19d6cdd391ee6256eb5c
kedar8 <kedar.kokil@metamagic.in>
2018-07-20 19:41:22 +0530

    card: code optimization

--- src/module/layout/card/card.component.ts
+++ src/module/layout/card/card.component.ts
@@ -1,32 +1,29 @@
-import { AmexioFooterComponent } from './../../panes/action/pane.action.footer';
-import { AmexioBodyComponent } from './../../panes/body/pane.action.body';
-import { ContentChildren, QueryList } from '@angular/core';
-import {AmexioHeaderComponent} from '../../panes/header/pane.action.header';
+
 /**
  * Created by ketangote on 12/18/17.
  */
-
- /*
- Component Name : Amexio card
-  Component Selector : <amexio-card>
- Component Description : Amexio Card which renders card based on title, body and actions user has configured
-
-.
-
+/*
+Component Name : Amexio card
+ Component Selector : <amexio-card>
+Component Description : Amexio Card which renders card based on title, body and actions user has configured
 */
 
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef} from '@angular/core';
-
+import { AfterContentInit, AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ContentChildren, QueryList } from '@angular/core';
+import { AmexioHeaderComponent } from '../../panes/header/pane.action.header';
+import { AmexioFooterComponent } from './../../panes/action/pane.action.footer';
+import { AmexioBodyComponent } from './../../panes/body/pane.action.body';
 @Component({
-  selector: 'amexio-card', template: `
-
+  selector: 'amexio-card',
+  template: `
   <div class="card-container" *ngIf="show"  (window:resize)="onResize()">
     <header #cardHeader  [style.padding]="headerPadding"  class="card-header" *ngIf="header"
 
             [ngClass]="{'flex-start':(headeralign=='left'),'flex-end':(headeralign=='right'),'flex-center':(headeralign=='center')}">
       <ng-content select="amexio-header"></ng-content>
     </header>
-<div class="card-body cardbody" [style.padding]="bodyPadding"  [ngStyle]="{'height.px' : height,'overflow-y' : height!= null ? 'auto' : '','min-height.px' : minHeight}">
+<div class="card-body cardbody" [style.padding]="bodyPadding"
+[ngStyle]="{'height.px' : height,'overflow-y' : height!= null ? 'auto' : '','min-height.px' : minHeight}">
       <ng-content select="amexio-body"></ng-content>
     </div>
     <footer  #cardFooter [style.padding]="footerPadding"  class="card-footer" *ngIf="footer"
@@ -34,13 +31,9 @@ import {Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef} f
       <ng-content select="amexio-action"></ng-content>
     </footer>
   </div>
-
-
-
   `,
 })
-export class AmexioCardComponent implements OnInit {
-
+export class AmexioCardComponent implements OnInit, AfterViewInit, AfterContentInit {
   /*
 Properties
 name : header-align
@@ -50,8 +43,7 @@ default : left
 description : Align of item elements inside card header example : right,center,left.
 */
   @Input('header-align') headeralign: string;
-
-    /*
+  /*
 Properties
 name : header-align
 datatype : string
@@ -60,8 +52,7 @@ default : left
 description : User can enable header of card by setting this flag to true..
 */
   @Input() header: boolean;
-
-    /*
+  /*
 Properties
 name : footer
 datatype : boolean
@@ -70,7 +61,6 @@ default : false
 description : User can enable footer of card by setting this flag to true.
 */
   @Input() footer: boolean;
-
   /*
 Properties
 name : footer-align
@@ -80,8 +70,7 @@ default : left
 description : Align of item elements inside card footer example : right,center,left..
 */
   @Input('footer-align') footeralign: string;
-
-    /*
+  /*
 Properties
 name : show
 datatype :  boolean
@@ -89,8 +78,7 @@ version : 4.0 onwards
 default : true
 description : User can bind variable to this and hide/unhide card based on requirement..
 */
-  @Input() show: boolean = true;
-
+  @Input() show = true;
   /*
 Properties
 name : height
@@ -99,8 +87,7 @@ version : 4.0 onwards
 default :
 description : User can set the height to body..
 */
-  @Input()  height : any;
-
+  @Input() height: any;
   /*
 Properties
 name : min-height
@@ -109,8 +96,7 @@ version : 4.0 onwards
 default :
 description : Provides minimum card height.
 */
-  @Input('min-height')  minHeight : any;
-
+  @Input('min-height') minHeight: any;
   /*
 Properties
 name : body-height
@@ -119,98 +105,71 @@ version : 4.0 onwards
 default :
 description : Provides card height.
 */
-  @Input('body-height') bodyheight : any;
+  @Input('body-height') bodyheight: any;
 
-  @ViewChild('cardHeader', {read: ElementRef}) public cardHeader: ElementRef;
+  @ViewChild('cardHeader', { read: ElementRef }) public cardHeader: ElementRef;
 
-  @ViewChild('cardFooter', {read: ElementRef}) public cardFooter: ElementRef;
+  @ViewChild('cardFooter', { read: ElementRef }) public cardFooter: ElementRef;
 
-  headerPadding:string;
+  headerPadding: string;
+  bodyPadding: string;
+  footerPadding: string;
 
-  bodyPadding:string;
-
-  footerPadding:string;
-
-  @ContentChildren(AmexioHeaderComponent) amexioHeader:QueryList<AmexioHeaderComponent>;
-
-  headerComponentList:AmexioHeaderComponent[];
-
-  @ContentChildren(AmexioBodyComponent) amexioBody:QueryList<AmexioBodyComponent>;
-
-  bodyComponentList:AmexioBodyComponent[];
-
-  @ContentChildren(AmexioFooterComponent) amexioFooter:QueryList<AmexioFooterComponent>;
-
-  footerComponentList:AmexioFooterComponent[];
-
+  @ContentChildren(AmexioHeaderComponent) amexioHeader: QueryList<AmexioHeaderComponent>;
+  headerComponentList: AmexioHeaderComponent[];
+  @ContentChildren(AmexioBodyComponent) amexioBody: QueryList<AmexioBodyComponent>;
+  bodyComponentList: AmexioBodyComponent[];
+  @ContentChildren(AmexioFooterComponent) amexioFooter: QueryList<AmexioFooterComponent>;
+  footerComponentList: AmexioFooterComponent[];
   constructor() {
-    this.headeralign = "left";
-    this.footeralign = "right";
-
+    this.headeralign = 'left';
+    this.footeralign = 'right';
   }
-
-
   ngOnInit() {
-   
   }
-
-
   ngAfterViewInit() {
-    this.onResize();
-    
-
+    setTimeout(() => {
+      this.onResize();
+    }, 500);
   }
-
-  ngAfterContentInit(){
-    //FOR HEADER PADING
+  ngAfterContentInit() {
+    // FOR HEADER PADING
     this.headerComponentList = this.amexioHeader.toArray();
-    this.headerComponentList.forEach((item:AmexioHeaderComponent,currentIndex)=>{
-      if(item.padding){
-        this.headerPadding=item.padding;
+    this.headerComponentList.forEach((item: AmexioHeaderComponent, currentIndex) => {
+      if (item.padding) {
+        this.headerPadding = item.padding;
       }
     });
-
-    //FOR BODY PADDING
+    // FOR BODY PADDING
     this.bodyComponentList = this.amexioBody.toArray();
-    this.bodyComponentList.forEach((item:AmexioBodyComponent,currentIndex)=>{
-      if(item.padding){
-        this.bodyPadding=item.padding;
+    this.bodyComponentList.forEach((item: AmexioBodyComponent, currentIndex) => {
+      if (item.padding) {
+        this.bodyPadding = item.padding;
       }
     });
-    //FOR FOOTER PADDING
+    // FOR FOOTER PADDING
     this.footerComponentList = this.amexioFooter.toArray();
-    this.footerComponentList.forEach((item:AmexioFooterComponent,currentIndex)=>{
-      if(item.padding){
-        this.footerPadding=item.padding;
+    this.footerComponentList.forEach((item: AmexioFooterComponent, currentIndex) => {
+      if (item.padding) {
+        this.footerPadding = item.padding;
       }
     });
   }
-  /**
-   *  Calculate body size based on browser height
-   **/
-  onResize(){
-
-    if(this.bodyheight){
-      let h = (window.innerHeight/100)*this.bodyheight;
-
-      if(this.cardHeader && this.cardHeader.nativeElement && this.cardHeader.nativeElement.offsetHeight)
+  // Calculate body size based on browser height
+  onResize() {
+    if (this.bodyheight) {
+      let h = (window.innerHeight / 100) * this.bodyheight;
+      if (this.cardHeader && this.cardHeader.nativeElement && this.cardHeader.nativeElement.offsetHeight) {
         h = h - this.cardHeader.nativeElement.offsetHeight;
-
-      if(this.cardFooter && this.cardFooter.nativeElement && this.cardFooter.nativeElement.offsetHeight)
+      }
+      if (this.cardFooter && this.cardFooter.nativeElement && this.cardFooter.nativeElement.offsetHeight) {
         h = h - this.cardFooter.nativeElement.offsetHeight;
-
-
-      if(this.bodyheight === 100)
+      }
+      if (this.bodyheight === 100) {
         h = h - 40;
-
+      }
       this.minHeight = h;
       this.height = h;
     }
   }
-
-
-
 }
-
-
-
