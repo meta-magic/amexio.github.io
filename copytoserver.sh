2e8e02da15307dab3175cee1ede6a211190fd977
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-07-24 19:19:01 +0530

    Version : v4.3.1 added

--- copytoserver.sh
+++ copytoserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Copy dist/ to Apache Server"
-cd dist && npm pack && mv amexio-ng-extensions-4.2.7.tgz amexio-ng-extensions.tgz
+cd dist && npm pack && mv amexio-ng-extensions-4.3.1.tgz amexio-ng-extensions.tgz
 cp -a amexio-ng-extensions.tgz /var/www/html/desireplatform/amexio-builds/
 
