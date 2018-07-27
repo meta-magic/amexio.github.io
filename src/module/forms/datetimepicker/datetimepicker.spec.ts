a3e2e292f8455e6b820a0eeaef2c6df882fc12a8
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-07-27 12:31:44 +0530

    Datepicker : Test case added

---
+++ src/module/forms/datetimepicker/datetimepicker.spec.ts
@@ -0,0 +1,28 @@
+import { AmexioButtonComponent } from './../buttons/button.component';
+import { AmexioFormIconComponent } from './../icon/icon.component';
+import { AmexioDateTimePickerComponent } from './datetimepicker.component';
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import { IconLoaderService } from '../../../index';
+
+describe('amexio-date-time-picker' , () => {
+let comp: AmexioDateTimePickerComponent;
+let fixture: ComponentFixture<AmexioDateTimePickerComponent>;
+
+beforeEach(() => {
+TestBed.configureTestingModule({
+imports : [FormsModule],
+declarations: [ AmexioDateTimePickerComponent,AmexioFormIconComponent,AmexioButtonComponent],
+providers:[IconLoaderService]
+});
+  fixture = TestBed.createComponent(AmexioDateTimePickerComponent);
+  comp = fixture.componentInstance;
+});
+
+it('Display correct label', () => {
+
+  fixture.detectChanges();
+});
+
+});
