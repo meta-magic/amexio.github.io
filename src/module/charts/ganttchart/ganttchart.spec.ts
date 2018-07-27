736d7606d6c212144d64136a0a95576791406b25
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-07-27 16:40:05 +0530

    Gantt chart: Test case

--- src/module/charts/ganttchart/ganttchart.spec.ts
+++ src/module/charts/ganttchart/ganttchart.spec.ts
@@ -1,4 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { GanttChartComponent } from './gantt.chart.component';
+import { ChartLoaderService } from './../chart.loader.service';
 
-describe('Gantt chart tests', () => {
-  it('true is true', () => expect(true).toBe(true));
-});
+describe('amexio-text-field' , () => {
+  let comp: GanttChartComponent;
+  let fixture: ComponentFixture<GanttChartComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(() => {
+  TestBed.configureTestingModule({
+  imports : [FormsModule],
+  declarations: [ GanttChartComponent],
+  providers:[ChartLoaderService]
+  });
+  fixture = TestBed.createComponent(GanttChartComponent);
+  comp = fixture.componentInstance;
+
+  });
+
+  it('datacheck null', () => {
+    let newdata = [{'name':'chart'}];
+    comp.data = newdata ;
+    expect(newdata).toEqual(comp.data);
+    expect(true).toBe(comp.showChart);
+  });
+
+  });
