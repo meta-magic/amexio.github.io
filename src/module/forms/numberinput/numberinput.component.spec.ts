5700e5d661b911b792517a91bf8d4e287d647997
Manisha1205 <manisha.boddu@metamagic.in>
2018-08-02 14:06:16 +0530

    numberinput: innervalue tested

--- src/module/forms/numberinput/numberinput.component.spec.ts
+++ src/module/forms/numberinput/numberinput.component.spec.ts
@@ -1,6 +1,33 @@
 /**
  * Created by pratik on 1/12/17.
  */
-describe('DropDown tests', () => {
+import { AmexioNumberInputComponent } from './numberinput.component';
+import { AmexioFormIconComponent } from '../icon/icon.component';
+import { FormsModule } from '@angular/forms';
+import { IconLoaderService } from '../../../index'
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+describe('NUMBER INPUT', () => {
+
+  let comp: AmexioNumberInputComponent;
+  let fixture: ComponentFixture<AmexioNumberInputComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports : [FormsModule],
+    declarations: [ AmexioNumberInputComponent,AmexioFormIconComponent],
+    providers:[IconLoaderService]
+    });
+    fixture = TestBed.createComponent(AmexioNumberInputComponent);
+    comp = fixture.componentInstance;
+
   it('true is true', () => expect(true).toBe(true));
+});
+
+ 
+it('Get innervalue', () => {
+  //comp.value='sagfaf';
+  
+//this.fixture.detectChanges();
+       expect(comp.value).toEqual('');
+    });
+
 });
