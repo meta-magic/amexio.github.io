d81b5561bf130b260c3aa595562db060ebc2ae03
Manisha1205 <manisha.boddu@metamagic.in>
2018-08-03 11:31:23 +0530

    numberinput:  4 test cases

--- src/module/forms/numberinput/numberinput.component.spec.ts
+++ src/module/forms/numberinput/numberinput.component.spec.ts
@@ -23,11 +23,42 @@ describe('NUMBER INPUT', () => {
 });
 
  
-it('Get innervalue', () => {
-  //comp.value='sagfaf';
+// it('Set', () => {
+//   comp.value='sagfaf';
   
-//this.fixture.detectChanges();
-       expect(comp.value).toEqual('');
-    });
+// //this.fixture.detectChanges();
+//        expect(comp.innerValue).toEqual(comp.value);
+//     }); 
+
+//wrking 1- set errormsg
+    it('set errormsg', () => {
+      comp.errormsg='data incorect';
+            expect(comp.helpInfoMsg).toEqual('data incorect<br/>');
+        });
+
+//working 2 get minerrormsg
+it('get minerrormsg', () => {
+  expect(comp._minerrormsg).toEqual(comp._minerrormsg);
+})
+
+//working 3 get maxerrormsg
+it('get _maxerrormsg', () => {
+  expect(comp._maxerrormsg).toEqual(comp._maxerrormsg);
+})
+
+//set maxerrormsg
+
+
+//set minerrormsg
+// it('set minerrormsg', () => {
+//   comp.enablepopover=true;
+//   comp.helpInfoMsg="";
+//   comp._errormsg="Please enter age";
+//   fixture.detectChanges();
+//   //expect(comp.helpInfoMsg).toBe(comp.helpInfoMsg);
+//   expect(comp.helpInfoMsg).toContain(comp.helpInfoMsg + 'Min value: ' + comp.value + '<br/>');
+ 
+// });
+
 
 });
