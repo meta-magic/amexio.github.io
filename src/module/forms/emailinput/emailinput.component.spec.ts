9719be16493fee98d7a96ce8cf995ed68732a87e
kedar8 <kedar.kokil@metamagic.in>
2018-08-06 12:17:53 +0530

    unit testing:  email input testing

--- src/module/forms/emailinput/emailinput.component.spec.ts
+++ src/module/forms/emailinput/emailinput.component.spec.ts
@@ -1,6 +1,38 @@
-/**
- * Created by pratik on 1/12/17.
- */
-describe('DropDown tests', () => {
-  it('true is true', () => expect(true).toBe(true));
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { AmexioButtonComponent } from './../buttons/button.component';
+import { AmexioFormIconComponent } from './../icon/icon.component';
+import { AmexioEmailInputComponent } from './emailinput.component';
+
+describe('amexio-label' , () => {
+  let comp: AmexioEmailInputComponent;
+  let fixture: ComponentFixture<AmexioEmailInputComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports : [FormsModule],
+      declarations: [ AmexioEmailInputComponent, AmexioFormIconComponent, AmexioButtonComponent],
+      providers: [IconLoaderService],
+    });
+    fixture = TestBed.createComponent(AmexioEmailInputComponent);
+    comp = fixture.componentInstance;
+  });
+
+
+
+  // it('check enableclick true', () => {
+  //   comp.multi = true;
+  //   comp.ngOnInit();
+  //   expect(comp.multi).toEqual(true);
+  // });
+  it('check private variable innerValue empty', () => {
+    expect((<any>comp).innerValue).toBe('');
+  });
+
+  it('check private variable innerValue empty', () => {
+    expect(comp.showToolTip).toEqual(jasmine.any(Boolean));
+  });
+
 });
