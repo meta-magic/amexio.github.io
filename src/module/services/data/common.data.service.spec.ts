86e00e1a4b3efd509fb30ae40887192c383c7976
kedar8 <kedar.kokil@metamagic.in>
2018-08-07 15:36:53 +0530

    common data service: code optimization

--- src/module/services/data/common.data.service.spec.ts
+++ src/module/services/data/common.data.service.spec.ts
@@ -1,6 +1,44 @@
+import {of} from "rxjs/observable/of";
+import { CommonDataService } from "./common.data.service";
 /**
- * Created by pratik on 1/12/17.
+ * Created by dattaram on 7/8/18.
  */
-describe('data service tests', () => {
-  it('true is true', () => expect(true).toBe(true));
+describe('CommonDataService', () => {
+  let commonDataService: CommonDataService,
+  serviceUrl, methodType,
+  mockHttp: any;
+   beforeEach(() => {
+     mockHttp = jasmine.createSpyObj('mockHttp', ['get', 'post'])
+     commonDataService = new CommonDataService(mockHttp);
+
+   });
+   it('fetch data ith get method ', () => {
+     serviceUrl = '';
+     methodType = 'get';
+     mockHttp.get.and.returnValue(of(false));
+     commonDataService.fetchData(serviceUrl, methodType);
+     expect(methodType).toEqual('get');
+     expect(mockHttp.get).toHaveBeenCalledWith(serviceUrl, jasmine.any(Object));
+   });
+
+  it('fetch data with post method ', () => {
+    serviceUrl = '';
+    methodType = 'post';
+    mockHttp.post.and.returnValue(of(false));
+    commonDataService.fetchData(serviceUrl, methodType);
+    expect(methodType).toEqual('post');
+    expect(mockHttp.post).toHaveBeenCalledWith(serviceUrl, {}, jasmine.any(Object));
+  });
+
+  it('uploadFile method', () => {
+    serviceUrl = '';
+    methodType = 'post';
+    const requestData = jasmine.any(Object);
+    mockHttp.post.and.returnValue(of(false));
+    commonDataService.uploadFile(serviceUrl, methodType, requestData);
+    expect(methodType).toEqual('post');
+    expect(mockHttp.post).toHaveBeenCalledWith(serviceUrl, requestData , jasmine.any(Object));
+
+
+  });
 });
