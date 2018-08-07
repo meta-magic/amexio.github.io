168940bfd07e29984d33a414dc08d778e0bc7bd2
kedar8 <kedar.kokil@metamagic.in>
2018-08-07 18:44:27 +0530

    code optimization: email input

--- src/module/forms/emailinput/emailinput.component.spec.ts
+++ src/module/forms/emailinput/emailinput.component.spec.ts
@@ -38,28 +38,77 @@ describe('amexio-email-input' , () => {
     expect((<any>comp).innerValue).toEqual(null);
    (<any>comp).innerValue = '';
    expect((<any>comp).innerValue).toEqual('');
-
    expect(comp.isValid).toEqual(jasmine.any(Boolean));
-
-
   });
+
+
   it('condition check value is null or empty in onBlank method', () => {
     comp.onBlank({'touched': true});
     (<any>comp).value = '';
      expect((<any>comp).value).toEqual('');
      (<any>comp).value = null;
      expect((<any>comp).value).toEqual(null);
-  
   });
 
+
   it('onfocus method check boolean value  showtooltip is true', () => {
-    
     comp.onFocus();
     expect((<any>comp).showToolTip).toEqual(true);
   });
+
+
   it('onfocus method writevalue method call', () => {
     comp.writeValue('value');
     (<any>comp).innerValue = '';
     expect((<any>comp).innerValue).not.toEqual('value');
   });
+
+// it('conditions check of the validateClasses', () => {
+//     comp.validateClasses('value');
+//     (<any>comp).allowblank = 'value';
+//    expect(comp.allowblank).toEqual(false);
+//   });
+
+it('set errormsg', () => {
+  comp.errormsg='data incorect';
+        expect(comp.helpInfoMsg).toEqual('data incorect<br/>');
+    });
+
+    it('get errormsg', () => {
+    //  comp.errormsg='data incorect';
+            expect(comp.errormsg).toEqual(comp._errormsg);
+        });
+
+//get pattern
+it('get pattern', () => {
+expect(comp.pattern).toEqual(comp._pattern);
+})
+
+//set pattern
+// it('set pattern', () => {
+
+//   let obj = new RegExp(comp.pattern);
+//   expect(comp.value).not.toEqual(null);
+//   expect(comp.regEx).toEqual(obj);
+//  })
+
+it('register on change', () => {
+let fn: any;
+comp.registerOnChange(fn);
+expect(comp['onChangeCallback']).toEqual(fn);
+});
+
+
+it('register on touched', () => {
+let fn: any;
+comp.registerOnTouched(fn);
+expect(comp['onTouchedCallback']).toEqual(fn);
+});
+
+it('getCssClass()', () => {
+  comp.getCssClass();
+  expect(comp.getCssClass).toBeUndefined;
+  });
+
+ 
 });
