a8060581869417d1d8e03aba0ff2de7ee0611958
ankita202 <ankita.jain@metamgic.in>
2018-08-10 12:31:30 +0530

    linechart:code optimized

--- src/module/charts/linechart/line.chart.component.spec.ts
+++ src/module/charts/linechart/line.chart.component.spec.ts
@@ -5,82 +5,40 @@ import { IconLoaderService } from '../../../index'
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonDataService } from '../../services/data/common.data.service';
 import { ChartLoaderService } from './../chart.loader.service';
-
+import { ChartTitleComponent } from '../charttitle/chart.title.component';
 describe('LINE CHART', () => {
-
-    let comp: LineChartComponent;
-    let fixture: ComponentFixture<LineChartComponent>;
+    //let ChartTitleComponent=new ChartTitleComponent()
+    // let ChartTitleComponent = [{'name':'chart','title':'','position':'','color':'','fontname':'','fontsize':'','bold':false,'italic':''}];
+    let linechartcomp: LineChartComponent;
+    let charttitlecomp: ChartTitleComponent;
+    let linefixture: ComponentFixture<LineChartComponent>;
+    let charttitlefixture: ComponentFixture<ChartTitleComponent>;
     beforeEach(() => {
-      TestBed.configureTestingModule({
-      imports : [FormsModule],
-      declarations: [ LineChartComponent,LineChartComponent],
-      providers:[IconLoaderService, CommonDataService, ChartLoaderService]
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [LineChartComponent, ChartTitleComponent],
+            providers: [IconLoaderService, CommonDataService, ChartLoaderService]
+        });
+        linefixture = TestBed.createComponent(LineChartComponent);
+        charttitlefixture = TestBed.createComponent(ChartTitleComponent);
+        linechartcomp = linefixture.componentInstance;
+        charttitlecomp = charttitlefixture.componentInstance;
+        linechartcomp.chartTitleComponent = charttitlecomp;
+    });
+    it('show chart', () => {
+        linechartcomp.showChart = false;
+        expect(false).toBe(linechartcomp.showChart);
+        let newdata = [{ 'name': 'chart' }];
+        linechartcomp.data = newdata;
+    });
+    it('dont show chart', () => {
+        let newdata;
+        linechartcomp.data = newdata ;
+        expect(false).toBe( linechartcomp.showChart);
       });
-      fixture = TestBed.createComponent(LineChartComponent);
-      comp = fixture.componentInstance;
-     // chart = new google.visualization.LineChart(comp[linechart]);
-    });
-//wrking
-    it('on resize', () => {
-        let event: any;
-        comp.onResize(event);
-        comp.drawChart();
-    });
-
-//wrking
-    it('get data', () => {
-         expect(comp.data).toEqual(comp._data);
-    }); 
-//not wrking
-//     it('set data with not null', () => {
-//         expect(comp.data).not.toBeNull
-//         comp.data;
-//         // comp.showChart = true;
-//         expect(comp.showChart).toEqual(false);
-//    }); 
-//not wrking   
-//    it('set data with Null', () => {
-//     expect(comp.data).toBeNull;
-//     comp.showChart = false;
-// }); 
-
-//not wrking
-   it('drawchart function1',() => {
-       comp.drawChart();
-       comp.showChart=true;
-        expect(comp.showChart).toEqual(true);
-        let obj1=comp['lineData'] = '';
-        // expect(comp['lineData']).toEqual(comp.createTable(comp._data));
-        let obj2=comp['options'] = {
-            title: comp.chartTitleComponent ? comp.chartTitleComponent.title : null,
-            titleTextStyle: comp.chartTitleComponent ? comp.chartTitleTextStyle() : null,
-            backgroundcolor: comp.backgroundcolor,
-            legend: comp.chartLengendComponent ? comp.chartLegendStyle() : 'none',
-            chartArea: comp.chartAreaComponent ? comp.chartBackgroundStyle() : null,
-          };
-          it('conditional statement',()=>{
-              comp['lineData']='true';
-              comp.hasLoaded=true;
-              //this.chart = new google.visualization.LineChart(comp.linechart.nativeElement);
-          })
 
 
-
-         //not wrking 
-          it('chartTitleTextStyle', () => {
-
-            let obj = jasmine.any(Object);
-               comp.chartTitleTextStyle()&&obj;
-        }); 
-
-it('ngoninit()',() =>{
-comp.ngOnInit();
-expect(comp.hasLoaded).toEqual(false);
-comp.drawChart();
 
 });
 
-   }); 
 
-
-});
