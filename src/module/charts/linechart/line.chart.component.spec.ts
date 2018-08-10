71ccc63195bff05aa7c8f38d84e68478fe501fee
ankita202 <ankita.jain@metamgic.in>
2018-08-10 15:06:17 +0530

    linechart:code optimized

--- src/module/charts/linechart/line.chart.component.spec.ts
+++ src/module/charts/linechart/line.chart.component.spec.ts
@@ -6,25 +6,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonDataService } from '../../services/data/common.data.service';
 import { ChartLoaderService } from './../chart.loader.service';
 import { ChartTitleComponent } from '../charttitle/chart.title.component';
+import {ChartLegendComponent} from '../chartlegend/chart.legend.component';
 declare var google: any;
 describe('LINE CHART', () => {
     //let ChartTitleComponent=new ChartTitleComponent()
     // let ChartTitleComponent = [{'name':'chart','title':'','position':'','color':'','fontname':'','fontsize':'','bold':false,'italic':''}];
     let linechartcomp: LineChartComponent;
     let charttitlecomp: ChartTitleComponent;
+    let chartlegendcomp: ChartLegendComponent;
     let linefixture: ComponentFixture<LineChartComponent>;
     let charttitlefixture: ComponentFixture<ChartTitleComponent>;
+    let chartlegendfixture:ComponentFixture<ChartLegendComponent>;
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [FormsModule],
-            declarations: [LineChartComponent, ChartTitleComponent],
+            declarations: [LineChartComponent, ChartTitleComponent,ChartLegendComponent],
             providers: [IconLoaderService, CommonDataService, ChartLoaderService]
         });
         linefixture = TestBed.createComponent(LineChartComponent);
         charttitlefixture = TestBed.createComponent(ChartTitleComponent);
+        chartlegendfixture=TestBed.createComponent(ChartLegendComponent);
         linechartcomp = linefixture.componentInstance;
         charttitlecomp = charttitlefixture.componentInstance;
+        chartlegendcomp=chartlegendfixture.componentInstance;
         linechartcomp.chartTitleComponent = charttitlecomp;
+
     });
     it('show chart', () => {
         linechartcomp.showChart = false;
@@ -46,7 +52,17 @@ describe('LINE CHART', () => {
         charttitlecomp.position='';
         charttitlecomp.title='';
         charttitlecomp.italic=false;
-    })
+    });
+    it('chartLegendStyle()',()=>{
+        chartlegendcomp.position='';
+        chartlegendcomp.alignment='';
+        chartlegendcomp.color='';
+        chartlegendcomp.fontname='';
+        chartlegendcomp.fontsize='';
+        chartlegendcomp.bold=false;
+        chartlegendcomp.maxlines;
+    });
+    
     it('get data method',()=>{
         linechartcomp.data;
         expect(linechartcomp.data).toBe(linechartcomp._data);
