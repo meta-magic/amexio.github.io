58356609bf3fcd4b12a13b6e5b96b9a271e378e1
kedar8 <kedar.kokil@metamagic.in>
2018-08-10 16:46:50 +0530

    paginator: code optimization

--- src/module/data/paginator/paginator.component.spec.ts
+++ src/module/data/paginator/paginator.component.spec.ts
@@ -1,6 +1,34 @@
 /**
  * Created by pratik on 11/12/17.
  */
-describe('Button tests', () => {
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { AmexioPaginatorComponent } from './paginator.component';
+import { stringify } from 'querystring';
+
+describe('amexio-paginator', () => {
   it('true is true', () => expect(true).toBe(true));
+  
+  let comp: AmexioPaginatorComponent;
+  let fixture: ComponentFixture<AmexioPaginatorComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [AmexioPaginatorComponent],
+      providers: [IconLoaderService],
+    });
+    fixture = TestBed.createComponent(AmexioPaginatorComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('check variable Definations', () => {
+    expect((<any>comp).fullPageSet.length).toEqual(0);
+    expect((<any>comp).activePages.length).toEqual(0);
+    expect((<any>comp).pageIndex.length).toEqual(0);
+    
+  });
+
 });
