e6b034c9a6ea42a2f735fdb36568f6322b2ba221
kedar8 <kedar.kokil@metamagic.in>
2018-08-13 13:07:34 +0530

    steps: code updated

---
+++ src/module/panes/steps/steps.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { AmexioStepsComponent } from './steps.component';
+import { AmexioIconPaneComponent} from '../icon/icon.component';
+import { toUnicode } from 'punycode';
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
+
+describe('amexio-steps', () => {
+    let comp: AmexioStepsComponent;
+    let fixture: ComponentFixture<AmexioStepsComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [AmexioStepsComponent,AmexioIconPaneComponent],
+            providers: [IconLoaderService],
+        });
+        fixture = TestBed.createComponent(AmexioStepsComponent);
+        comp = fixture.componentInstance;
+    });
+
+
+    it('check onStepClick  method', () => {
+        let clickData: any;
+        let ev: any;
+        comp.onStepClick(clickData,ev);
+        let ok = {event: ev, data: clickData}
+        comp.getStepBlockData.subscribe((g: any) => {
+            expect(ok).toEqual(g);
+        });
+        comp.onClick.subscribe((g: any) => {
+            expect(clickData).toEqual(g);
+        })
+    });
+    // it('ngAfterContentInit method check',() => {
+    //     comp.ngAfterContentInit();
+    //     expect(comp.data.length).toBeGreaterThan(0);
+    // })
+});
