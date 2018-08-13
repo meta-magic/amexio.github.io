f72849b3ad4aa2bef9e46a65928c469709b91a85
kedar8 <kedar.kokil@metamagic.in>
2018-08-13 18:46:23 +0530

    panel: code updated

--- src/module/panes/panel/panel.component.spec.ts
+++ src/module/panes/panel/panel.component.spec.ts
@@ -3,7 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { IconLoaderService } from '../../../index';
 import { AmexioPanelComponent } from './panel.component';
-import { AmexioIconPaneComponent} from '../icon/icon.component';
+import { AmexioStepsComponent } from '../steps/steps.component';
+import { AmexioIconPaneComponent } from '../icon/icon.component';
 import { toUnicode } from 'punycode';
 import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 
@@ -14,21 +15,38 @@ describe('amexio-steps', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [FormsModule],
-            declarations: [AmexioPanelComponent,AmexioIconPaneComponent],
+            declarations: [AmexioPanelComponent, AmexioStepsComponent, AmexioIconPaneComponent],
             providers: [IconLoaderService],
         });
         fixture = TestBed.createComponent(AmexioPanelComponent);
         comp = fixture.componentInstance;
     });
 
-    it('',() => {
+    it('checking css', () => {
 
         (<any>comp).faFaIconUPCss = 'fa fa-caret-up';
         (<any>comp).faFaIconDownCss = 'fa fa-caret-down';
 
-        expect( (<any>comp).faFaIconUPCss).toEqual('fa fa-caret-up');
-        expect( (<any>comp).faFaIconDownCss).toEqual('fa fa-caret-down');
+        expect((<any>comp).faFaIconUPCss).toEqual('fa fa-caret-up');
+        expect((<any>comp).faFaIconDownCss).toEqual('fa fa-caret-down');
+
+    });
+    it('checking ngOnInit method', () => {
+
+        comp.ngOnInit();
+        comp.header = false;
+        expect(comp.header).toEqual(false);
+        comp.expanded = true;
+        expect(comp.expanded).toEqual(true);
+
+        comp.header = true;
+        comp.height = 20;
+        comp.ngOnInit();
+        expect(comp.height).toEqual(20);
         
-    })
+        
+
+    });
+
 });
-    
+
