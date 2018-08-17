2df2adc56f18e5b1326a01044b60497c0670bcb3
ankita202 <ankita.jain@metamgic.in>
2018-08-17 18:42:29 +0530

    Linechart:testcase

--- src/module/charts/linechart/line.chart.component.spec.ts
+++ src/module/charts/linechart/line.chart.component.spec.ts
@@ -133,10 +133,37 @@ describe('LINE CHART', () => {
         const chartbgstyle = linechartcomp.chartBackgroundStyle();
         //const json1 = {"backgroundcolor":null,"left":null,"top":null,"height":50,"width":100}
         //console.log(JSON.stringify(chartbgstyle));
+    });
+    it('create table()', () => {
+        const dupArray = '';
+        const data = '';
+        const labelobject = '';
+        let aa =   [
+            [{"datatype":"number" ,"label":'Day'},{"datatype":"number","label":'Guardians of the Galaxy'},
+              {"datatype":"number","label":'The Avengers'},{"datatype":"number","label":'Transformers: Age of Extinction'}
+            ],
+            [1,  37.8, 80.8, 41.8],
+            [2,  30.9, 69.5, 32.4],
+            [3,  25.4,   57, 25.7],
+            [4,  11.7, 18.8, 10.5],
+            [5,  11.9, 17.6, 10.4],
+            [6,   8.8, 13.6,  7.7],
+            [7,   7.6, 12.3,  9.6],
+            [8,  12.3, 29.2, 10.6],
+            [9,  16.9, 42.9, 14.8],
+            [10, 12.8, 30.9, 11.6],
+            [11,  5.3,  7.9,  4.7],
+            [12,  6.6,  8.4,  5.2],
+            [13,  4.8,  6.3,  3.6],
+            [14,  4.2,  6.2,  3.4]
+         ];
+        linechartcomp.ngOnInit();
+        linechartcomp.createTable(aa);
+
     })
     it('ngOnInit()', () => {
         linechartcomp.ngOnInit();
-        expect(false).toBe( linechartcomp.hasLoaded);
+        expect(false).toBe(linechartcomp.hasLoaded);
         linechartcomp.drawChart();
     });
     it('get data method', () => {
