465eed7e48428b82767d5d2448b8f68b048925aa
kedar8 <kedar.kokil@metamagic.in>
2018-08-20 14:24:24 +0530

    gauge: code updated

---
+++ src/module/dashboard/gaugechart/gauge.chart.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { GaugeChartComponent } from './gauge.chart.component';
+import { toUnicode } from 'punycode';
+import { DashboardLoaderService } from '../chart.loader.service';
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
+
+describe('amexio-gauge-chart-dataPoint', () => {
+    let comp: GaugeChartComponent;
+    let fixture: ComponentFixture<GaugeChartComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [GaugeChartComponent],
+            providers: [IconLoaderService, DashboardLoaderService],
+        });
+        fixture = TestBed.createComponent(GaugeChartComponent);
+        comp = fixture.componentInstance;
+
+    });
+
+    it('check data variable', () => {
+        let data: any;
+
+        expect(comp.data).toBe(comp._data);
+        comp.showChart = true;
+        expect(comp.showChart).toBe(true);
+        comp.showChart = true;
+        expect(comp.showChart).toBe(true);
+
+    });
+});
+
+
