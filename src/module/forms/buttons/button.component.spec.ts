384107e58ece9b533350f30a83d7b4308dc7426a
kedar8 <kedar.kokil@metamagic.in>
2018-08-20 19:16:15 +0530

    button: code updated

--- src/module/forms/buttons/button.component.spec.ts
+++ src/module/forms/buttons/button.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { IconLoaderService } from '../../../index';
 import { AmexioFormIconComponent } from './../icon/icon.component';
 import { AmexioButtonComponent } from './button.component';
 
-describe('amexio-button' , () => {
+describe('amexio-button', () => {
   let comp: AmexioButtonComponent;
   let fixture: ComponentFixture<AmexioButtonComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [FormsModule],
-      declarations: [ AmexioButtonComponent, AmexioFormIconComponent, AmexioButtonComponent],
+      imports: [FormsModule],
+      declarations: [AmexioButtonComponent, AmexioFormIconComponent, AmexioButtonComponent],
       providers: [IconLoaderService],
     });
     fixture = TestBed.createComponent(AmexioButtonComponent);
@@ -25,4 +25,78 @@ describe('amexio-button' , () => {
   //   expect(comp.disabled).toEqual(jasmine.any(false));
   //   //comp.setDisabled();
   // });
+
+
+  it('check method  getBackgroundColor', () => {
+
+    comp.badgeClass();
+    comp.type = 'primary';
+    expect(comp.type).toEqual('primary');
+    comp.type = 'theme-color';
+    expect(comp.type).toEqual('theme-color');
+    let colorCode = 'btn-primary-badge';
+    expect(colorCode).toBe('btn-primary-badge');
+
+
+
+
+    comp.badgeClass();
+    comp.type = 'secondary';
+    expect(comp.type).toEqual('secondary');
+    comp.type = 'theme-backgroundcolor';
+    expect(comp.type).toEqual('theme-backgroundcolor');
+    colorCode = 'btn-secondary-badge';
+    expect(colorCode).toBe('btn-secondary-badge');
+
+    comp.badgeClass();
+    comp.type = 'success';
+    expect(comp.type).toEqual('success');
+    comp.type = 'green';
+    expect(comp.type).toEqual('green');
+    colorCode = 'btn-success-badge';
+    expect(colorCode).toBe('btn-success-badge');
+
+
+
+    comp.badgeClass();
+    comp.type = 'danger';
+    expect(comp.type).toEqual('danger');
+    comp.type = 'red';
+    expect(comp.type).toEqual('red');
+    colorCode = 'btn-danger-badge';
+    expect(colorCode).toBe('btn-danger-badge');
+
+
+
+    comp.badgeClass();
+    comp.type = 'warning';
+    expect(comp.type).toEqual('warning');
+    comp.type = 'yellow';
+    expect(comp.type).toEqual('yellow');
+    colorCode = 'btn-warning-badge';
+    expect(colorCode).toBe('btn-warning-badge');
+
+  });
+
+  it('check method  buttonClick', () => {
+    let btn: any;
+    comp.buttonClick(btn);
+    comp.disabled = false;
+
+    expect(comp.disabled).toEqual(false);
+    comp.onClick.subscribe((g: any) => {
+      expect(btn).toEqual(g);
+    });
+
+    it('check setDisabled method', () => {
+      let dis: any;
+      comp.setDisabled(dis);
+      comp.disabled = true;
+      expect(comp.disabled).toEqual(true);
+    });
+  });
+
+
+
+
 });
