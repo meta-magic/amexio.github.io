ec2cb738d931f051180422650e20d7224e44b3a7
kedar8 <kedar.kokil@metamagic.in>
2018-08-20 20:12:15 +0530

    tab: code updated

--- src/module/panes/tab/tab.component.spec.ts
+++ src/module/panes/tab/tab.component.spec.ts
@@ -1,3 +1,37 @@
-/**
- * Created by pratik on 11/12/17.
- */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { AmexioTabComponent } from './tab.component';
+import { AmexioIconPaneComponent } from '../icon/icon.component';
+import {AmexioTabPillComponent} from './tab.pill.component';
+
+describe('amexio-tab', () => {
+    let comp: AmexioTabComponent;
+    let fixture: ComponentFixture<AmexioTabComponent>;
+    let comData: AmexioTabPillComponent;
+
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [FormsModule],
+                declarations: [AmexioTabComponent, AmexioIconPaneComponent],
+                providers: [IconLoaderService],
+            });
+            fixture = TestBed.createComponent(AmexioTabComponent);
+            comp = fixture.componentInstance;
+        });
+
+    it('check showprev variable ', () => {
+
+        comp.showprev = false;
+        expect(comp.showprev).toEqual(false);
+    });
+
+    it('check showprev variable ', () => {
+        comp.shownext = false;
+            expect(comp.shownext).toEqual(false);
+    });
+
+
+
+});
