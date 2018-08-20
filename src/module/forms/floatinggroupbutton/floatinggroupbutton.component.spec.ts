174c0c097642c1c57e4f798671d6bb574d4a4699
kedar8 <kedar.kokil@metamagic.in>
2018-08-20 16:39:10 +0530

    floatingbuttongroup: code updated

--- src/module/forms/floatinggroupbutton/floatinggroupbutton.component.spec.ts
+++ src/module/forms/floatinggroupbutton/floatinggroupbutton.component.spec.ts
@@ -1,25 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { IconLoaderService } from '../../../index';
 import { AmexioButtonComponent } from './../buttons/button.component';
 import { AmexioFormIconComponent } from './../icon/icon.component';
 import { AmexioFloatingGroupButtonComponent } from './floatinggroupbutton.component';
-
-describe('amexio-floating-group-button' , () => {
+import { AmexioFloatingButtonComponent } from '../floatingbutton/floatingbutton.component'
+describe('amexio-floating-group-button', () => {
   let comp: AmexioFloatingGroupButtonComponent;
   let fixture: ComponentFixture<AmexioFloatingGroupButtonComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [FormsModule],
-      declarations: [ AmexioFloatingGroupButtonComponent, AmexioFormIconComponent, AmexioButtonComponent],
+      imports: [FormsModule],
+      declarations: [AmexioFloatingGroupButtonComponent, AmexioFloatingButtonComponent, AmexioFormIconComponent, AmexioButtonComponent],
       providers: [IconLoaderService],
     });
     fixture = TestBed.createComponent(AmexioFloatingGroupButtonComponent);
     comp = fixture.componentInstance;
   });
 
+
+  it('check variable togglefloatinggroup', () => {
+    comp.togglefloatinggroup = false;
+    expect(comp.togglefloatinggroup).toEqual(false);
+
+    comp.datacount = 0;
+    expect(comp.datacount).toEqual(0);
+  });
+
+  it('check onButtonClick', () => {
+    let node: any;
+    let click1: any;
+
+    comp.onButtonClick(node, click1);
+    comp.togglefloatinggroup = false;
+    expect(comp.togglefloatinggroup).toEqual(false);
+    expect(comp.togglefloatinggroup).not.toBe(true);
+
+    let dat1 = { this: node, parent: this, event: click1 };
+
+    comp.onClick.subscribe((g: any) => {
+      expect(dat1).toEqual(g);
+    });
+  });
+
+  // it('method testing onFloatingButtonClick', () => {
+
+  //   let floatingbtn: any;
+
+  //   comp.onFloatingButtonClick(floatingbtn);
+  //  // comp.onButtonClick(node,floatingbtn);
+
+  // });
+
+ 
 
 
 });
