7fcf91a34c2b8bc08caf01aa02f5cd5cd9b9203e
kedar8 <kedar.kokil@metamagic.in>
2018-08-20 12:43:08 +0530

    box: component updated

--- src/module/layout/box/box.component.spec.ts
+++ src/module/layout/box/box.component.spec.ts
@@ -4,6 +4,7 @@ import { By } from '@angular/platform-browser';
 import { IconLoaderService } from '../../services/icon/icon.service';
 
 import { AmexioButtonComponent } from '../../forms/buttons/button.component';
+import {AmexioFormIconComponent} from '../../forms/icon/icon.component';
 import {AmexioIconLayoutComponent} from '../../layout/icon/icon.component';
 import { AmexioBoxComponent } from './box.component';
 
@@ -14,20 +15,39 @@ describe('amexio-box' , () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports : [FormsModule],
-      declarations: [ AmexioBoxComponent, AmexioIconLayoutComponent, AmexioButtonComponent],
+      declarations: [ AmexioBoxComponent,AmexioFormIconComponent, AmexioIconLayoutComponent, AmexioButtonComponent],
       providers: [IconLoaderService],
     });
     fixture = TestBed.createComponent(AmexioBoxComponent);
     comp = fixture.componentInstance;
   });
 
-  // it('closable check true',() => {
+  it('close check true',() => {
+   
+    comp.close = true;
+    expect(comp.close).toEqual(true);
+  });
 
-  //   expect(comp.closable).toEqual(true);
-  // });
-  // it('check enableclick true', () => {
-  //   // comp.enableclick=true;
-  //   // comp.ngOnInit();
-  //   // expect(comp.enableclick).toEqual(true);
-  // });
+  it('closable check closebox method check',() => {
+    let dt: any;
+    comp.closeBox(dt);
+    comp.close = false;
+    expect(comp.close).toEqual(false);
+    comp.closable = false;
+    expect(comp.closable).toEqual(false);
+  });
+  it('check enableclick true', () => {
+    comp.ngOnInit();
+    comp.borderColor = null;
+    expect(comp.borderColor).toEqual(null);
+
+
+    comp.ngOnInit();
+    comp.borderColor = 'box-default';
+    comp.bgColor = null;
+    expect(comp.bgColor).toBe(null);
+    expect(comp.borderColor).toEqual('box-default');
+    
+    
+  });
 });
