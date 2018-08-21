460017a663494d21119c2ed5b5909d6e43c9d0eb
Manisha1205 <manisha.boddu@metamagic.in>
2018-08-21 12:25:01 +0530

    advancesearch: test case written

--- src/module/forms/advancesearch/searchadvance.component.spec.ts
+++ src/module/forms/advancesearch/searchadvance.component.spec.ts
@@ -1,7 +1,38 @@
-
+import { AmexioSearchAdvanceComponent } from './searchadvance.component';
+import { IconLoaderService } from '../../../index';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef, ElementRef } from '@angular/core';
+import { AmexioButtonComponent } from './../buttons/button.component';
+import { AmexioFormIconComponent } from './../icon/icon.component';
 describe('searchadvance ', () => {
+  let comp: AmexioSearchAdvanceComponent;
+  let fixture: ComponentFixture<AmexioSearchAdvanceComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [AmexioSearchAdvanceComponent, AmexioFormIconComponent, AmexioButtonComponent],
+      providers: [IconLoaderService ]
+    });
+    fixture = TestBed.createComponent(AmexioSearchAdvanceComponent);
+    comp = fixture.componentInstance;
+    event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
+   comp.advanceSearchFlag = false;
+  });
+
+
     it('true is true', () => 
     expect(true).toBe(true));
+
+    it('closeSearchForm()', () => {
+      comp. closeSearchForm();
+      comp.advanceSearchFlag = false;
+      expect(comp.advanceSearchFlag).toEqual(false);
+
+
+    });
+
   });
 
 
--- src/module/forms/advancesearch/searchboxtool.component.spec.ts
+++ src/module/forms/advancesearch/searchboxtool.component.spec.ts
@@ -1,4 +1,38 @@
+import { SearchboxtoolComponent } from './searchboxtool.component';
+import { IconLoaderService } from '../../../index';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef, ElementRef } from '@angular/core';
+import { AmexioButtonComponent } from './../buttons/button.component';
+import { AmexioFormIconComponent } from './../icon/icon.component';
+import { CommonDataService } from '../../services/data/common.data.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
 describe('searchboxtool ', () => {
+  let comp: SearchboxtoolComponent;
+  let fixture: ComponentFixture<SearchboxtoolComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [SearchboxtoolComponent, AmexioFormIconComponent, AmexioButtonComponent],
+      providers: [IconLoaderService,  CommonDataService,HttpClient,HttpHandler]
+    });
+    fixture = TestBed.createComponent(SearchboxtoolComponent);
+    comp = fixture.componentInstance;
+    event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
+   });
+
+   it('set data', () => {
+    let value = '';
+    comp._data = value;
+    expect(comp._data).toEqual(value);
+
+      comp['componentLoaded'] = true;
+      expect(comp['componentLoaded']).toEqual(true);
+      comp.updateComponent();
+  });
+
     it('true is true', () => 
     expect(true).toBe(true));
   });
