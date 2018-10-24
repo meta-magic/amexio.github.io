11c3f9e2132e49501be0c2fc1c81fc32c1c43717
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-10-24 19:54:55 +0530

    tree: Host listner method removed

--- src/module/data/tree/tree.component.ts
+++ src/module/data/tree/tree.component.ts
@@ -507,13 +507,11 @@ description : Context Menu provides the list of menus on right click.
   @HostListener('document:click')
   onWindowClick() {
     this.flag = false;
-    this.setSelectedFlag();
   }
 
   @HostListener('document:scroll')
   onscroll() {
     this.flag = false;
-    this.setSelectedFlag();
   }
 
   loadContextMenu(rightClickData: any) {
