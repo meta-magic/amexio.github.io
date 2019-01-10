84ad1e5bd79201e7ca63ce502d36436761740280
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-01-10 15:14:51 +0530

    toggle: tslint

--- src/module/forms/toggle/toggle.component.ts
+++ src/module/forms/toggle/toggle.component.ts
@@ -96,7 +96,7 @@ description : The label of this field.
 
   ngOnInit() {
 
-    this.componentId =  this.createCompId('togglebtn',this.fieldlabel);
+    this.componentId =  this.createCompId('togglebtn', this.fieldlabel);
 
     if (this.shape === '' || this.shape == null) {
       this.shape = 'round';
