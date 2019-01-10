2f1b0731f569e001bc1d38cd3e0918f061310ad8
ketangote <ketan.gote@metamagic.in>
2019-01-10 17:23:15 +0530

    Changes: Added animation to Accordion Pane

--- src/module/layout/accordion/accordion.pane.ts
+++ src/module/layout/accordion/accordion.pane.ts
@@ -3,76 +3,99 @@
  Component Selector : <amexio-accordion-tab>
  Component Description : Amexio Accordion provides an easy way to organize big forms by grouping the fields in accordion tabs.
 */
-
-import {AfterViewInit, Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import { animate, state, style, transition, trigger} from '@angular/animations';
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'amexio-accordion-tab',
-  templateUrl : './accordion.pane.html',
+  templateUrl: './accordion.pane.html',
+  animations: [
+    trigger('accordionState', [
+      state('hidden', style({
+        display: 'none',
+        height: '0px',
+      })),
+      state('visible', style({
+        display: 'block',
+        height: '*',
+      })),
+      transition('*=>*',  animate('200ms')),
+    ]),
+  ],
 })
 
-export class AmexioAccordionTabComponent  {
+export class AmexioAccordionTabComponent {
 
-/*
-Properties
-name : header
-datatype : any
-version : 4.0 onwards
-default :
-description : User can bind title for accordion tab.*/
+  /*
+  Properties
+  name : header
+  datatype : any
+  version : 4.0 onwards
+  default :
+  description : User can bind title for accordion tab.*/
   @Input() header: any;
-/*
-Properties
-name : active
-datatype : boolean
-version : 4.0 onwards
-default : false
-description : user can Set true for specific accordian open as default
-*/
-@Input() active: boolean;
-/*
-Events
-name : emittedEvent
-datatype :none
-version : none
-default : none
-description : Fires the on accordion pane click event.
-*/
+  /*
+  Properties
+  name : active
+  datatype : boolean
+  version : 4.0 onwards
+  default : false
+  description : user can Set true for specific accordian open as default
+  */
+  @Input() active: boolean;
+  /*
+  Events
+  name : emittedEvent
+  datatype :none
+  version : none
+  default : none
+  description : Fires the on accordion pane click event.
+  */
   @Output() emittedEvent = new EventEmitter();
-/*
-Properties
-name : left-icon
-datatype : string
-version : 4.0 onwards
-default :
-description : places the icon on left
-*/
-@Input('left-icon') leftIcon: string;
-/*
-Properties
-name : angle-icon
-datatype : boolean
-version : 4.0 onwards
-default : false
-description : Can use Angle Icons instead of default plus/minus icons
-*/
+  /*
+  Properties
+  name : left-icon
+  datatype : string
+  version : 4.0 onwards
+  default :
+  description : places the icon on left
+  */
+  @Input('left-icon') leftIcon: string;
+  /*
+  Properties
+  name : angle-icon
+  datatype : boolean
+  version : 4.0 onwards
+  default : false
+  description : Can use Angle Icons instead of default plus/minus icons
+  */
   @Input('angle-icon') angleIcon: boolean;
-/*
-Properties
-name : disabled
-datatype : boolean
-version : 4.0 onwards
-default : false
-description : 	Disabled specific panes
-*/
+  /*
+  Properties
+  name : disabled
+  datatype : boolean
+  version : 4.0 onwards
+  default : false
+  description : 	Disabled specific panes
+  */
   @Input('disabled') disabled: boolean;
-isTransparent: boolean;
-constructor() {
-}
-emitEvent() {
-    if ( !this.disabled ) {
-      this.active = !this.active ;
+  isTransparent: boolean;
+  currentstate: string;
+
+  constructor() {
+    this.currentstate = 'hidden';
+  }
+  emitEvent() {
+    if (!this.disabled) {
+      this.active = !this.active;
       this.emittedEvent.emit(this);
-}
+    }
+
+    if (this.active) {
+      this.currentstate = 'visible';
+    } else {
+      this.currentstate = 'hidden';
+    }
+
   }
 }
