df38645859752fa554db1651d86e3499c563ef54
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-01-11 20:28:42 +0530

    copyright: added

--- src/module/data/datagrid/datagrid.component.ts
+++ src/module/data/datagrid/datagrid.component.ts
@@ -1,4 +1,21 @@
 /*
+* Copyright [2019] [Metamagic]
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+/*
  Component Name : Amexio data grid
  Component Selector : <amexio-datagrid>
  Component Description : Data grid component to render large amount of data-set with
