1c4086bbb42e46e0fa2c8f8989176f138968262e
kedar8 <kedar.kokil@metamagic.in>
2019-02-27 15:27:50 +0530

    progress bar: aria code updated.

--- src/module/data/progress/progress.component.ts
+++ src/module/data/progress/progress.component.ts
@@ -2,102 +2,100 @@
  * Created by pratik on 27/12/17.
  */
 
- /*
- Component Name : Amexio progress bar
- Component Selector : <amexio-progress-bar>
- Component Description : Progress Bar Component Provides up-to-date feedback
- on the progress of a workflow or action with simple yet flexible progress bars
- and easy to configure.
+/*
+Component Name : Amexio progress bar
+Component Selector : <amexio-progress-bar>
+Component Description : Progress Bar Component Provides up-to-date feedback
+on the progress of a workflow or action with simple yet flexible progress bars
+and easy to configure.
 */
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
-  selector: 'amexio-progress-bar', template: `
-    <div *ngIf="showProgress" class="progress" [ngStyle]="{'height':height}">
-      <span [ngClass]="progressclass" *ngIf="infinite" style="width: 100%;">{{label != null ? label : 'Loading...'}}</span>
-      <span [ngClass]="progressclass" *ngIf="!infinite" [ngStyle]="{'width.%' : currentvalue}"></span>
-    </div>
-  `,
+  selector: 'amexio-progress-bar',
+  templateUrl: './progress.component.html',
 })
 export class AmexioProgressBarComponent implements OnInit {
-/*
-Properties
-name : show
-datatype : boolean
-version : 4.0 onwards
-default : true
-description :  Shows / Hides the progress bar.
-*/
+  /*
+  Properties
+  name : show
+  datatype : boolean
+  version : 4.0 onwards
+  default : true
+  description :  Shows / Hides the progress bar.
+  */
   @Input('show') showProgress = true;
-/*
-Properties
-name : infinite
-datatype : boolean
-version : 4.0 onwards
-default : false
-description : Enable/Disable Infinite Mode.
-*/
+  /*
+  Properties
+  name : infinite
+  datatype : boolean
+  version : 4.0 onwards
+  default : false
+  description : Enable/Disable Infinite Mode.
+  */
   @Input() infinite: boolean;
-/*
-Properties
-name : type
-datatype : string
-version : 4.0 onwards
-default :
-description : Type of progress bar can be default,
-theme-color,theme-backgroundcolor, green, red, yellow( primary, secondary,
-  success , danger & warning
-*/
+  /*
+  Properties
+  name : type
+  datatype : string
+  version : 4.0 onwards
+  default :
+  description : Type of progress bar can be default,
+  theme-color,theme-backgroundcolor, green, red, yellow( primary, secondary,
+    success , danger & warning
+  */
   @Input() type: string;
-/*
-Properties
-name : amexio-color
-datatype : string
-version : 4.1 onwards
-default :
-description : Use different inbuilt amexio colors available
-(e.g amexio-black, amexio-red etc)
-*/
+  /*
+  Properties
+  name : amexio-color
+  datatype : string
+  version : 4.1 onwards
+  default :
+  description : Use different inbuilt amexio colors available
+  (e.g amexio-black, amexio-red etc)
+  */
   @Input('amexio-color') amexiocolor: string;
-/*
-Properties
-name : current-value
-datatype : string
-version : 4.0 onwards
-default :
-description : Current Position of progress.
-*/
+  /*
+  Properties
+  name : current-value
+  datatype : string
+  version : 4.0 onwards
+  default :
+  description : Current Position of progress.
+  */
   @Input('current-value') currentvalue: string;
-/*
-Properties
-name : label
-datatype : any
-version : 4.0 onwards
-default :
-description : Custom labels on bar.
-*/
+  /*
+  Properties
+  name : label
+  datatype : any
+  version : 4.0 onwards
+  default :
+  description : Custom labels on bar.
+  */
   @Input() label: any;
-/*
-Properties
-name : height
-datatype : any
-version : 4.0 onwards
-default :
-description : Height of bar.
-*/
+  /*
+  Properties
+  name : height
+  datatype : any
+  version : 4.0 onwards
+  default :
+  description : Height of bar.
+  */
   @Input() height: any;
-/*
-Properties
-name : stripped
-datatype : boolean
-version : 4.0 onwards
-default : false
-description : Bar styled stripped.
-*/
+  /*
+  Properties
+  name : stripped
+  datatype : boolean
+  version : 4.0 onwards
+  default : false
+  description : Bar styled stripped.
+  */
   @Input() stripped: boolean;
-/*
-not in use
- */
+  /*
+  not in use
+   */
   @Input() multi: boolean;
+
+  @Input('tooltip') tooltip: string;
 
   progressclass = '';
 
@@ -109,7 +107,7 @@ not in use
     if (this.height) {
       this.height = this.height + 'px';
     } else {
-      this.height  = '20px';
+      this.height = '20px';
     }
     if (this.stripped) {
       this.progressclass = this.progressclass + this.strippedCss;
