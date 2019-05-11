6eb7a7ece4ea0cdaaeac402adc52805eb808f24d
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-11 17:23:10 +0530

    menu: updated

--- src/module/creative/menu/menu.component.ts
+++ src/module/creative/menu/menu.component.ts
@@ -38,6 +38,7 @@ export class AmexioMenuCEComponent implements OnInit {
 
   @Input('animation-type') type: any;
 
+  @Input('menu-alignment') menuAlignment = 'left';
   animatedClass: any;
 
   ngOnInit() {
