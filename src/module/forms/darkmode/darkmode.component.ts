c6c923cc88f13b340c5766572bfa1cce4c08964b
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-16 15:39:32 +0530

    darkmode: updated

--- src/module/forms/darkmode/darkmode.component.ts
+++ src/module/forms/darkmode/darkmode.component.ts
@@ -5,44 +5,56 @@ import { AmexioToggleComponent } from './../toggle/toggle.component';
     templateUrl: './darkmode.component.html',
 })
 export class DarkmodeComponent implements OnInit {
-
     @Input('field-label') fieldLabel: string;
 
     @Input('shape') shape: 'round';
-    @Input('type') type: any;
+    @Input('type') type = 2;
     @Input('size') size = 'medium';
+    @Input('mode') mode = 'day-night';
+    @Input('data') colorData: any;
+    stepVal = 0;
+    maxValue = 2;
+    sepiaColorCode = '#f4ecd8';
+    sepiaFontColor = '#020202';
 
     themesdata: any = [];
     themeStyles: any[] = [];
 
+    appBackground = '--appBackground';
+    appForeground = '--appForegroundColor';
+    componentBackground = '--componentBGColor';
+    componentForeground = '--componentFontColor';
+
     constructor() {
         this.themesdata = [
             {
-                varName: '--appBackground',
+                varName: this.appBackground,
                 darkValue: '#2d2d2d',
                 themeValue: '#f5f5f5',
             },
             {
-                varName: '--appForegroundColor',
+                varName: this.appForeground,
                 darkValue: '#f5f5f5',
                 themeValue: '#121212',
             },
             {
-                varName: '--componentBGColor',
+                varName: this.componentBackground,
                 darkValue: '#121212',
                 themeValue: '#ffffff',
             },
             {
-                varName: '--componentFontColor',
+                varName: this.componentForeground,
                 darkValue: '#ffffff',
                 themeValue: '#121212',
             },
-            // ],
-            // },
         ];
     }
 
     ngOnInit() {
+        if (this.mode === 'custom') {
+            this.maxValue = this.colorData.length - 1;
+            this.otherMode();
+        }
     }
     onToggleClick(event: any) {
         this.themeStyles = [];
@@ -66,6 +78,44 @@ export class DarkmodeComponent implements OnInit {
             document.documentElement.style.setProperty(style.name, style.value);
         });
 
+    }
+
+    onChange() {
+        if (this.mode === 'sepia') {
+            this.sepiaMode();
+        } else if (this.mode === 'custom') {
+            this.otherMode();
+        }
+    }
+
+    sepiaMode() {
+        if (this.stepVal === 0) {
+            this.onToggleClick(false);
+        }
+
+        if (this.stepVal === 1) {
+            document.documentElement.style.setProperty(this.appBackground, this.sepiaColorCode);
+            document.documentElement.style.setProperty(this.appForeground, this.sepiaFontColor);
+            document.documentElement.style.setProperty(this.componentBackground, this.sepiaColorCode);
+            document.documentElement.style.setProperty(this.componentForeground, this.sepiaFontColor);
+        }
+        if (this.stepVal === 2) {
+            this.onToggleClick(true);
+        }
+    }
+
+    otherMode() {
+        if (this.colorData && this.colorData.length > 0) {
+            this.colorData.forEach((element: any, index: any) => {
+                if (this.stepVal === index) {
+                    document.documentElement.style.setProperty(this.appBackground, element.bgColor);
+                    document.documentElement.style.setProperty(this.appForeground, element.fgColor);
+                    document.documentElement.style.setProperty(this.componentBackground, element.bgColor);
+                    document.documentElement.style.setProperty(this.componentForeground, element.fgColor);
+
+                }
+            });
+        }
     }
 
 }
