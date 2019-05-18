7cc96152bec8017be70e323f8ea5dea4b2814388
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-18 16:29:34 +0530

    slider: issue resolved

--- src/module/forms/slider/slider.component.ts
+++ src/module/forms/slider/slider.component.ts
@@ -228,6 +228,7 @@ description : Triggers when slider reaches the end
     }
 
     if (!this.sliderHandleClick) {
+      debugger;
       this.updateDomData();
       this.handleChange(event);
     }
