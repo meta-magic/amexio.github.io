accf5cc909d188bb6a8f7f105de59ad622d80e10
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-29 17:54:41 +0530

    tree-tab: added and updated suggested changes

--- src/module/enterprise/tree-tab/tree-tab.component.ts
+++ src/module/enterprise/tree-tab/tree-tab.component.ts
@@ -1,20 +1,29 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 @Component({
     selector: 'amexio-tree-tab',
     templateUrl: './tree-tab.component.html',
 })
 
-export class TreeTabComponent implements OnInit {
+export class TreeTabComponent implements OnInit, AfterViewInit {
 
     @Input('data') data: any;
     @Input('data-reader') dataReader: string;
     @ViewChild('tab') tab: ElementRef;
     @Output('nodeClick') nodeClick: any = new EventEmitter<any>();
+    @Output('onLoad') onLoad: any = new EventEmitter<any>();
+    @Input('width') width: string;
+
     emitData: any = {};
     constructor() { }
 
     ngOnInit() {
     }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+          this.onLoad.emit(this.tab);
+        }, 500);
+      }
 
     addtab(event: any) {
         this.emitData['data'] = event;
