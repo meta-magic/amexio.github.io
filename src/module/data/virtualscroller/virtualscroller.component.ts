b35d809625ef42b7554391ff9dd3921c06ae131b
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-06-07 21:43:04 +0530

    update:scroller

--- src/module/data/virtualscroller/virtualscroller.component.ts
+++ src/module/data/virtualscroller/virtualscroller.component.ts
@@ -1,105 +1,156 @@
-import { Component , ContentChild, ElementRef, EventEmitter, Input, NgZone, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
+import { Component , ContentChild, ElementRef, EventEmitter, Input, OnDestroy,
+  OnInit, Output, Renderer2, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'amexio-virtual-scroller',
-  templateUrl: 'virtualscroller.component.html',
-  styles: [`#scroll-content>div {
-    height: 40px;
-    margin-bottom: 5px;
-  }`],
+ selector: 'amexio-virtual-scroller',
+ templateUrl: 'virtualscroller.component.html',
 })
-export class AmexioVirtualScrollerComponent implements OnInit {
+export class AmexioVirtualScrollerComponent implements OnInit, OnDestroy {
 
-  @Input('height') height: number;
+ @Input('height') height: number;
 
-  @Input('data') data: any[];
+ @Input('data') data: any[];
 
-  @Input('item-size') itemSize: number;
+ @Input('item-size') itemSize: number;
 
-  @Output() onScrollDown: any = new EventEmitter<any>();
+ @Input('window-scroll') windowScroll = true;
 
-  @ContentChild('amexioBodyTmpl') bodyTemplate: TemplateRef<any>;
+ @Output() onScrollDown: any = new EventEmitter<any>();
 
-  @ViewChild('virtualscoller') public virtualscoller: ElementRef;
+ @ContentChild('amexioBodyTmpl') bodyTemplate: TemplateRef<any>;
 
-  currentViewData: any[] = [];
-  totalCount: number;
-  pageSize: number;
-  isOverloaded = false;
-  constructor(public ngZone: NgZone) {}
-  ngOnInit() {
-    this.pageAndTotalCountCal();
-    this.initLoad();
-  }
+ @ViewChild('virtualscoller') public virtualscoller: ElementRef;
 
-  // TO CALCULATE PAGE AND TOTAL SIZE
-  private pageAndTotalCountCal() {
-    if (this.data && this.height && this.itemSize && this.data.length > 0) {
-      this.totalCount = this.data.length;
-      this.pageSize = Math.round(this.height / this.itemSize);
-    }
-  }
+ windowscrolllistener: any;
+ divscrolllistener: any;
+ currentViewData: any[] = [];
+ totalCount: number;
+ pageSize: number;
+ isOverloaded = false;
+ show = true;
+ newheight: string;
+ constructor(private renderer: Renderer2) {
+   this.newheight = '100%';
+ }
 
-  // TO INIT LOAD DATA
-  private initLoad() {
-    this.ngZone.runOutsideAngular(() => {
-      requestAnimationFrame(() => {
-        if (this.currentViewData && this.currentViewData.length === 0) {
-          for (let i = 0; i < this.pageSize; i++) {
-            if (this.data && i < this.totalCount) {
-              this.currentViewData.push(this.data[i]);
-            }
-          }
-        }
-      });
-    });
-  }
+ ngOnInit() {
+   setTimeout(() => {
+     this.pageAndTotalCountCal();
+     this.initLoad();
+     this.bindDocumentClickListener();
+   }, 500);
+ }
 
-  // ON SCROLL HANDLE
-  // tslint:disable-next-line:cognitive-complexity
-  scrollHandler(event: any) {
-    const scroller = this.virtualscoller.nativeElement;
-    const height = scroller.clientHeight;
-    const scrollHeight = scroller.scrollHeight - height;
-    const scrollTop = scroller.scrollTop;
-    const percent = Math.floor((scrollTop / scrollHeight) * 100);
-    this.isOverloaded = false;
-    if (percent >= 80 && percent <= 100) {
-      this.isOverloaded = true;
-      // EMIT EVENT WHEN TOTAL RECORDS ARE SHOWN
-      if (this.currentViewData.length === this.totalCount - 1) {
-        this.onScrollDown.emit();
-      }
-    }
-    if (
-      this.isOverloaded &&
-      this.currentViewData &&
-      this.currentViewData.length > 0 &&
-      (this.currentViewData.length + this.pageSize) <= this.totalCount
-    ) {
-      const currentViewDataLength = this.currentViewData.length;
-      const currentPageSize = currentViewDataLength + this.pageSize;
-      for (let i = currentViewDataLength; i < currentPageSize; i++) {
+ // TO CALCULATE PAGE AND TOTAL SIZE
+ private pageAndTotalCountCal() {
+   const el = this.virtualscoller.nativeElement;
+   this.height = this.windowScroll ? el.clientHeight : this.height;
+   if (this.data && this.height && this.itemSize && this.data.length > 0) {
+     this.totalCount = this.data.length;
+     this.pageSize = Math.round(this.height / this.itemSize);
+   }
+   this.newheight = this.height + 'px';
+ }
+
+ // TO INIT LOAD DATA
+ private initLoad() {
+   if (this.currentViewData && this.currentViewData.length === 0) {
+     for (let i = 0; i < this.pageSize; i++) {
+       if (this.data && i < this.totalCount) {
+         this.currentViewData.push(this.data[i]);
+       }
+     }
+   }
+ }
+
+ // ON SCROLL HANDLE
+ handleDivScrollListener(event: any) {
+   if (!this.windowScroll) {
+     const scroller = this.virtualscoller.nativeElement;
+     const height = scroller.clientHeight;
+     const scrollHeight = scroller.scrollHeight - height;
+     const scrollTop = scroller.scrollTop;
+     const percent = Math.floor((scrollTop / scrollHeight) * 100);
+     this.isOverloaded = false;
+     this.loadNewData(percent);
+   }
+ }
+
+ //  TO LAOD NEW DATA ONCE SCROLL IS 80 to 90 %
+// tslint:disable-next-line:cognitive-complexity
+private loadNewData(percent: any) {
+   if (percent >= 80 && percent <= 100) {
+     this.isOverloaded = true;
+     // EMIT EVENT WHEN TOTAL RECORDS ARE SHOWN
+     if (this.currentViewData.length === this.totalCount - 1) {
+       this.onScrollDown.emit();
+     }
+   }
+   if (
+     this.isOverloaded &&
+     this.currentViewData &&
+     this.currentViewData.length > 0 &&
+     (this.currentViewData.length + this.pageSize) <= this.totalCount
+   ) {
+     const currentViewDataLength = this.currentViewData.length;
+     const currentPageSize = currentViewDataLength + this.pageSize;
+     for (let i = currentViewDataLength; i < currentPageSize; i++) {
+       if (i <= this.totalCount) {
+         this.currentViewData.push(this.data[i]);
+       }
+     }
+   }
+   if ((this.totalCount - this.currentViewData.length) < this.pageSize) {
+      for (let i = this.currentViewData.length; i < this.totalCount ; i++) {
         if (i <= this.totalCount) {
           this.currentViewData.push(this.data[i]);
         }
       }
     }
-    if ((this.totalCount - this.currentViewData.length) < this.pageSize) {
-       for (let i = this.currentViewData.length; i < this.totalCount ; i++) {
-         if (i <= this.totalCount) {
-           this.currentViewData.push(this.data[i]);
-         }
-       }
-     }
+ }
+ // TO ADD DYAMIC NEW RECORDS
+ addData(newData: any) {
+   if (newData && newData.length > 0) {
+     newData.forEach((record: any) => {
+       this.data.push(record);
+     });
+   }
+ }
+// Listener FOR WINDOW
+ bindDocumentClickListener() {
+   if (this.windowScroll) {
+        this.windowscrolllistener = this.renderer
+                                         .listen('window', 'scroll', (event: any) => this.handleDocumentListener(event));
   }
+}
+// UNBIND DOCUMENT
+unbindDocumentClickListener() {
+ if (this.windowscrolllistener) {
+     this.bindDocumentClickListener();
+     this.windowscrolllistener = null;
+ }
+}
+//  TO HANDEL DOCUMENT LISTNER
+private handleDocumentListener(event: any) {
+  if (this.windowScroll) {
+   const percent = Math.floor(this.getScrollPercent());
+   this.isOverloaded = false;
+   this.loadNewData(percent);
+  }
+}
+// GET WINDOW GET SCROLLER PERCENTAGE
+private getScrollPercent() {
+ // tslint:disable-next-line:one-variable-per-declaration
+ const h = document.documentElement,
+     b = document.body,
+     st = 'scrollTop',
+     sh = 'scrollHeight';
+ return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+}
 
-  // TO ADD DYAMIC NEW RECORDS
-  addData(newData: any) {
-    if (newData && newData.length > 0) {
-      newData.forEach((record: any) => {
-        this.data.push(record);
-      });
-    }
-  }
+// TO DESTROY
+ngOnDestroy(): void {
+  this.unbindDocumentClickListener();
+}
+
 }
