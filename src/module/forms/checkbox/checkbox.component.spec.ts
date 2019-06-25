c53e1a8b2f3b64181d21826a327c6ae0bdf2606e
ankita202 <ankita.jain@metamgic.in>
2019-06-25 18:43:00 +0530

    Test:checkbox

--- src/module/forms/checkbox/checkbox.component.spec.ts
+++ src/module/forms/checkbox/checkbox.component.spec.ts
@@ -1,11 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }              from '@angular/platform-browser';
-import { DebugElement }    from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {AmexioCheckBoxComponent} from "./checkbox.component";
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { AmexioCheckBoxComponent } from "./checkbox.component";
 
 
-describe('amexio-checkbox' , ()=> {
+describe('amexio-checkbox', () => {
   let comp: AmexioCheckBoxComponent;
   let fixture: ComponentFixture<AmexioCheckBoxComponent>;
   let de: DebugElement;    // => Handle to to Components DOM instance
@@ -13,8 +13,8 @@ describe('amexio-checkbox' , ()=> {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [FormsModule],
-      declarations: [ AmexioCheckBoxComponent ], // declare the test component
+      imports: [FormsModule],
+      declarations: [AmexioCheckBoxComponent], // declare the test component
     });
 
     fixture = TestBed.createComponent(AmexioCheckBoxComponent);  // => Fixture Creates the environment surrounding the component & the has access to the instance itself.
@@ -26,14 +26,29 @@ describe('amexio-checkbox' , ()=> {
   });
 
   //Check if correct field label is applied
-  it('should display correct label',()=>{
+  it('should display correct label', () => {
     let labelValue = 'UserName';
     comp.fieldlabel = labelValue;     // => Set the field label
     fixture.detectChanges();        // Fire Change
     expect(el.textContent).toContain(labelValue);  // check ?
   });
 
-  // More test cases here
+  // it('callback method', () => {
+  //   comp.onBlur();
+  //   comp.tabFocus = false;
+  //   comp['onTouchedCallback']();
+  // });
+  it('tab Focus varible', () => {
+   comp.tabFocus = false;
+  });
+
+  // it('toogle method', () =>{
+  //   comp.isChecked()
+  // })
+  it('ngOnInit', () =>{
+    comp.componentId =comp.createCompId('checkbox',comp.name);
+  })
+
 
 
 });
