0ad65144e384ccffb725c1cc48eda7fcc7023232
ankita202 <ankita.jain@metamgic.in>
2019-06-25 12:58:33 +0530

    Test:tree  commited

--- src/module/data/tree/tree.component.spec.ts
+++ src/module/data/tree/tree.component.spec.ts
@@ -1,3 +1,27 @@
-/**
- * Created by pratik on 11/12/17.
- */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { IconLoaderService } from '../../../index';
+import { AmexioTreeViewComponent } from './tree.component';
+import {AmexioContextMenuComponent} from '../../base/base.contextmenu.component';
+import { CommonDataService } from '../../services/data/common.data.service';
+import {HttpClient,HttpHandler} from '@angular/common/http';
+describe('amexio-treeview', () => {
+    let comp: AmexioTreeViewComponent;
+    let fixture: ComponentFixture<AmexioTreeViewComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [AmexioTreeViewComponent,AmexioContextMenuComponent],
+            providers: [IconLoaderService, CommonDataService, HttpClient, HttpHandler],
+        });
+        fixture = TestBed.createComponent(AmexioTreeViewComponent);
+        comp = fixture.componentInstance;
+    });
+    it('true is true', () => expect(true).toBe(true));
+    it('get data', () => {
+        expect(comp.data).toEqual(comp._data);
+    });
+});
+
+
