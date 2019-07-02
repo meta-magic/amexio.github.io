ec1d8996ddad73173811a41d5dc62a76ecc8ef20
Manisha1205 <manisha.boddu@metamagic.in>
2019-07-02 14:20:47 +0530

    navmenu: test case added

--- src/module/navigation/navbar/navmenu.component.spec.ts
+++ src/module/navigation/navbar/navmenu.component.spec.ts
@@ -13,6 +13,8 @@ describe('navmenu', () => {
         });
         fixture = TestBed.createComponent(AmexioNavMenuComponent);
         comp = fixture.componentInstance;
+        event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
+
     });
 
     it('check variable in navmenu', () => {
@@ -29,4 +31,78 @@ describe('navmenu', () => {
         expect(comp.mobilemode).toBe(flag);
        
       });
+
+      it('dataObject() method check', () => {
+          let n;
+         comp.dataObject(n, event);
+         return { data: n, event: event };
+      });
+
+      it('onHeaderClick() method check', () => {
+       comp.onHeaderClick(event);
+       const node = {
+        header: true,
+        title: comp.title,
+        icon: comp.icon,
+      };
+      comp.mobileToggleModel = !comp.mobileToggleModel;
+      comp.mobilemode = true;
+      comp.showMenus = true;
+      expect(comp.mobilemode).toEqual(true);
+      comp.showMenus = !comp.showMenus;
+      comp.onClick(node, event);
+     });
+
+     it('onMouseOver() method check', () => {
+        comp.mobilemode = true;
+        comp.onMouseOver(event);
+        // if (this.mobilemode) {
+        expect(comp.mobilemode).toEqual(true);
+        comp.showMenus = true;
+        return;
+    });
+
+    it('onMouseLeave() method check', () => {
+        comp.mobilemode = true;
+       comp.onMouseLeave(event);
+       expect(comp.mobilemode).toEqual(true);
+       comp.showMenus = false;
+       return;
+    });
+
+    it('toggleMenu() method check', () => {
+        comp.mobilemode = true;
+        comp.toggleMenu(event);
+        expect(comp.mobilemode).toEqual(true);
+        comp.showMenus = !comp.showMenus;
+      });
+
+    //   it('onMouseoverTitle() method check', () => {
+    //     comp.onMouseoverTitle(event);
+    //         comp.position = comp.getMenuPosition(event);
+    //    });
+
+    //    it(' getMenuPosition() method check', () => {
+    //     const fixture = TestBed.createComponent(AmexioNavMenuComponent);
+    //     fixture.detectChanges();
+    //     const compiled = fixture.debugElement.nativeElement;
+
+    //         comp.getMenuPosition(fixture);
+    //         const remainingleft = compiled.getBoundingClientRect().left;
+    // const remainingright = window.screen.width - compiled.getBoundingClientRect().right;
+    // let directionflag: string;
+    //    });
+       
+       it('onIconClick() method check', () => {
+        let node ={
+            submenus:[{submenu: 's'}, {submenu: 'u'}],
+            isExpanded: false
+          }
+        comp.onIconClick(event, node);
+        event.stopPropagation();
+        // if (node.hasOwnProperty('isExpanded')) {
+        expect(node.hasOwnProperty('isExpanded')).toEqual(true);
+            node.isExpanded = !node.isExpanded;
+            node['isExpanded'] = true;
+        });  
 });
