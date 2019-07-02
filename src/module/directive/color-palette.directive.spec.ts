9d3ce34d8d8b7183c742b03065cd0edbc91b031c
ankita202 <ankita.jain@metamgic.in>
2019-07-02 12:29:20 +0530

    Color-Palette:test

--- src/module/directive/color-palette.directive.spec.ts
+++ src/module/directive/color-palette.directive.spec.ts
@@ -1,8 +1,43 @@
 import { ColorPaletteDirective } from './color-palette.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { ViewContainerRef } from '@angular/core';
 
-describe('ColorPaletteDirective', () => {
+// fdescribe('ColorPaletteDirective', () => {
+//   let comp: ColorPaletteDirective;
+//   let fixture: ComponentFixture<ColorPaletteDirective>;
+//   let service: ColorPaletteDirective;
+//   let _viewContainerRef: ViewContainerRef;
+//   let _httpClient: HttpClient
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [FormsModule],
+//       declarations: [ColorPaletteDirective],
+//     });
+//     fixture = TestBed.createComponent(ColorPaletteDirective);
+//     comp = fixture.componentInstance;
+//   });
+//   it('should create an instance', () => {
+//     service = new ColorPaletteDirective(_viewContainerRef, _httpClient);
+//   });
+//   it('check variable themejson', () => {
+//     comp.ngOnInit();
+//     comp.themejson
+//   })
+// });
+describe('Directive: Color', () => {
+  let comp: ColorPaletteDirective;
+  let fixture: ComponentFixture<ColorPaletteDirective>;
+  let service: ColorPaletteDirective;
+  let _viewContainerRef: ViewContainerRef;
+  let _httpClient: HttpClient
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ColorPaletteDirective]
+    });
+  });
   it('should create an instance', () => {
-    // const directive = new ColorPaletteDirective();
-    // expect(directive).toBeTruthy();
+    service = new ColorPaletteDirective(_viewContainerRef, _httpClient);
   });
 });
