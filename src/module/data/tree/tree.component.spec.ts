929a6b8fa6d87f5a63876fcdbb83b9a33231a5e3
kedar8 <kedar.kokil@metamagic.in>
2019-07-02 12:47:27 +0530

    tree: unit testing.

--- src/module/data/tree/tree.component.spec.ts
+++ src/module/data/tree/tree.component.spec.ts
@@ -2,9 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { IconLoaderService } from '../../../index';
 import { AmexioTreeViewComponent } from './tree.component';
-import {AmexioContextMenuComponent} from '../../base/base.contextmenu.component';
+import { AmexioContextMenuComponent } from '../../base/base.contextmenu.component';
 import { CommonDataService } from '../../services/data/common.data.service';
-import {HttpClient,HttpHandler} from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 describe('amexio-treeview', () => {
     let comp: AmexioTreeViewComponent;
     let fixture: ComponentFixture<AmexioTreeViewComponent>;
@@ -12,16 +12,82 @@ describe('amexio-treeview', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [FormsModule],
-            declarations: [AmexioTreeViewComponent,AmexioContextMenuComponent],
+            declarations: [AmexioTreeViewComponent, AmexioContextMenuComponent],
             providers: [IconLoaderService, CommonDataService, HttpClient, HttpHandler],
         });
         fixture = TestBed.createComponent(AmexioTreeViewComponent);
         comp = fixture.componentInstance;
+        event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
+
     });
     it('true is true', () => expect(true).toBe(true));
     it('get data', () => {
         expect(comp.data).toEqual(comp._data);
     });
+
+    //    it('set data method',() => {
+    //     (<any>comp)['componentLoaded'] = true;  
+    //     (<any>comp).data();
+
+    //       expect((<any>comp)['componentLoaded'] ).toEqual(true);
+    //         expect((<any>comp).updateComponent()).toHaveBeenCalled;
+    //       });
+
+
+    //   it('onClick() on method call', () => {
+    //     let node: any;
+    //     node.extend = true;
+    //     comp.onClick(node);
+    //     expect(node.extend).toEqual(true);
+    //     node.extend = false;
+    //     comp.onClick(node);
+    //     expect(node.extend).toEqual(false);
+    //   });
+
+
+    // it('onArrowDown method', () => {
+
+    //     let data = {
+    //         "item": [{
+    //             "text": "Web App",
+    //             "expand": true,
+    //             "children": [
+    //                 {
+    //                     "text": "app",
+    //                     "expand": true,
+    //                     "children": [
+    //                         {
+    //                             "leaf": true,
+    //                             "text": "Application.js"
+    //                         }
+    //                     ]
+    //                 }
+    //             ]
+    //         }]
+    //     };
+    //     let event = data;
+    //     let node = data;
+    //     let index = 0;
+    //     comp.onArrowDown(event, data, node, index);
+    //     const incrementindex = index + 1;
+    //     expect(incrementindex).toBe(1);
+    //     // const itemid = data[incrementindex];
+
+    //     // node.expand = true;
+    //     // expect(node.expand).toBe(true);
+
+
+    // });
+
+    it('onTreeNodeCheck() on method call', () => {
+
+        let data = comp.data;
+        comp.onTreeNodeCheck(data);
+        comp.onTreeNodeChecked.subscribe((g: any) => {
+            expect(comp.onTreeNodeChecked).toEqual(g);
+        });
+    });
+
 });
 
 
