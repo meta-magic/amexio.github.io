3fb8e30b1d83b10371521fb87bb33af32eca5577
kedar8 <kedar.kokil@metamagic.in>
2019-07-03 17:43:36 +0530

    tree: unit test

--- src/module/data/tree/tree.component.spec.ts
+++ src/module/data/tree/tree.component.spec.ts
@@ -182,13 +182,17 @@ describe('amexio-treeview', () => {
             expect(i['active']).toEqual(true);
         }
 
-        // comp.activateNode(LocalData2, node);
-        // for (const i of LocalData2) {
-        //     i['active'] = false;
-        //     expect(i.item['children']).toBe('children');
-        //     expect(i.item[0]).not.toEqual(node);
-        //     expect(i['active']).toEqual(true);
-        // }
+        comp.activateNode(LocalData2, node);
+        for (const i of LocalData2) {
+            i['active'] = false;
+            expect(i.item[0]).not.toEqual(node);
+            expect(i['active']).toEqual(false);
+            i.item['children']= true;
+            expect(i.item['children']).toBe(true);
+            // comp.activateNode(i[comp.childarraykey], node);
+
+        }
+        
 
     });
 
