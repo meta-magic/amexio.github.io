7f133adc4b4fbed3d0142eb3ee47cce5ee120d0b
medha410 <medha.jadhav@metamagic.in>
2019-07-03 16:01:30 +0530

    navmenu: test case

--- src/module/navigation/navbar/navmenu.component.spec.ts
+++ src/module/navigation/navbar/navmenu.component.spec.ts
@@ -14,7 +14,7 @@ describe('navmenu', () => {
         fixture = TestBed.createComponent(AmexioNavMenuComponent);
         comp = fixture.componentInstance;
         event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
-
+        
     });
 
     it('check variable in navmenu', () => {
@@ -49,34 +49,43 @@ describe('navmenu', () => {
       comp.mobilemode = true;
       comp.showMenus = true;
       expect(comp.mobilemode).toEqual(true);
-      comp.showMenus = !comp.showMenus;
+      expect(comp.showMenus).toEqual(true);
       comp.onClick(node, event);
      });
 
      it('onMouseOver() method check', () => {
         comp.mobilemode = true;
+        comp.showMenus = true;
         comp.onMouseOver(event);
         // if (this.mobilemode) {
         expect(comp.mobilemode).toEqual(true);
-        comp.showMenus = true;
-        return;
+        expect(comp.showMenus).toEqual(true);
     });
 
     it('onMouseLeave() method check', () => {
         comp.mobilemode = true;
+        comp.showMenus = false;
        comp.onMouseLeave(event);
        expect(comp.mobilemode).toEqual(true);
-       comp.showMenus = false;
-       return;
+       expect(comp.showMenus).toEqual(false);
     });
 
     it('toggleMenu() method check', () => {
         comp.mobilemode = true;
+        comp.showMenus = false;
         comp.toggleMenu(event);
         expect(comp.mobilemode).toEqual(true);
-        comp.showMenus = !comp.showMenus;
+        expect(comp.showMenus).toEqual(true);
       });
 
+      it('navItem click method', () => {
+      let  event1 = {
+            'event':event
+        }
+        comp.navItemClick(event1);   
+        event1.event.stopPropagation(); 
+        comp.onNavItemClick.emit(event);
+      });
     //   it('onMouseoverTitle() method check', () => {
     //     comp.onMouseoverTitle(event);
     //         comp.position = comp.getMenuPosition(event);
