ac502278e1ffdff44e0595d10ea25d3b9bdd8a89
kedar8 <kedar.kokil@metamagic.in>
2019-07-04 12:56:41 +0530

    bar-chart: test cases

--- src/module/charts/barchart/bar.chart.component.spec.ts
+++ src/module/charts/barchart/bar.chart.component.spec.ts
@@ -1,31 +1,23 @@
-import { BarChartComponent} from './bar.chart.component';
-//import { AmexioFormIconComponent } from '../icon/icon.component';
+import { BarChartComponent } from './bar.chart.component';
 import { FormsModule } from '@angular/forms';
-import { IconLoaderService } from '../../../index'
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CommonDataService } from '../../services/data/common.data.service';
 import { ChartLoaderService } from './../chart.loader.service';
 import { ChartTitleComponent } from '../charttitle/chart.title.component';
 import { ChartLegendComponent } from '../chartlegend/chart.legend.component';
 import { ChartAreaComponent } from '../chartarea/chart.area.component';
 declare var google: any;
 describe('BAR CHART', () => {
-    //let ChartTitleComponent=new ChartTitleComponent()
-    // let ChartTitleComponent = [{'name':'chart','title':'','position':'','color':'','fontname':'','fontsize':'','bold':false,'italic':''}];
     let barchartcomp: BarChartComponent;
     let charttitlecomp: ChartTitleComponent;
     let chartlegendcomp: ChartLegendComponent;
     let chartareacomp: ChartAreaComponent;
-    let chartAreaArray:ChartAreaComponent[];
-    let chartLegendArray: ChartLegendComponent[];
-    let chartTitleComponent:  ChartTitleComponent[];
     let linefixture: ComponentFixture<BarChartComponent>;
     let charttitlefixture: ComponentFixture<ChartTitleComponent>;
     let chartlegendfixture: ComponentFixture<ChartLegendComponent>;
     let chartareafixture: ComponentFixture<ChartAreaComponent>;
 
-    let chartAreaArray2: ChartAreaComponent [];
-    let chartLegendArray2: ChartLegendComponent []; 
+    let chartAreaArray2: ChartAreaComponent[];
+    let chartLegendArray2: ChartLegendComponent[];
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -44,8 +36,8 @@ describe('BAR CHART', () => {
         chartareacomp = chartareafixture.componentInstance;
 
         barchartcomp.chartTitleComponent = charttitlecomp;
-        barchartcomp.chartLengendComponent=chartlegendcomp;
-        barchartcomp.chartAreaComponent=chartareacomp;
+        barchartcomp.chartLengendComponent = chartlegendcomp;
+        barchartcomp.chartAreaComponent = chartareacomp;
 
         chartAreaArray2 = [];
         chartAreaArray2.push(chartareacomp);
@@ -53,19 +45,19 @@ describe('BAR CHART', () => {
         chartLegendArray2 = [];
         chartLegendArray2.push(chartlegendcomp);
 
+        barchartcomp.showChart = false;
+
     });
     it('show chart', () => {
-        barchartcomp.showChart = false;
+
         charttitlecomp.title = '';
-        expect(false).toBe( barchartcomp.showChart);
+        expect(false).toBe(barchartcomp.showChart);
         let newdata = [{ 'name': 'linechart' }];
         barchartcomp.data = newdata;
     });
-    
+
     it('dont show chart', () => {
-        let newdata;
-        barchartcomp.data = newdata;
-        expect(false).toBe(barchartcomp.showChart);
+        expect(barchartcomp.showChart).toBe(false);
     });
     it('chartTitleTextStyle() properties', () => {
         charttitlecomp.color = '';
@@ -83,7 +75,7 @@ describe('BAR CHART', () => {
         chartlegendcomp.fontname = '';
         chartlegendcomp.fontsize = '';
         chartlegendcomp.bold = false;
-        chartlegendcomp.maxlines=null;
+        chartlegendcomp.maxlines = null;
     });
     it('createChartBar() properties', () => {
         chartareacomp.chartbackgroundcolor = '';
@@ -93,7 +85,7 @@ describe('BAR CHART', () => {
         chartareacomp.topposition = null;
 
     });
-  
+
     it('drawchart()', () => {
         barchartcomp.drawChart();
         barchartcomp.chartTitleComponent;
@@ -106,61 +98,73 @@ describe('BAR CHART', () => {
     it('chartTitleTextStyle()', () => {
         barchartcomp.chartTitleComponent;
         barchartcomp.chartTitleComponent.color = 'red';
-        barchartcomp.chartTitleComponent.fontname='times new roman';
-        barchartcomp.chartTitleComponent.fontsize=5;
-        barchartcomp.chartTitleComponent.bold=true;
-        barchartcomp.chartTitleComponent.italic=true;
-        const charttextstyle= barchartcomp.chartTitleTextStyle();
+        barchartcomp.chartTitleComponent.fontname = 'times new roman';
+        barchartcomp.chartTitleComponent.fontsize = 5;
+        barchartcomp.chartTitleComponent.bold = true;
+        barchartcomp.chartTitleComponent.italic = true;
+        barchartcomp.chartTitleTextStyle();
     });
-    
-    it('chartLegendStyle()',()=>{
-        barchartcomp.chartLengendComponent.position='left';
-        barchartcomp.chartLengendComponent.maxlines=5;
-        barchartcomp.chartLengendComponent.color='black';
-        barchartcomp.chartLengendComponent.fontsize='12';
-        barchartcomp.chartLengendComponent.alignment='center';
-        barchartcomp.chartLengendComponent.fontname='times';
-        barchartcomp.chartLengendComponent.bold=true;
-        const chartlegendstyle= barchartcomp.createChartLegend();
+
+    it('chartLegendStyle()', () => {
+        barchartcomp.chartLengendComponent.position = 'left';
+        barchartcomp.chartLengendComponent.maxlines = 5;
+        barchartcomp.chartLengendComponent.color = 'black';
+        barchartcomp.chartLengendComponent.fontsize = '12';
+        barchartcomp.chartLengendComponent.alignment = 'center';
+        barchartcomp.chartLengendComponent.fontname = 'times';
+        barchartcomp.chartLengendComponent.bold = true;
+        barchartcomp.createChartLegend();
 
     })
-    it('chartBackgroundStyle()',()=>{
-        barchartcomp.chartAreaComponent.chartbackgroundcolor='red';
-        barchartcomp.chartAreaComponent.chartheight=50;
-        barchartcomp.chartAreaComponent.chartwidth=100;
-        barchartcomp.chartAreaComponent.leftposition=null;
-        const chartbgstyle =  barchartcomp.createChartBar();
+    it('chartBackgroundStyle()', () => {
+        barchartcomp.chartAreaComponent.chartbackgroundcolor = 'red';
+        barchartcomp.chartAreaComponent.chartheight = 50;
+        barchartcomp.chartAreaComponent.chartwidth = 100;
+        barchartcomp.chartAreaComponent.leftposition = null;
+        barchartcomp.createChartBar();
     })
 
     it('get data method', () => {
         barchartcomp.data;
-        expect( barchartcomp.data).toBe( barchartcomp._data);
+        expect(barchartcomp.data).toBe(barchartcomp._data);
     });
-    it('ngOnInit()',()=>{
+    it('ngOnInit()', () => {
         expect(barchartcomp.hasLoaded).toBeUndefined;
-        //barchartcomp.ngOnInit();
+        barchartcomp.ngOnInit();
     })
+
 
     it('Draw Chart Test', () => {
         const script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://www.gstatic.com/charts/loader.js';
-        script.async = true;
+        script.async = false;
         script.defer = true;
         script.onload = () => {
             barchartcomp.showChart = true;
-            let newdata = [{ name: 'linechart' }];
+            const newdata = [{ name: 'linechart' }];
             barchartcomp.data = newdata;
             barchartcomp.drawChart();
-            expect(false).toBe( barchartcomp.hasLoaded);
-        }
+            expect(false).toBe(barchartcomp.hasLoaded);
+        };
     });
-    it('onResize()',()=>{
-        barchartcomp.onResize(ComponentFixture);
-        barchartcomp.drawChart();
-      });
-      
-}); 
+    it('onResize()', () => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://www.gstatic.com/charts/loader.js';
+        script.async = false;
+        script.defer = true;
+        script.onload = () => {
+            barchartcomp.showChart = true;
+            const newdata = [{ name: 'linechart' }];
+            barchartcomp.data = newdata;
+            barchartcomp.onResize(ComponentFixture);
+            barchartcomp.drawChart();
+            expect(false).toBe(barchartcomp.hasLoaded);
+        };
+    });
+
+});
 
 
 
