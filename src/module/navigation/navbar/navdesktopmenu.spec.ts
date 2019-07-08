f04786a71a2cfdce337e51ab3399582c02836f02
ankita202 <ankita.jain@metamgic.in>
2019-07-08 18:31:16 +0530

    navdesktopmenu:test

--- src/module/navigation/navbar/navdesktopmenu.spec.ts
+++ src/module/navigation/navbar/navdesktopmenu.spec.ts
@@ -3,27 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { IconLoaderService } from '../../../index';
 import { CommonIconComponent } from './../../base/components/common.icon.component';
-// import { StepWizardComponent } from './stepwizard.component';
-// import { AmexioCardCEComponent } from '../../creative/card/amexio.cards.component';
-// import { AmexioCardCEHeaderComponent } from '../../creative/common/amexio.header.component';
 import { AmexioImageComponent } from '../../media/image/image.component';
-// import { AmexioCardCEBodyComponent } from '../../creative/common/amexio.body.component';
-// import { AmexioCardCEActionComponent } from '../../creative/common/amexio.action.component';
-// import { AmexioFormActionCEComponent } from '../../creative/form/form.action.component'
-// import { AmexioFormCEComponent } from '../../creative/form/amexio.form.component';
-// import { AmexiodialoguePaneComponent } from '../../panes/dialogue/dialogue.pane.component';
-// import { AmexioLabelComponent } from '../../forms/label/label.component';
-// import { AmexioCardComponent } from '../../layout/card/card.component';
-// import { AmexioHeaderComponent } from '../../panes/header/pane.action.header';
-// import { AmexioBodyComponent } from '../../panes/body/pane.action.body';
-// import { AmexioFooterComponent } from '../../panes/action/pane.action.footer';
-// import { AmexioButtonComponent } from '../../forms/buttons/button.component';
-// import { AmexioContextMenuComponent } from '../../base/base.contextmenu.component';
 import { DeviceQueryService } from '../../services/device/device.query.service';
-// import { SpeechBubbleComponent } from '../../data/speech-bubble/speech-bubble.component';
 import { AmexioNavDesktopMenuComponent } from '../../navigation/navbar/navdesktopmenu';
-// import { CommonIconComponent} from '../../base/components/common.icon.component';
-// import {  }
+
 describe('navdesktopmenu', () => {
   let comp1: AmexioNavDesktopMenuComponent;
   let fixture1: ComponentFixture<AmexioNavDesktopMenuComponent>;
@@ -31,19 +14,7 @@ describe('navdesktopmenu', () => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [AmexioNavDesktopMenuComponent,
-        // AmexioFormActionCEComponent, 
-        // SpeechBubbleComponent,
-        // AmexioButtonComponent, AmexioFooterComponent, AmexioHeaderComponent,
-        // AmexioBodyComponent, 
-        // AmexioCardComponent, 
-        // AmexioLabelComponent,
-        // AmexioContextMenuComponent, AmexiodialoguePaneComponent,
-        // AmexioFormCEComponent,
-        //  AmexioCardCEBodyComponent,
-        // AmexioCardCEActionComponent,
         AmexioImageComponent,
-        //  AmexioCardCEComponent,
-        // AmexioCardCEHeaderComponent,
         CommonIconComponent],
       providers: [IconLoaderService, DeviceQueryService],
     });
@@ -62,32 +33,31 @@ describe('navdesktopmenu', () => {
       const fixture = TestBed.createComponent(AmexioNavDesktopMenuComponent);
       fixture.detectChanges();
       const compiled = fixture.debugElement.nativeElement;
-      //  if ((window.innerWidth - this.menus.nativeElement.getBoundingClientRect().right) < 300) {
       expect(window.innerWidth - fixture.debugElement.nativeElement.getBoundingClientRect().right).toBeLessThan(300);
-      // if ((window.innerWidth - this.menus.nativeElement.getBoundingClientRect().right) < 300) {
-      // let a =  comp1.menus.nativeElement.getBoundingClientRect().right = 78;
       let a = 8;
-      // window.innerWidth = 100;
-      // expect(window.innerWidth - a).toBeLessThan(300);
       comp1.position = 'right';
       expect(window.innerWidth - a).toBeGreaterThan(300);
-      // expect(window.innerWidth- fixture.debugElement.nativeElement.getBoundingClientRect().right).toBeGreaterThan(300);
-
       comp1.position = 'left';
-      // }
     }, 100);
   });
-
-
+  it('ngAfterViewInit() else block', () => {
+    setTimeout(() => {
+      comp1.ngAfterViewInit();
+      const fixture = TestBed.createComponent(AmexioNavDesktopMenuComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      // let a = 8;
+      expect(window.innerWidth - fixture.debugElement.nativeElement.getBoundingClientRect().right).toBeGreaterThan(300);
+      comp1.position = 'left';
+    }, 100);
+  });
   it('onMouseOver()', () => {
     let event;
     let node = { submenus: 'as' }
     comp1.onMouseOver(event, node);
     expect(node.submenus).toBeDefined();
     expect(node.submenus.length).toBeGreaterThan(0);
-    // if (node.submenus && node.submenus.length > 0) {
     node['showInnerMenus'] = true;
-    // }
   });
   it('onMouseOver() for else block', () => {
     let event;
