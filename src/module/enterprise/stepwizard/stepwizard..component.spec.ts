898153f64a09bd78992eab17cbf4925b80c97557
ankita202 <ankita.jain@metamgic.in>
2019-07-11 15:46:59 +0530

    stepwizard:test

--- src/module/enterprise/stepwizard/stepwizard..component.spec.ts
+++ src/module/enterprise/stepwizard/stepwizard..component.spec.ts
@@ -7,8 +7,8 @@ import { AmexioCreativeModule } from '../../creative/amexio.creative.module';
 import { AmexioMediaModule } from '../../media/amexio.media.module';
 import { DeviceQueryService } from './../../services/device/device.query.service';
 import { StepWizardItemComponent } from './stepwizard.item.component';
-import { DebugElement,Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormBuilder , FormGroup, NgForm, NgModel} from '@angular/forms';
+import { DebugElement, Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, NgModel } from '@angular/forms';
 
 @Component({
     selector: 'test-cmp',
@@ -16,8 +16,8 @@ import { FormBuilder , FormGroup, NgForm, NgModel} from '@angular/forms';
         <amexio-step-wizard-item stepTitle="step1"></amexio-step-wizard-item>
         <amexio-step-wizard-item stepTitle="step2"></amexio-step-wizard-item>
     </amexio-step-wizard>`,
-  })
-  class TestWrapperComponent { }
+})
+class TestWrapperComponent { }
 describe('amexio-step-wizard', () => {
     let component: StepWizardComponent;
     let fixture: ComponentFixture<TestWrapperComponent>;
@@ -27,20 +27,28 @@ describe('amexio-step-wizard', () => {
     let de: DebugElement;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-          schemas: [ NO_ERRORS_SCHEMA ],
-          declarations: [
-            StepWizardComponent,
-            StepWizardItemComponent, TestWrapperComponent
-          ],
-          providers: [IconLoaderService, DeviceQueryService, FormBuilder],
+            schemas: [NO_ERRORS_SCHEMA],
+            declarations: [
+                StepWizardComponent,
+                StepWizardItemComponent, TestWrapperComponent
+            ],
+            providers: [IconLoaderService, DeviceQueryService, FormBuilder],
         }).compileComponents();
-      }));
+    }));
 
     beforeEach(() => {
         service = TestBed.get(DeviceQueryService);
         fixture = TestBed.createComponent(TestWrapperComponent);
         component = fixture.debugElement.children[0].componentInstance;
+        event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
         fixture.detectChanges();
+    });
+
+    it('method ngAfterContentInit else block', () => {
+        component.ngAfterContentInit();
+        fixture.detectChanges();
+        component.stepItemList = null;
+        expect(component.stepItemList).toEqual(null);
     });
 
     it('method ngAfterContentInit', () => {
@@ -48,15 +56,12 @@ describe('amexio-step-wizard', () => {
         const time = new Date().getTime();
         fixture.detectChanges();
         component.stepItemList = component.stepItemQueryList.toArray();
+        component.ngAfterContentInit();
         expect(component.stepItemList).toBeDefined();
         expect(component.stepItemList.length).toBeGreaterThan(0);
         component.stepItemList[0].active = true;
-        component.ngAfterContentInit();
-        // expect(component.stepItemList).not.toBeDefined();
-        // expect(component.stepItemList.length).not.toBeGreaterThan(0);
-
-    })
-
+    });
+    
 });
 
 
