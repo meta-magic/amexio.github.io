62928a2a1cc512c96a071ce41de51fe4afc25b19
kedar8 <kedar.kokil@metamagic.in>
2019-07-12 19:42:19 +0530

    fliter-tree: testing.

--- src/module/data/tree/filter.tree.component.spec.ts
+++ src/module/data/tree/filter.tree.component.spec.ts
@@ -668,9 +668,8 @@ describe('amexio-tree-filter-view', () => {
         text: "Home", icon: "fa fa-home fa-fw", mdaIcon: "home", link: "/home/dashboard", selected: true, badge: "21"
       }
     ];
-    
-    comp.filterData();
     comp.onClickSearch = true;
+    comp.filterData();
     expect(comp.filterText.length).not.toBeGreaterThanOrEqual(comp.triggerchar);
     expect(comp.onClickSearch).toEqual(true);
     const tData = JSON.parse(JSON.stringify(comp.orgTreeData));
@@ -681,17 +680,15 @@ describe('amexio-tree-filter-view', () => {
   });
   it('filterData() third 0 length else ', () => {
     comp.filterText = "h";
-    comp.triggerchar = 4;
+    comp.triggerchar = 1;
     comp.orgTreeData = [{
       text: "Home", icon: "fa fa-home fa-fw", mdaIcon: "home", link: "/home/dashboard", selected: true, badge: "21"
     },
     {
       text: "Home", icon: "fa fa-home fa-fw", mdaIcon: "home", link: "/home/dashboard", selected: true, badge: "21"
     }];
-    
-    comp.filterData();
     comp.onClickSearch = true;
-    expect(comp.filterText.length).not.toBeGreaterThanOrEqual(comp.triggerchar);
+    comp.filterData();
     expect(comp.onClickSearch).toEqual(true);
     const tData = JSON.parse(JSON.stringify(comp.orgTreeData));
     const treeNodes = comp.searchTree(tData, comp.filterText);
@@ -718,12 +715,10 @@ describe('amexio-tree-filter-view', () => {
   });
   it('filterData() third 0 length if', () => {
     comp.filterText = "h";
-    comp.triggerchar = 4;
+    comp.triggerchar = 1;
     comp.orgTreeData = [];
-    
-    comp.filterData();
     comp.onClickSearch = true;
-    expect(comp.filterText.length).not.toBeGreaterThanOrEqual(comp.triggerchar);
+    comp.filterData();
     expect(comp.onClickSearch).toEqual(true);
     const tData = JSON.parse(JSON.stringify(comp.orgTreeData));
     const treeNodes = comp.searchTree(tData, comp.filterText);
