bffd2a2447a482e84ae60e5eb0fc0c82ec44c6b5
kedar8 <kedar.kokil@metamagic.in>
2019-07-16 18:43:13 +0530

    icon-service: reverted.

--- src/module/services/icon/icon.service.ts
+++ src/module/services/icon/icon.service.ts
@@ -19,306 +19,306 @@ import { Component } from '@angular/core';
 
 import { Injectable } from '@angular/core';
 
-const faFaCaretIcon  = 'fa fa-caret-down';
+const faFaCaretIcon = 'fa fa-caret-down';
 
-const faFaTimesIcon    = 'fa fa-times';
+const faFaTimesIcon = 'fa fa-times';
 
 const ICON_MAPPING =
-[
-  {
-    component: 'datepicker_previous',
-    fa: 'fa fa-chevron-left',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'datepicker_calendar',
-    fa: 'fa fa-calendar',
-    mat: 'date_range',
-  },
-  {
-    component: 'datepicker_next',
-    fa: 'fa fa-chevron-right',
-    mat: 'keyboard_arrow_right',
-  },
-  {
-    component: 'datepicker_previous_fast',
-    fa: 'fa fa-step-backward',
-    mat: 'fast_rewind',
-  },
-  {
-    component: 'datepicker_next_fast',
-    fa: 'fa fa-step-forward',
-    mat: 'fast_forward',
-  },
-  {
-    component: 'accordion_expand',
-    fa: 'fa fa-plus',
-    mat: 'add',
-  },
-  {
-    component: 'accordion_collapse',
-    fa: 'fa fa-minus',
-    mat: 'remove',
-  },
-  {
-    component: 'tree_expand',
-    fa: 'fa fa-chevron-down',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'tree_collapse',
-    fa: 'fa fa-chevron-right',
-    mat: 'keyboard_arrow_right',
-  },
-  {
-    component: 'dropdown_caret',
-    fa: faFaCaretIcon,
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'tab_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'window_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'window_maximize',
-    fa: 'fa fa-window-maximize',
-    mat: 'open_with',
-  },
-  {
-    component: 'window_restore',
-    fa: 'fa fa-window-restore',
-    mat: 'indeterminate_check_box',
-  },
-  {
-    component: 'paginator_previous',
-    fa: 'fa fa-angle-left',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'paginator_next',
-    fa: 'fa fa-angle-right',
-    mat: 'keyboard_arrow_right',
-  },
-  {
-    component: 'paginator_first',
-    fa: 'fa fa-angle-double-left',
-    mat: 'first_page',
-  },
-  {
-    component: 'paginator_last',
-    fa: 'fa fa-angle-double-right',
-    mat: 'last_page',
-  },
-  {
-    component: 'itemselector_caretup',
-    fa: 'fa fa-caret-up',
-    mat: 'arrow_drop_up',
-  },
-  {
-    component: 'itemselector_caretdown',
-    fa: faFaCaretIcon,
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'itemselector_arrowup',
-    fa: 'fa fa-arrow-up',
-    mat: 'arrow_upward',
-  },
-  {
-    component: 'itemselector_arrowdown',
-    fa: 'fa fa-arrow-down',
-    mat: 'arrow_downward',
-  },
-  {
-    component: 'itemselector_arrowleft',
-    fa: 'fa fa-arrow-left',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'itemselector_arrowright',
-    fa: 'fa fa-arrow-right',
-    mat: 'keyboard_arrow_right',
-  },
-  {
-    component: 'tab_previous',
-    fa: 'fa fa-angle-left fa-2x',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'tab_next',
-    fa: 'fa fa-angle-right  fa-2x',
-  },
-  {
-    component: 'tab_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'fieldset_expand',
-    fa: 'fa fa-plus',
-    mat: 'add',
-  },
-  {
-    component: 'fieldset_collpase',
-    fa: 'fa fa-minus',
-    mat: 'remove',
-  },
-  {
-    component: 'carousel_previous',
-    fa: 'fa fa-angle-left fa-2x',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'carousel_next',
-    fa: 'fa fa-angle-right  fa-2x',
-  },
-  {
-    component: 'dockbar_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'notify_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'sidenav_bar',
-    fa: 'fa fa-bars fa-2x',
-    mat: 'menu',
-  },
-  {
-    component: 'sidenav_close',
-    fa: faFaTimesIcon,
-    mat: 'close',
-  },
-  {
-    component: 'datagrid_arrowdown',
-    fa: 'fa fa-arrow-down',
-    mat: 'arrow_downward',
-  },
-  {
-    component: 'datagrid_arrowup',
-    fa: 'fa fa-arrow-up',
-    mat: 'arrow_upward',
-  },
-  {
-    component: 'datagrid_list',
-    fa: 'fa fa-th-list',
-    mat: 'view_list',
-  },
-  {
-    component: 'datagrid_expand',
-    fa: 'fa fa-caret-right',
-    mat: '',
-  },
-  {
-    component: 'datagrid_collapse',
-    fa: faFaCaretIcon,
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'datagrid_collapse',
-    fa: faFaCaretIcon,
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'datagrid_filter',
-    fa: 'fa fa-filter',
-    mat: 'filter_list',
-  },
-  {
-    component: 'tree_filter',
-    fa: 'fa fa-filter',
-    mat: 'filter_list',
-  },
-  {
-    component: 'data_check',
-    fa: 'fa fa-check',
-    mat: 'check',
-  },
-  {
-    component: 'button_caret-down',
-    fa: faFaCaretIcon,
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'button_angle-right',
-    fa: 'fa fa-angle-right',
-    mat: 'arrow_drop_right',
-  },
-  {
-    component: 'button_angle-down',
-    fa: 'fa fa-angle-down',
-    mat: 'arrow_drop_down',
-  },
-  {
-    component: 'menubar_ravelry',
-    fa: 'fa fa-ravelry',
-    mat: 'done',
-  },
-  {
-    component: 'sidenav-angle-up',
-    fa: 'fa fa-angle-up',
-    mat: 'keyboard_arrow_up',
-  },
-  {
-    component: 'sidenav-angle-down',
-    fa: 'fa fa-angle-down',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'window-msgtype-error',
-    fa: 'fa fa-exclamation-triangle fa-2x fa-fw',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'window-msgtype-warning',
-    fa: 'fa fa-exclamation-triangle fa-2x fa-fw',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'window-msgtype-help',
-    fa: 'fa fa-question-circle fa-2x fa-fw',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'window-msgtype-confirm',
-    fa: 'fa fa-check-circle fa-2x fa-fw',
-    mat: 'keyboard_arrow_down',
-  },
-  {
-    component: 'horizontal-tree-expanded',
-    fa: 'fa fa-caret-right fa-fw',
-    mat: 'keyboard_arrow_right',
-  },
-  {
-    component: 'horizontal-tree-collapse',
-    fa: 'fa fa-caret-left fa-fw',
-    mat: 'keyboard_arrow_left',
-  },
-  {
-    component: 'button-loading-icon',
-    fa: 'fa fa-refresh fa-spin',
-    mat: 'cached',
-  },
-  {
-    component: 'datepicker-clock-icon',
-    fa: 'fa fa-clock-o',
-    mat: 'access_time',
-  },
-  {
-    component: 'sidenavnode-icon',
-    fa: 'fa fa-user-circle',
-    mat: 'person_pin',
-  },
-];
+  [
+    {
+      component: 'datepicker_previous',
+      fa: 'fa fa-chevron-left',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'datepicker_calendar',
+      fa: 'fa fa-calendar',
+      mat: 'date_range',
+    },
+    {
+      component: 'datepicker_next',
+      fa: 'fa fa-chevron-right',
+      mat: 'keyboard_arrow_right',
+    },
+    {
+      component: 'datepicker_previous_fast',
+      fa: 'fa fa-step-backward',
+      mat: 'fast_rewind',
+    },
+    {
+      component: 'datepicker_next_fast',
+      fa: 'fa fa-step-forward',
+      mat: 'fast_forward',
+    },
+    {
+      component: 'accordion_expand',
+      fa: 'fa fa-plus',
+      mat: 'add',
+    },
+    {
+      component: 'accordion_collapse',
+      fa: 'fa fa-minus',
+      mat: 'remove',
+    },
+    {
+      component: 'tree_expand',
+      fa: 'fa fa-chevron-down',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'tree_collapse',
+      fa: 'fa fa-chevron-right',
+      mat: 'keyboard_arrow_right',
+    },
+    {
+      component: 'dropdown_caret',
+      fa: faFaCaretIcon,
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'tab_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'window_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'window_maximize',
+      fa: 'fa fa-window-maximize',
+      mat: 'open_with',
+    },
+    {
+      component: 'window_restore',
+      fa: 'fa fa-window-restore',
+      mat: 'indeterminate_check_box',
+    },
+    {
+      component: 'paginator_previous',
+      fa: 'fa fa-angle-left',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'paginator_next',
+      fa: 'fa fa-angle-right',
+      mat: 'keyboard_arrow_right',
+    },
+    {
+      component: 'paginator_first',
+      fa: 'fa fa-angle-double-left',
+      mat: 'first_page',
+    },
+    {
+      component: 'paginator_last',
+      fa: 'fa fa-angle-double-right',
+      mat: 'last_page',
+    },
+    {
+      component: 'itemselector_caretup',
+      fa: 'fa fa-caret-up',
+      mat: 'arrow_drop_up',
+    },
+    {
+      component: 'itemselector_caretdown',
+      fa: faFaCaretIcon,
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'itemselector_arrowup',
+      fa: 'fa fa-arrow-up',
+      mat: 'arrow_upward',
+    },
+    {
+      component: 'itemselector_arrowdown',
+      fa: 'fa fa-arrow-down',
+      mat: 'arrow_downward',
+    },
+    {
+      component: 'itemselector_arrowleft',
+      fa: 'fa fa-arrow-left',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'itemselector_arrowright',
+      fa: 'fa fa-arrow-right',
+      mat: 'keyboard_arrow_right',
+    },
+    {
+      component: 'tab_previous',
+      fa: 'fa fa-angle-left fa-2x',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'tab_next',
+      fa: 'fa fa-angle-right  fa-2x',
+    },
+    {
+      component: 'tab_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'fieldset_expand',
+      fa: 'fa fa-plus',
+      mat: 'add',
+    },
+    {
+      component: 'fieldset_collpase',
+      fa: 'fa fa-minus',
+      mat: 'remove',
+    },
+    {
+      component: 'carousel_previous',
+      fa: 'fa fa-angle-left fa-2x',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'carousel_next',
+      fa: 'fa fa-angle-right  fa-2x',
+    },
+    {
+      component: 'dockbar_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'notify_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'sidenav_bar',
+      fa: 'fa fa-bars fa-2x',
+      mat: 'menu',
+    },
+    {
+      component: 'sidenav_close',
+      fa: faFaTimesIcon,
+      mat: 'close',
+    },
+    {
+      component: 'datagrid_arrowdown',
+      fa: 'fa fa-arrow-down',
+      mat: 'arrow_downward',
+    },
+    {
+      component: 'datagrid_arrowup',
+      fa: 'fa fa-arrow-up',
+      mat: 'arrow_upward',
+    },
+    {
+      component: 'datagrid_list',
+      fa: 'fa fa-th-list',
+      mat: 'view_list',
+    },
+    {
+      component: 'datagrid_expand',
+      fa: 'fa fa-caret-right',
+      mat: '',
+    },
+    {
+      component: 'datagrid_collapse',
+      fa: faFaCaretIcon,
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'datagrid_collapse',
+      fa: faFaCaretIcon,
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'datagrid_filter',
+      fa: 'fa fa-filter',
+      mat: 'filter_list',
+    },
+    {
+      component: 'tree_filter',
+      fa: 'fa fa-filter',
+      mat: 'filter_list',
+    },
+    {
+      component: 'data_check',
+      fa: 'fa fa-check',
+      mat: 'check',
+    },
+    {
+      component: 'button_caret-down',
+      fa: faFaCaretIcon,
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'button_angle-right',
+      fa: 'fa fa-angle-right',
+      mat: 'arrow_drop_right',
+    },
+    {
+      component: 'button_angle-down',
+      fa: 'fa fa-angle-down',
+      mat: 'arrow_drop_down',
+    },
+    {
+      component: 'menubar_ravelry',
+      fa: 'fa fa-ravelry',
+      mat: 'done',
+    },
+    {
+      component: 'sidenav-angle-up',
+      fa: 'fa fa-angle-up',
+      mat: 'keyboard_arrow_up',
+    },
+    {
+      component: 'sidenav-angle-down',
+      fa: 'fa fa-angle-down',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'window-msgtype-error',
+      fa: 'fa fa-exclamation-triangle fa-2x fa-fw',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'window-msgtype-warning',
+      fa: 'fa fa-exclamation-triangle fa-2x fa-fw',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'window-msgtype-help',
+      fa: 'fa fa-question-circle fa-2x fa-fw',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'window-msgtype-confirm',
+      fa: 'fa fa-check-circle fa-2x fa-fw',
+      mat: 'keyboard_arrow_down',
+    },
+    {
+      component: 'horizontal-tree-expanded',
+      fa: 'fa fa-caret-right fa-fw',
+      mat: 'keyboard_arrow_right',
+    },
+    {
+      component: 'horizontal-tree-collapse',
+      fa: 'fa fa-caret-left fa-fw',
+      mat: 'keyboard_arrow_left',
+    },
+    {
+      component: 'button-loading-icon',
+      fa: 'fa fa-refresh fa-spin',
+      mat: 'cached',
+    },
+    {
+      component: 'datepicker-clock-icon',
+      fa: 'fa fa-clock-o',
+      mat: 'access_time',
+    },
+    {
+      component: 'sidenavnode-icon',
+      fa: 'fa fa-user-circle',
+      mat: 'person_pin',
+    },
+  ];
 
 @Injectable()
 export class IconLoaderService {
@@ -358,27 +358,25 @@ export class IconLoaderService {
     this.iconMappings = ICON_MAPPING;
   }
 
-  modifyIconClass(componentName: string, newValue: string) {
-   let object = this.getCompareRes(componentName);
-   if(object != null) {
-    object[this._iconToUse.toString()] = newValue;
-   }
+  modifyIconClass(componentKey: string, newValue: string) {
+    if (this.iconMappings != null) {
+      this.iconMappings.forEach((icon: any) => {
+        if (icon.component === componentKey) {
+          icon[this._iconToUse.toString()] = newValue;
+        }
+      });
+    }
   }
   // TO GET ICON OBJECT
   getIconObject(componentName: string): any {
-     return this.getCompareRes(componentName);
-  }
-
-
-  getCompareRes(key: string): any {
-    let obj = null
+    let obj = null;
     if (this.iconMappings !== null) {
       this.iconMappings.forEach((object: any) => {
-        if (object.component === key) {
+        if (object.component === componentName) {
           obj = object;
         }
       });
+    }
+    return obj;
   }
-  return obj;
-}
 }
