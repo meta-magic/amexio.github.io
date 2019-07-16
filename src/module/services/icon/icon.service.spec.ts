f0f4fda58ecdc56fea54043d630591b9f0c1db56
kedar8 <kedar.kokil@metamagic.in>
2019-07-16 19:23:07 +0530

    icon service: testing.

--- src/module/services/icon/icon.service.spec.ts
+++ src/module/services/icon/icon.service.spec.ts
@@ -1,10 +1,10 @@
-import {IconLoaderService} from "./icon.service";
-import {inject,TestBed} from "@angular/core/testing";
+import { IconLoaderService } from './icon.service';
+import { inject, TestBed } from '@angular/core/testing';
 /**
  * Created by dattaram on 8/8/18.
  */
-describe('IconLoaderService',() =>{
-  let service : any;
+describe('IconLoaderService', () => {
+  let service: any;
   let ICON_MAPPING =
     [
       {
@@ -12,7 +12,7 @@ describe('IconLoaderService',() =>{
         fa: 'fa fa-chevron-left',
         mat: 'keyboard_arrow_left',
       }
-      ];
+    ];
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [IconLoaderService]
@@ -25,7 +25,7 @@ describe('IconLoaderService',() =>{
     service.iconMappings = ICON_MAPPING;
   }));
 
-  it('get iconToUse',()=>{
+  it('get iconToUse', () => {
     service._iconToUse = 'fa fa-clock-o';
     expect(service._iconToUse).not.toBeNull();
     expect(service._iconToUse).toBe(service._iconToUse);
@@ -33,24 +33,60 @@ describe('IconLoaderService',() =>{
     service._iconToUse = null;
     expect(service._iconToUse).toBeNull();
     service._iconToUse = 'fa';
-    
+
   });
 
-  it('set iconToUse',()=>{
+  it('set iconToUse', () => {
     const value = jasmine.any(String);
     service._iconToUse = value;
+    expect(service._iconToUse).toBeDefined();
   });
 
-//   it('modifyIconClass',()=>{
-//     let componentKey = 'datepicker_previous';
-//     let newValue = 'fa fa-clock-o';
-//     service.modifyIconClass(componentKey,newValue);
-//     expect(service.iconMappings).not.toBeNull();
-//     ICON_MAPPING.forEach((icon: any) => {
-//       expect(icon.component).toEqual(componentKey);
-//       icon[service._iconToUse] = newValue;
-//     });
-//   });
+  it('modifyIconClass 1st if condition', () => {
+    service._iconToUse = 'fa';
+    let componentKey = 'datepicker_previous';
+    let newValue = 'fa fa-clock-o';
+    service.iconMappings = [
+      {
+        component: 'datepicker_previous',
+        fa: 'fa fa-chevron-left',
+        mat: 'keyboard_arrow_left',
+      }
+    ];
+    service.modifyIconClass(componentKey, newValue);
+    expect(service.iconMappings).not.toBeNull();
 
+    service.iconMappings.forEach((icon: any) => {
+      expect(icon.component).toEqual(componentKey);
+      icon[service._iconToUse.toString()] = newValue;
+    });
+  });
+
+  it('modifyIconClass 1st else condition', () => {
+    service._iconToUse = 'fa';
+    let componentKey = 'datepicker_previous';
+    let newValue = 'fa fa-clock-o';
+    service.iconMappings = null;
+    service.modifyIconClass(componentKey, newValue);
+    expect(service.iconMappings).toBeNull();
+  });
+
+  it('modifyIconClass with nested if-chi-else condition', () => {
+    service._iconToUse = 'fa';
+    let componentKey = 'datepicker_previouss';
+    let newValue = 'fa fa-clock-o';
+    service.iconMappings = [
+      {
+        component: 'datepicker_previous',
+        fa: 'fa fa-chevron-left',
+        mat: 'keyboard_arrow_left',
+      }
+    ];
+    service.modifyIconClass(componentKey, newValue);
+    expect(service.iconMappings).not.toBeNull();
+    service.iconMappings.forEach((icon: any) => {
+      expect(icon.component).not.toEqual(componentKey);
+    });
+  });
 
 });
