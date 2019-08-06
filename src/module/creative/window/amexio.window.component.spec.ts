5abbc91ed6dda3e2197fda4ffc0bb0170db47899
kedar8 <kedar.kokil@metamagic.in>
2019-08-06 18:01:45 +0530

    test case: ce-window.

--- src/module/creative/window/amexio.window.component.spec.ts
+++ src/module/creative/window/amexio.window.component.spec.ts
@@ -1,33 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { FormsModule, FormBuilder } from '@angular/forms';
 import { IconLoaderService } from '../../../index';
 
-import { CommonDataService } from '../../services/data/common.data.service';
-import { HttpClient, HttpHandler } from '@angular/common/http';
-import { Renderer2, Renderer, TemplateRef } from '@angular/core';
-import { CommonIconComponent } from './../../base/components/common.icon.component';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AmexioWindowCEComponent } from './amexio.window.component';
-import { AmexioButtonComponent } from '../../forms/amexio.forms.module';
+import { AmexioCardCEHeaderComponent } from '../common/amexio.header.component';
+
+@Component({
+    selector: 'test-cmp',
+    template: `
+    <amexio-window-ce >
+       <amexio-header-ce>
+       </amexio-header-ce>
+       </amexio-window-ce>
+       
+       `,
+})
+class TestWindowComponent { }
 describe('amexio-window-ce', () => {
     let comp: AmexioWindowCEComponent;
-    let fixture: ComponentFixture<AmexioWindowCEComponent>;
-    let checkD: any;
-    let data: any;
+    let fixture: ComponentFixture<TestWindowComponent>;
 
-    beforeEach(() => {
+    // let queryitem: AmexioCardCEHeaderComponent;
+    // let fixture2: ComponentFixture<AmexioCardCEHeaderComponent>
+    // let de: DebugElement;
+
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [FormsModule],
-            declarations: [CommonIconComponent, AmexioWindowCEComponent, AmexioButtonComponent],
-            providers: [IconLoaderService, CommonDataService, Renderer2, HttpClient, HttpHandler],
-        });
-        fixture = TestBed.createComponent(AmexioWindowCEComponent);
-        comp = fixture.componentInstance;
-        fixture.detectChanges();
-
-        const compiled = fixture.debugElement.nativeElement;
+            schemas: [NO_ERRORS_SCHEMA],
+            declarations: [
+                AmexioWindowCEComponent,
+                AmexioCardCEHeaderComponent,
+                TestWindowComponent
+            ],
+            providers: [IconLoaderService, FormBuilder],
+        }).compileComponents();
+    }));
+    beforeEach(() => {
+        // service = TestBed.get(DeviceQueryService);
+        fixture = TestBed.createComponent(TestWindowComponent);
+        comp = fixture.debugElement.children[0].componentInstance;
         event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
-        let renderer = Renderer2;
-
+        fixture.detectChanges();
 
     });
 
@@ -52,6 +66,40 @@ describe('amexio-window-ce', () => {
         comp.minimizeFlag = false;
     });
 
+
+    it('ngAfterContentInit  method check minimize  true condition', () => {
+
+        fixture.detectChanges();
+        comp.minimize = true;
+        comp.ngAfterContentInit();
+        expect(comp.amexioHeader).toBeDefined();
+        expect(comp.amexioHeader.toArray().length).toBeGreaterThan(0);
+        expect(comp.minimize).toEqual(true);
+        comp.amexioHeader.toArray()[0].minimize = comp.minimize;
+        comp.amexioHeader.toArray()[0].minimizeWindow.subscribe((event: any) => {
+            comp.textName = event.textName;
+            expect(comp.onMinimizeClick(event)).toHaveBeenCalled();
+        });
+    });
+    it('ngAfterContentInit  method check minimize false condition', () => {
+
+        fixture.detectChanges();
+        comp.minimize = false;
+        comp.ngAfterContentInit();
+        expect(comp.amexioHeader).toBeDefined();
+        expect(comp.amexioHeader.toArray().length).toBeGreaterThan(0);
+        expect(comp.minimize).toEqual(false);
+    });
+    it('onMinimizeClick  method check', () => {
+        let name = comp.textName;
+        comp.show = false;
+        comp.minimizeFlag = true;
+        comp.onMinimizeClick(name);
+        comp.show = !comp.show;
+        jasmine.createSpyObj('comp', ['minimizeBtnClick'])
+        comp.minimizeBtnClick();
+
+    });
     it('minimizeBtnClick  method check', () => {
         comp.minimizeBtnClick();
         comp.show = !comp.show;
