7d5b312b150b5ae8d28dd84d48ebda3d5447b58c
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-08-09 18:44:18 +0530

    fieldset: testcase added

--- src/module/panes/fieldset/fieldset.component.spec.ts
+++ src/module/panes/fieldset/fieldset.component.spec.ts
@@ -1,44 +1,44 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { FormsModule } from '@angular/forms';
-// import { IconLoaderService } from '../../../index';
-// import { AmexioFieldSetComponent } from './fieldset.component';
-// import { AmexioStepsComponent } from '../steps/steps.component';
-// import { CommonIconComponent } from './../../base/components/common.icon.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { IconLoaderService } from '../../../index';
+import { AmexioFieldSetComponent } from './fieldset.component';
+import { AmexioStepsComponent } from '../steps/steps.component';
+import { CommonIconComponent } from './../../base/components/common.icon.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// describe('amexio-fieldset', () => {
-//     let comp: AmexioFieldSetComponent;
-//     let fixture: ComponentFixture<AmexioFieldSetComponent>;
+describe('amexio-fieldset', () => {
+    let comp: AmexioFieldSetComponent;
+    let fixture: ComponentFixture<AmexioFieldSetComponent>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             imports: [FormsModule, BrowserAnimationsModule],
-//             declarations: [AmexioFieldSetComponent, AmexioStepsComponent, CommonIconComponent],
-//             providers: [IconLoaderService],
-//         });
-//         fixture = TestBed.createComponent(AmexioFieldSetComponent);
-//         comp = fixture.componentInstance;
-//         fixture.autoDetectChanges()
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, BrowserAnimationsModule],
+            declarations: [AmexioFieldSetComponent, AmexioStepsComponent, CommonIconComponent],
+            providers: [IconLoaderService],
+        });
+        fixture = TestBed.createComponent(AmexioFieldSetComponent);
+        comp = fixture.componentInstance;
+        fixture.autoDetectChanges()
+    });
 
 
-//     // it('checking ngOnInit method', () => {
+    it('checking ngOnInit method', () => {
 
-//     //     comp.ngOnInit();
-//     //     comp.collapsible = false;
-//     //     expect(comp.collapsible).toEqual(false);
-//     //     comp.isActive = true;
-//     //     expect(comp.isActive).toEqual(true);
-//     // });
+        comp.ngOnInit();
+        comp.collapsible = false;
+        expect(comp.collapsible).toEqual(false);
+        comp.isActive = true;
+        expect(comp.isActive).toEqual(true);
+    });
 
-//     // it('onLegendClick', () => {
+    it('onLegendClick', () => {
 
-//     //     comp.onLegendClick();
-//     //     comp.collapsible = true;
-//     //     expect(comp.collapsible).toEqual(true);
-//     //     comp.isActive = false;
-//     //     expect(comp.isActive).toEqual(false);
-//     // });
+        comp.onLegendClick();
+        comp.collapsible = true;
+        expect(comp.collapsible).toEqual(true);
+        comp.isActive = false;
+        expect(comp.isActive).toEqual(false);
+    });
 
-// });
+});
 
