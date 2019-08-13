e2e7b75e8422c8669cb57cd3153da90ce0305eb7
kedar8 <kedar.kokil@metamagic.in>
2019-08-13 16:07:40 +0530

    tslint: solved.

--- src/module/creative/window/ceMinimize.window.component.ts
+++ src/module/creative/window/ceMinimize.window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MinimizeService } from '../../panes/window/minimize-service.service';
 
 @Component({
