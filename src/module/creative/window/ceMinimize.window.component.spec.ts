7eefb8ff0f025454aceaec591dbc4593ff66c1b9
kedar8 <kedar.kokil@metamagic.in>
2019-08-13 15:20:38 +0530

    ce-mini-window: test cases.

--- src/module/creative/window/ceMinimize.window.component.spec.ts
+++ src/module/creative/window/ceMinimize.window.component.spec.ts
@@ -5,28 +5,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonIconComponent } from '../../base/components/common.icon.component';
 import { CeMinimizeWindowComponent } from './ceMinimize.window.component';
 
-import { CeMinimizeService } from './ceMinimize-service.service';
+import { MinimizeService } from '../../panes/window/minimize-service.service';
+
 describe('amexio-minimize-ce-window', () => {
 
     let comp: CeMinimizeWindowComponent;
     let fixture: ComponentFixture<CeMinimizeWindowComponent>;
-    let service: CeMinimizeService;
+    let service: MinimizeService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [],
             declarations: [CeMinimizeWindowComponent, CommonIconComponent, AmexioButtonComponent],
-            providers: [CeMinimizeService],
+            providers: [MinimizeService],
         });
         fixture = TestBed.createComponent(CeMinimizeWindowComponent);
         comp = fixture.componentInstance;
-        service = TestBed.get(CeMinimizeService);
+        service = TestBed.get(MinimizeService);
     });
 
     it('should trigger ngOnInit with detectChanges', () => {
         comp.ceMiniButton = true;
         comp.ngOnInit();
-        service.observableMessage.subscribe((element: any) => {
+        service.currentMessage.subscribe((element: any) => {
             expect(element).not.toBeNull();
             comp.arrayData = element;
             expect(comp.ceMiniButton).toEqual(true);
