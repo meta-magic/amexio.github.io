e5d4b626c17f8f6f80bdb24391f9f03bf284fa7e
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-08-30 11:47:27 +0530

    text: componentId updated

--- src/module/forms/textinput/textinput.component.ts
+++ src/module/forms/textinput/textinput.component.ts
@@ -242,7 +242,6 @@ description : On field value change event
   ngOnInit() {
     this.name = this.generateName(this.name, this.fieldlabel, 'textinput');
     this.componentId = this.createCompId('textinput', this.name);
-    console.log('this.componentID', this.componentId);
   }
   public validate(c: FormControl) {
     return this.isFieldValid() ? null : {
