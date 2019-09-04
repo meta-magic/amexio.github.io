52965c2164bbfda43c4742f3aa9d1cdee4428fa7
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-09-04 22:58:01 +0530

    build: issue

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ export * from './module/services/icon/icon.service';
 export * from './module/charts/chart.loader.service';
 export * from './module/dashboard/chart.loader.service';
 export * from './module/map/map.loader.service';
-export * from './module/services/icon/icon.mapping.config';
 export * from './module/layout/accordion/accordion.service';
 export * from './module/services/data/display.field.service';
 export * from './module/services/routing/routing.service';
