6f8de083718751a3cfc2f508d8055f5e8a8a889b
Manisha1205 <manisha.boddu@metamagic.in>
2019-09-06 18:27:28 +0530

    color pallette: test case added

--- src/module/directive/color-palette.directive.spec.ts
+++ src/module/directive/color-palette.directive.spec.ts
@@ -104,13 +104,23 @@ describe('Directive: Color', () => {
         }, 1000);
     });
 
-    it('getBGColorStyles()', () => {
+    it('getBGColorStyles()case1', () => {
       let obj = {amexioComponentId: 'amexio-card', setColorPalette: ()=>{}}
       dirIn.hostComponent = obj
       dirIn.hostComponent.amexioComponentId = 'amexio-card';
       dirIn.getBGColorStyles(dirIn.hostComponent);
       let c;
-      dirIn.ColorPaletteConstants = {card:  c, cardce: c}
+      // navbar = 'amexio-navbar';
+      // static  accordion = 'amexio-accordion';
+      // static  panel = 'amexio-panel';
+      // static  window = 'amexio-window';
+      // static  dialogue = 'amexio-dialogue';
+      // static  grid = 'amexio-grid';
+      // static  box = 'amexio-box';
+      // static  tab = 'amexio-tab';
+      // static banner = 'amexio-banner';
+      // static floatingpanel = 'amexio-floating-panel';
+      dirIn.ColorPaletteConstants = {card:  c, cardce: c, }
 
       expect(dirIn.ColorPaletteConstants.card).toBeUndefined();
       expect(dirIn.ColorPaletteConstants.cardce).toBeUndefined();
@@ -123,6 +133,165 @@ describe('Directive: Color', () => {
         expect(dirIn.ColorPaletteConstants.cardce).toBeDefined(); 
 
     });
+
+    it('getBGColorStyles()case2', () => {
+      let obj = {amexioComponentId: 'amexio-banner', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-banner';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {banner:  c };
+      expect(dirIn.ColorPaletteConstants.banner).toBeUndefined();
+      dirIn.ColorPaletteConstants = {banner: 'amexio-banner' };
+        dirIn.ColorPaletteConstants.banner = 'amexio-banner'
+        expect(dirIn.ColorPaletteConstants.banner).toBeDefined();
+    });
+
+    it('getBGColorStyles()case3', () => {
+      let obj = {amexioComponentId: 'amexio-navbar', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-navbar';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {navbar:  c };
+      expect(dirIn.ColorPaletteConstants.banner).toBeUndefined();
+      dirIn.ColorPaletteConstants = {navbar: 'amexio-navbar' };
+        dirIn.ColorPaletteConstants.navbar = 'amexio-navbar'
+        expect(dirIn.ColorPaletteConstants.navbar).toBeDefined();
+    });
+
+    it('getBGColorStyles()case4', () => {
+      let obj = {amexioComponentId: 'amexio-accordion', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-accordion';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {accordion:  c };
+      expect(dirIn.ColorPaletteConstants.accordion).toBeUndefined();
+      dirIn.ColorPaletteConstants = {accordion: 'amexio-accordion' };
+        dirIn.ColorPaletteConstants.accordion = 'amexio-accordion'
+        expect(dirIn.ColorPaletteConstants.accordion ).toBeDefined();
+    });
+
+    it('getBGColorStyles()case5', () => {
+      let obj = {amexioComponentId: 'amexio-panel', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-panel';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {panel:  c };
+      expect(dirIn.ColorPaletteConstants.panel).toBeUndefined();
+      dirIn.ColorPaletteConstants = {panel: 'amexio-panel' };
+        dirIn.ColorPaletteConstants.panel = 'amexio-panel'
+        expect(dirIn.ColorPaletteConstants.panel).toBeDefined();
+    });
+
+    it('getBGColorStyles()case6', () => {
+      let obj = {amexioComponentId: 'amexio-floating-panel', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-floating-panel';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {floatingpanel:  c };
+      expect(dirIn.ColorPaletteConstants.floatingpanel).toBeUndefined();
+      dirIn.ColorPaletteConstants = {floatingpanel: 'amexio-floating-panel' };
+        dirIn.ColorPaletteConstants.floatingpanel = 'amexio-floating-panel'
+        expect(dirIn.ColorPaletteConstants.floatingpanel).toBeDefined();
+    });
+
+    it('getBGColorStyles()case7', () => {
+      let obj = {amexioComponentId: 'amexio-window', setColorPalette: ()=>{}}
+      dirIn.hostComponent = obj
+      dirIn.hostComponent.amexioComponentId = 'amexio-window';
+      dirIn.getBGColorStyles(dirIn.hostComponent);
+      let c;
+       dirIn.ColorPaletteConstants = {window:  c };
+      expect(dirIn.ColorPaletteConstants.window).toBeUndefined();
+      dirIn.ColorPaletteConstants = {window: 'amexio-window' };
+        dirIn.ColorPaletteConstants.window = 'amexio-window'
+        expect(dirIn.ColorPaletteConstants.window).toBeDefined();
+    });
+// ###########################################
+
+it('getGradientStyles()case2', () => {
+  let obj = {amexioComponentId: 'amexio-banner', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-banner';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {banner:  c };
+  expect(dirIn.ColorPaletteConstants.banner).toBeUndefined();
+  dirIn.ColorPaletteConstants = {banner: 'amexio-banner' };
+    dirIn.ColorPaletteConstants.banner = 'amexio-banner'
+    expect(dirIn.ColorPaletteConstants.banner).toBeDefined();
+});
+
+it('getGradientStyles()case3', () => {
+  let obj = {amexioComponentId: 'amexio-navbar', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-navbar';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {navbar:  c };
+  expect(dirIn.ColorPaletteConstants.banner).toBeUndefined();
+  dirIn.ColorPaletteConstants = {navbar: 'amexio-navbar' };
+    dirIn.ColorPaletteConstants.navbar = 'amexio-navbar'
+    expect(dirIn.ColorPaletteConstants.navbar).toBeDefined();
+});
+
+it('getGradientStyles()case4', () => {
+  let obj = {amexioComponentId: 'amexio-accordion', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-accordion';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {accordion:  c };
+  expect(dirIn.ColorPaletteConstants.accordion).toBeUndefined();
+  dirIn.ColorPaletteConstants = {accordion: 'amexio-accordion' };
+    dirIn.ColorPaletteConstants.accordion = 'amexio-accordion'
+    expect(dirIn.ColorPaletteConstants.accordion ).toBeDefined();
+});
+
+it('getGradientStyles()case5', () => {
+  let obj = {amexioComponentId: 'amexio-panel', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-panel';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {panel:  c };
+  expect(dirIn.ColorPaletteConstants.panel).toBeUndefined();
+  dirIn.ColorPaletteConstants = {panel: 'amexio-panel' };
+    dirIn.ColorPaletteConstants.panel = 'amexio-panel'
+    expect(dirIn.ColorPaletteConstants.panel).toBeDefined();
+});
+
+it('getGradientStyles()case6', () => {
+  let obj = {amexioComponentId: 'amexio-floating-panel', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-floating-panel';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {floatingpanel:  c };
+  expect(dirIn.ColorPaletteConstants.floatingpanel).toBeUndefined();
+  dirIn.ColorPaletteConstants = {floatingpanel: 'amexio-floating-panel' };
+    dirIn.ColorPaletteConstants.floatingpanel = 'amexio-floating-panel'
+    expect(dirIn.ColorPaletteConstants.floatingpanel).toBeDefined();
+});
+
+it('getGradientStyles()case7', () => {
+  let obj = {amexioComponentId: 'amexio-window', setColorPalette: ()=>{}}
+  dirIn.hostComponent = obj
+  dirIn.hostComponent.amexioComponentId = 'amexio-window';
+  dirIn.getGradientStyles(dirIn.hostComponent);
+  let c;
+   dirIn.ColorPaletteConstants = {window:  c };
+  expect(dirIn.ColorPaletteConstants.window).toBeUndefined();
+  dirIn.ColorPaletteConstants = {window: 'amexio-window' };
+    dirIn.ColorPaletteConstants.window = 'amexio-window'
+    expect(dirIn.ColorPaletteConstants.window).toBeDefined();
+});
+
+
 
 
 it('getGradientStyles()', () => {
