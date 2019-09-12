8ee3799a27a45e6a074be16dea92f2ad22a52cee
Manisha1205 <manisha.boddu@metamagic.in>
2019-09-12 13:21:35 +0530

    dialpad: updated

--- src/module/forms/dialpad/dialpad.component.ts
+++ src/module/forms/dialpad/dialpad.component.ts
@@ -1,18 +1,29 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-    selector: 'amexio-dialpad',
+    selector: 'amexio-dial-pad',
     templateUrl: './dialpad.component.html',
 })
-export class AmexioDialpadComponent {
+export class AmexioDialpadComponent implements OnInit {
     btnArray1 = [0, 1, 2, 3, 4];
     btnArray2 = [5, 6, 7, 8, 9];
-
+    textType = '';
     @Input() value = '';
+    @Input('field-label') label = '';
+    @Input() type = '2-rows';
+    @Input() random: boolean;
+    @Input() password: boolean;
     @Output() valueChange: EventEmitter<any> = new EventEmitter<any>();
 
     @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
 
+    ngOnInit() {
+        if (this.password) {
+            this.textType = 'password';
+        } else {
+            this.textType = 'text';
+        }
+    }
     getBtnData(data: any) {
         this.value = this.value + data;
         this.emitBtnData(data);
@@ -31,6 +42,13 @@ export class AmexioDialpadComponent {
     emitBtnData(keycode: any) {
         const obj = { key: keycode, data: this.value };
         this.onClick.emit(obj);
+    }
+
+    clearData() {
+        this.value = '';
+        const object = { data: this.value };
+        this.onClick.emit(object);
+        this.valueChange.emit(this.value);
     }
 
 }
