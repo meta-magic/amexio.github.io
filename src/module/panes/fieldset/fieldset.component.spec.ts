57bf419ca2bfd51da0813bb34c427bca7278cee5
kedar8 <kedar.kokil@metamagic.in>
2019-09-23 15:35:34 +0530

    field set: test cases.

--- src/module/panes/fieldset/fieldset.component.spec.ts
+++ src/module/panes/fieldset/fieldset.component.spec.ts
@@ -6,7 +6,7 @@ import { AmexioStepsComponent } from '../steps/steps.component';
 import { CommonIconComponent } from './../../base/components/common.icon.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('amexio-fieldset', () => {
+fdescribe('amexio-fieldset', () => {
     let comp: AmexioFieldSetComponent;
     let fixture: ComponentFixture<AmexioFieldSetComponent>;
 
@@ -22,23 +22,47 @@ describe('amexio-fieldset', () => {
     });
 
 
-    it('checking ngOnInit method', () => {
+   
 
-        comp.ngOnInit();
-        comp.collapsible = false;
-        expect(comp.collapsible).toEqual(false);
-        comp.isActive = true;
-        expect(comp.isActive).toEqual(true);
-    });
-
-    it('onLegendClick', () => {
-
+    it('onLegendClick if condition', () => {
+        comp.collapsible = true;
         comp.onLegendClick();
         comp.collapsible = true;
         expect(comp.collapsible).toEqual(true);
-        comp.isActive = false;
-        expect(comp.isActive).toEqual(false);
+        comp.isActive = !comp.isActive;
     });
+    it('onLegendClick else condition', () => {
+        comp.collapsible = false;
+        comp.onLegendClick();
+        comp.collapsible = false;
+        expect(comp.collapsible).toEqual(false);
+    });
+
+    it('checking ngOnInit method if condition', () => {
+        comp.collapsible = false;
+        comp.expanded = false;
+        comp.ngOnInit();
+        comp.collapsible = false;
+        comp.expanded = false;
+        expect(comp.collapsible).toEqual(false);
+        expect(comp.expanded).toEqual(false);
+        comp.isActive = true;
+      
+    });
+
+    it('checking ngOnInit method else condition', () => {
+        comp.collapsible = true;
+        comp.expanded = true;
+        comp.ngOnInit();
+        comp.collapsible = true;
+        comp.expanded = true;
+        expect(comp.collapsible).toEqual(true);
+        expect(comp.expanded).toEqual(true);
+        comp.isActive = true;
+      
+    });
+
+
 
 });
 
