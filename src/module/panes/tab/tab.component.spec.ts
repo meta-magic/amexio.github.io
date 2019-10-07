8bd04fb8bd9a5634482cba13f4741631c9f14ef3
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-10-07 14:45:08 +0530

    tab: text case for okclick method updated

--- src/module/panes/tab/tab.component.spec.ts
+++ src/module/panes/tab/tab.component.spec.ts
@@ -82,33 +82,13 @@ describe('amexio-tab', () => {
         });
     });
 
+    it('Ok method Call', () => {
+      comp.tabPillClose(tabNode);
+      comp.openDialogue = false;
+    });
 
 
-    // it('onOkClick method', () => {
-    //     comp.onOkClick();
-    //     //  comp.tabPillClose(tabNode);
-    //     comp.openDialogue = false;
-    // });
 
-    // it('tabPillClose method', () => {
-    //     tabNode = {
-    //         active: false,
-    //         amexiocolor: undefined,
-    //         closable: true,
-    //         disabled: false,
-    //         tabId: 23540,
-    //         tabPillClass: "activecolortab",
-    //         tablk: "21659_tablk"
-    //     }
-
-    //     comp.tabPillClose(tabNode);
-    //     const newTab: AmexioTabPillComponent[] = [];
-    //     let index = 0;
-    //     let tabHighlightIndex = 0;
-    //     comp.tabCollection.forEach((tab: any, i: number) => {
-    //         tab.active = false;
-    //         expect(tab.tabId).toEqual(tabNode.tabId);
-    //     });
-    // });
+    
 });
 
