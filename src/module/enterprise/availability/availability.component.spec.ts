0cbaa670b23cd9c4265bbbc9c38777a5b5545c49
Manisha1205 <manisha.boddu@metamagic.in>
2019-10-09 18:57:16 +0530

    availability: test cases added

--- src/module/enterprise/availability/availability.component.spec.ts
+++ src/module/enterprise/availability/availability.component.spec.ts
@@ -94,17 +94,5 @@ describe('amexio-availability', () => {
     expect(comp.setColorRangeTest(slotArray, minmaxarr, labelelement)).toHaveBeenCalled;
   });
 
-  //  it('generateTimeArr()', () => {
-  //    let  startindex = 0;
-  //    comp.startTime = 3;
-  //    comp.completeTimeArr = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
-  //      comp.generateTimeArr();
-      
-  //      comp.completeTimeArr.forEach((element: any, index: number) => {
-  //       if (element === this.startTime) {
-  //       expect(startindex)
-  //       }
-  //     });
-  // });
-
+ 
 });
