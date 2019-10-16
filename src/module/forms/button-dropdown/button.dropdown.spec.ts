87bd8aaf2800f793b05935633e66e276bc8825a9
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-10-16 11:02:59 +0530

    btn: dropdown testcase added

--- src/module/forms/button-dropdown/button.dropdown.spec.ts
+++ src/module/forms/button-dropdown/button.dropdown.spec.ts
@@ -1,4 +1,31 @@
-describe('buttondropdown ', () => {
-    it('true is true', () => 
-    expect(true).toBe(true));
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../../../index';
+import { AmexioButtonDropDownItemComponent } from './button.dropdown.item';
+import { CommonIconComponent } from './../../base/components/common.icon.component';
+import { AmexioButtonDropdownComponent } from './button.dropdown';
+
+describe('amexio-spilt-button', () => {
+  let comp: AmexioButtonDropdownComponent;
+  let fixture: ComponentFixture<AmexioButtonDropdownComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [CommonIconComponent, AmexioButtonDropDownItemComponent, AmexioButtonDropdownComponent],
+      providers: [IconLoaderService],
+    });
+    fixture = TestBed.createComponent(AmexioButtonDropdownComponent);
+    comp = fixture.componentInstance;
   });
+
+  it('Init buttonDropdown', () => {
+    comp.label = 'button-dropdown';
+    comp.componentId = comp.label + window.crypto.getRandomValues(new Uint32Array(1))[0];
+  });
+
+});
+
+
+
