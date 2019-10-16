76644456de09c1c8517282d4c8e2090338a368af
Manisha1205 <manisha.boddu@metamagic.in>
2019-10-16 18:18:48 +0530

    googlemap: test case added

---
+++ src/module/data/googlemap/googlemap.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'; 
+import { AmexioGoogleMapComponent } from './googlemap.component';
+import { GoogleMapOverlays } from '../../../models/googlemap.model';
+import { GoogleMapScriptService } from '../../services/script/script.data.service';
+import { GOOGLEMAP_CONSTANT } from './googlemap.constant';
+
+fdescribe('AmexioGoogleMapComponent', () => {
+  let comp1: AmexioGoogleMapComponent;
+  let fixture1: ComponentFixture<AmexioGoogleMapComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AmexioGoogleMapComponent],
+      providers: [ GoogleMapScriptService],
+
+    });
+    fixture1 = TestBed.createComponent(AmexioGoogleMapComponent);
+    comp1 = fixture1.componentInstance;    
+  });
+
+  it('ngOnInit()', () => {
+   comp1.ngOnInit();
+   comp1.componentId = +window.crypto.getRandomValues(new Uint32Array(1))[0] + 'google';
+   expect(comp1.componentId).toBeDefined();
+  });
+
+});
