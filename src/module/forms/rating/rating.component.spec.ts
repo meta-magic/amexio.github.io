2eb37faedaf6aa64dd21b96324b0e6ca9f5dfb42
anaghak07 <anagha.kulkarni@metamagic.in>
2019-10-16 13:03:34 +0530

    rating:added the testcase

--- src/module/forms/rating/rating.component.spec.ts
+++ src/module/forms/rating/rating.component.spec.ts
@@ -1,6 +1,55 @@
+import { AmexioRatingComponent } from "./rating.component";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+
 /**
  * Created by pratik on 7/12/17.
  */
-describe('Button tests', () => {
+fdescribe('Button tests', () => {
+
+  let component: AmexioRatingComponent;
+  let fixture: ComponentFixture<AmexioRatingComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AmexioRatingComponent],
+      providers: [
+      ],
+
+    });
+    fixture = TestBed.createComponent(AmexioRatingComponent);
+    component = fixture.componentInstance;
+    component.ratingRange = [1, 2, 3, 4, 5];
+  });
+
   it('true is true', () => expect(true).toBe(true));
+
+  it('verify that the component AmexioRatingComponent is created', () => {
+    expect(fixture.componentInstance instanceof AmexioRatingComponent)
+      .toBe(true, 'should create AmexioRatingComponent');
+  });
+
+  it('ngOnInit: check onInit call', () => {
+    component.componentId = component.createCompId('rating', component.fieldlabel);
+    let starId = 'star' + Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0]);
+
+    expect(component.ratingRange).toBeDefined();
+    expect(starId).toBeDefined();
+
+    for (let i = 0; i < component.ratingRange.length; i++) {
+      const obj = {};
+      obj['number'] = i + 1;
+      obj['selected'] = false;
+      obj['tabindex'] = '-1';
+      component.ratingRangeData.push(obj);
+
+      expect(obj['number']).toEqual(i + 1);
+      expect(obj['selected']).toEqual(false);
+      expect(obj['tabindex']).toEqual('-1');
+    }
+    let temp: any;
+    temp = new AmexioRatingComponent();
+    temp.buildRanges();
+    var spy = spyOn<any>(temp, 'buildRanges');
+    temp.buildRanges();
+    expect(spy.calls.any()).toBeTruthy();
+  });
 });
