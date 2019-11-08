236b270b42a1c9303d31ffc272ec73a59a9c591d
kedar8 <kedar.kokil@metamagic.in>
2019-11-08 20:07:15 +0530

    browser: code updated.

--- src/module/data/browser-panel/browser-panel.component.ts
+++ src/module/data/browser-panel/browser-panel.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } 
 
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
+import { MinimizeService } from '../../panes/window/minimize-service.service';
+
 @Component({
     selector: 'amexio-browser-panel',
     templateUrl: './browser-panel.component.html',
@@ -10,58 +12,59 @@ export class BrowserPanelComponent implements OnInit {
 
     @Input('url') url: string;
 
+    @Input('browser-name') textName: string;
+
     @Input('url-field-editable') urlFieldEditable = false;
 
     @Input('browser-in-window') showBrowserWithWindow = false;
 
     @Input('reload-button') reloadBtn = false;
 
-    @Input('show') showBrowser = false;
+    @Input('show') show = false;
 
     @Output() showChange: any = new EventEmitter<any>();
+    amexioComponentId = 'amexio-browser' + Math.floor(Math.random() * 1000 + 999);
+
     iframeurl: SafeResourceUrl;
 
     // Html reference with view child.
     @ViewChild('fullscreen') fullscreen: any;
     @ViewChild('loadSite') loadSite: any;
     @ViewChild('newURL') newURL: any;
-    hideBtn = false;
     lockIconShow = false;
     globalListenFunc: () => void;
-    constructor(private sanitizer: DomSanitizer, private renderer: Renderer2) {
+    constructor(private sanitizer: DomSanitizer, private renderer: Renderer2, private _browserService: MinimizeService) {
     }
 
     ngOnInit() {
-        if (this.url.indexOf('https') === 0) {
+        if (this.url && this.url.indexOf('https') === 0) {
             this.lockIconShow = true;
         }
 
         this.globalListenFunc = this.renderer.listen('document', 'keyup.esc', (e: any) => {
             if (this.showBrowserWithWindow) {
-                this.showBrowser = false;
-                this.showChange.emit(this.showBrowser);
+                this.show = false;
+                this.showChange.emit(this.show);
             }
         });
 
         this.iframeurl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-        this.hideBtn = false;
     }
     onCloseClick() {
-        this.showBrowser = false;
-        this.showChange.emit(this.showBrowser);
+        this.show = false;
+        this._browserService.onCloseClick(this);
+        this.showChange.emit(this.show);
 
     }
     browserShowOnBtn() {
-        this.showBrowser = true;
-        this.hideBtn = false;
-        this.showChange.emit(this.showBrowser);
+        this.show = true;
+        this.showChange.emit(this.show);
 
     }
     onMinimizeClick() {
-        this.showBrowser = false;
-        this.hideBtn = true;
-        this.showChange.emit(this.showBrowser);
-
+        this.show = false;
+        this._browserService.onMinimizeClick(this);
+        this.showChange.emit(this.show);
     }
 
     onReloadPage() {
@@ -84,7 +87,5 @@ export class BrowserPanelComponent implements OnInit {
         } else {
             this.showBrowserWithWindow = true;
         }
-
     }
-
 }
