5934d266c8b66ff8996cf8103c2609d576a88130
Manisha1205 <manisha.boddu@metamagic.in>
2020-07-06 18:28:35 +0530

    dropdown: on chkbox click dropdown doesnt vanish

--- src/module/base/event.base.component.ts
+++ src/module/base/event.base.component.ts
@@ -26,7 +26,7 @@ export class EventBaseComponent<T> extends ValueAccessorBaseComponent<T> {
     documentClickListener: any;
     /* hideDropdown : flag for multiselect with chcekbox */
     hideDropdown: any = undefined;
-    dropFlag: boolean = false;
+    dropFlag = false;
     constructor(public renderer: Renderer2, public element: ElementRef, private cd: ChangeDetectorRef) {
         super();
         this.hide();
@@ -53,13 +53,11 @@ export class EventBaseComponent<T> extends ValueAccessorBaseComponent<T> {
     bindDocumentClickListener() {
         if (!this.documentClickListener) {
             this.documentClickListener = this.renderer
-                .listen('document', 'click', (event: any) => 
-                {
+                .listen('document', 'click', (event: any) => {
                    if (!this.dropFlag) {
                     this.handleDocumentListener(event);
                    }
-                }
-            );
+                });
         }
 
     }
