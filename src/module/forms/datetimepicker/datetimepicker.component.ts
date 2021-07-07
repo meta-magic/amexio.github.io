1adebd65fb8ce0f92f6cbe36a8f63cd63ff5f5dc
ashwiniagre1 <ashwini.agre@metamagic.in>
2021-07-07 09:40:20 +0530

    build issue resolved

--- src/module/forms/datetimepicker/datetimepicker.component.ts
+++ src/module/forms/datetimepicker/datetimepicker.component.ts
@@ -929,11 +929,10 @@ export class AmexioDateTimePickerComponent extends ListBaseDatepickerComponent<s
       if (this.required) {
         this.isValid = true;
       }
-    }else if(value == '' || value == null){
-      this.innerValue = '';
-      this.dateModel = '';
-    }
-     else {
+      } else if (value === '' || value === null){
+        this.innerValue = '';
+        this.dateModel = '';
+      } else {
       this.negateisValid();
     }
   }
